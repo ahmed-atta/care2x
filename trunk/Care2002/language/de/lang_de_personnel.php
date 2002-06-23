0018fad9922d61b63e0ba9887d11c2fc220dbc70
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2002-06-23 14:53:19 +0000

    Initial revision

---
+++ trunk/Care2002/language/de/lang_de_personnel.php
@@ -0,0 +1,9 @@
+<?
+$LDDbNoLink="Verbindung zu DB gescheitert!";
+$LDDbNoRead="Lesen aus der DB is gescheitert!";
+$LDDbNoSave="Schreiben in die DB is gescheitert!";
+$LDDbNoUpdate="Update ist gescheitert!";
+$LDDbNoLastData="Letzte Daten nicht gefunden!";
+$LDDbNoDelete="Löschen in DB ist gescheitert!";
+
+?>
