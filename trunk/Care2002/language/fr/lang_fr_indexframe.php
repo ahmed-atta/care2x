43a5b02d5b10dc65c6a0af1e2d0c44752ca33cc1
matejh <matejh@8154e661-2335-47f1-af25-7e1110f3596a>
2002-10-21 20:28:27 +0000

    pre-prepared files to translate into french

---
+++ trunk/Care2002/language/fr/lang_fr_indexframe.php
@@ -0,0 +1,43 @@
+<?php
+$indextag=array("Home&nbsp;",
+				"Directory&nbsp;",
+				"Admission&nbsp;",
+				"Medocs&nbsp;",
+				"Doctors&nbsp;",
+				"Nursing&nbsp;",
+				"OR&nbsp;",
+				"Tech Support&nbsp;",
+				"Laboratories&nbsp;",
+				"Radiology&nbsp;",
+				"Pharmacy&nbsp;",
+				"Medical Depot&nbsp;",
+				"EDP &nbsp;",
+				"Intranet Email&nbsp;",
+				"Internet Email&nbsp;",
+				"Specials &nbsp;",
+				"Login &nbsp;"
+				);
+/**
+* The divtag variable is experimental
+*/
+$divtag=array("Home",
+				"Phone Directory",
+				"Admission",
+				"Medocs",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				""
+				);
+$VersionChgTarget="_top";
+$LDLanguage="Language";
+$LDPlsWaitInit="Please hold on. Initialization process is going on....";
+?>
