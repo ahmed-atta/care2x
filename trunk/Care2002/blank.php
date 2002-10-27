c9add00042ba8e68d7a0932c0d6b52cc100f8822
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2002-10-27 17:32:14 +0000

    beta 1.0.03

--- trunk/Care2002/blank.php
+++ trunk/Care2002/blank.php
@@ -1,5 +1,6 @@
 <?php
 error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+require_once('include/inc_vars_resolve.php'); // globalize POST, GET, & COOKIE  vars
 if(!lang)
 {
  	$ck_lang_buffer="ck_lang$sid";
@@ -10,6 +11,12 @@ if(!lang)
 if(file_exists("language/".$lang."/lang_".$lang."_indexframe.php")) include("language/".$lang."/lang_".$lang."_indexframe.php");
     else include("language/en/lang_en_indexframe.php")
 ?><html>
+<?php 
+
+include_once('include/inc_charset_fx.php');
+echo setCharSet(); 
+
+?>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <title>Init Page</title>
