f4d421a4db8700eaa3a279765b0e5bf3c74fec06
bsiegert <bsiegert@8154e661-2335-47f1-af25-7e1110f3596a>
2002-10-29 17:26:13 +0000

    Neuer Copyright-Hinweis in besserem Deutsch :)

--- trunk/Care2002/language/de/de_copyrite.php
+++ trunk/Care2002/language/de/de_copyrite.php
@@ -2,16 +2,16 @@
 if(!isset($path_root)||$path_root=="") $path_root="../";
 require($path_root.'include/inc_credits_openwin.php');
 ?>
-<FONT    SIZE=1  FACE="Arial" color=gray>
-Dieses Program wird mit <a href="http://www.care2x.com" target=_new>CARE 2002</a> Beta version 1.0.03 betrieben. 
-CARE 2002 ist ein OSD Software und wird mit GNU GPL Lizenz geschrieben und verteilt. 
-CARE 2002 wird mit der "GNU General Public License" Lizenz geschrieben und kostenlos zur Verfügung gestellt. Für Details siehe die
-<a href="<?php echo $path_root ?>legal_gnu_gpl.htm" target=_new>Kopie dieser Lizenz</a>. 
-Für weitere Fragen kontaktieren Sie bitte
-Elpidio Latorilla an <a href=mailto:elpidio@latorilla.com>elpidio@latorilla.com</a>.
+<font size="1" face="Arial" color="gray">
+Dieses Programm wird mit <a href="http://www.care2x.com" target="_new">CARE 2002</a> Beta version 1.0.03 betrieben. 
+CARE 2002 ist Freie Software und unterliegt der "GNU General Public License".
+CARE 2002 wird unter den Bedingungen der GPL kostenlos zur Verfügung gestellt. Für Details siehe die
+<a href="<?php echo $path_root ?>legal_gnu_gpl.htm" target="_new">Kopie dieser Lizenz</a>. 
+Bei weiteren Fragen kontaktieren Sie bitte
+Elpidio Latorilla unter <a href="mailto:elpidio@latorilla.com">elpidio@latorilla.com</a>.
 <p>
-<font size=1 face="arial">
+<font size="1" face="arial">
 [ <a href="<?php echo $path_root ?>language/de/de_privacy.htm" target="pp">Unsere Datenschutzbestimmungen</a> ]
 [ <a href="javascript:openCreditsWindow()"> Anerkennung </a> ]<br></font>
 <br></font>
-</FONT>
+</font>
