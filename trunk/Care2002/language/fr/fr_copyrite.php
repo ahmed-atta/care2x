9f9708b14d98cb587f10be66ea35261c343bc0c1
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2002-10-31 21:12:38 +0000

    credits added beta 1.0.03  2002-10-31

--- trunk/Care2002/language/fr/fr_copyrite.php
+++ trunk/Care2002/language/fr/fr_copyrite.php
@@ -1,16 +1,18 @@
 <?php
-if(!isset($path_root)||$path_root=="") $path_root="../";
-
+if($path_root=='') $path_root='../';
 require($path_root.'include/inc_credits_openwin.php');
-
 ?>
-<FONT    SIZE=1  FACE="Arial" color=gray>
- Cette application est actionnée par <a href="http://www.care2x.com" target=_new>CARE 2002 version béta 1.0.03</a>. Logiciel libre disponible sous une catégorie de licence GPL.
- CARE 2002 est écrite et distribuée gratuitement sous GNU General Public License. Pour plus de détails sur cette licence veuillez voir cette
- <a href="<?php echo $path_root ?>legal_gnu_gpl.htm" target=_new>copie de la licence</a>.
- Pour encore plus d'enseigenements veuillez bien contacter Elpidio Latorilla à <a href=mailto:elpidio@latorilla.com>elpidio@latorilla.com</a>.
- <p>
-<font size=1 face="arial">
-[ <a href="<?php echo $path_root ?>language/fr/fr_privacy.htm" target="pp"> Notre Politique de discrétion </a> ]
+ <font size="1" face="Arial" color="gray"> La version française est développée et managée par Matej Hausenblas.
+Veuillez bien diriger toutes les questions concernant la version française 
+vers lui. Version Beta 1.0.03 </font> <font size="1" face="Arial" color="gray">CARE 2002 est écrite et
+distribuée librement sous la licence GNU General Public License. Pour
+plus de détail sur cette licence, allez voir  <a
+ href="../legal_gnu_gpl.htm" target="_new">la copie de la licence</a> (en
+anglais).   Pour plus d'investigation, veuillez-vous contacter 
+<a href="mailto:info@care2x.com">info@care2x.com</a>. </font></p>
+ 
+<p> <font size="1" face="Arial" color="gray"><font size="1" face="arial">
+[ <a href="<?php echo $path_root ?>language/fr/fr_privacy.htm" target="pp">Notre politique de discrétion</a> ]
 [ <a href="javascript:openCreditsWindow()"> Crédits </a> ]<br></font>
-</FONT>
+<br>
+ </font> </font> 
