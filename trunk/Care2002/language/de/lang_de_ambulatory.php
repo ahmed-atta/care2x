251b0cc02b76accc3d87980915c19d3cf3f03381
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2002-12-16 21:37:46 +0000

    beta 1.0.03

---
+++ trunk/Care2002/language/de/lang_de_ambulatory.php
@@ -0,0 +1,17 @@
+<?php
+/* 2002-10-02 EL */
+$LDDepartments='Ambulanz';
+$LDPendingRequest='Anstehende Anforderung';
+$LDPendingRequestTxt='Empfangen, Auflisten, Ausdrucken, Befunden';
+$LDGeneralSurgery='Allgemeine Ambulanz';
+$LDInternalMed='Innere Medizin Ambulanz';
+$LDEmergency='Unfall Ambulanz';
+$LDSonography='Sonographie';
+$LDNuclearMed='Nuklear Medizin Ambulanz';
+$LDAdmission='Aufnahme';
+$LDAdmissionTxt='Aufnahme & Entlassung ambulanter Patienten, Suche, Archiv';
+$LDNews='Nachrichten';
+$LDNewsTxt='Abteilungsspezifischer Nachrichten, Mitteilung, Info, u.a.';
+
+?>
+
