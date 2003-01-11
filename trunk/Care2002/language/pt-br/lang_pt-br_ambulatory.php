690296127dc2bff0c542fe6375d73206fe14c3f9
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-01-11 20:00:18 +0000

    v 1.0.03 translation by Daniel Zilli

---
+++ trunk/Care2002/language/pt-br/lang_pt-br_ambulatory.php
@@ -0,0 +1,17 @@
+<?php
+/* 2002-10-02 EL */
+$LDDepartments='Ambulatório';
+$LDPendingRequest='Pedido pendente';
+$LDPendingRequestTxt='Recepção, Lista, Impressão, Relatório médico e Resultados';
+$LDGeneralSurgery='Ambulatório Geral';
+$LDInternalMed='Ambulatório Medicina Interna';
+$LDEmergency='Ambulatório de Emergência';
+$LDSonography='Sonografia';
+$LDNuclearMed='Nuclear Diagnostics';
+$LDAdmission='Admissão';
+$LDAdmissionTxt='Admission/discharge of ambulatory, search, archive';
+$LDNews='Notícias';
+$LDNewsTxt='Notícias específicas da clínica, memorando, info, etc.';
+
+?>
+
