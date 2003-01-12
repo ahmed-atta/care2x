3c5a3c566c6c791190b36a820164349bd2a17508
roupec <roupec@8154e661-2335-47f1-af25-7e1110f3596a>
2003-01-12 05:22:42 +0000

    By Petr Roupec -- CZECH Version

---
+++ trunk/Care2002/language/cs-iso/lang_cs-iso_db_msg.php
@@ -0,0 +1,8 @@
+<?php
+$LDDbNoSave="Pokus o ulo¾ení dat do databáze selhal!";
+$LDDbNoRead="Pokus o ètení z databáze selhal!";
+$LDDbNoLink="Server se má problémy s pøipojením se k databázi!";
+$LDDbNoUpdate="Update databáze selhal!";
+$LDDbNoLastData="Naposledy ulo¾ená data nebyla nalezena!";
+$LDDbNoDelete="Mazání v databázi selhalo!";
+?>
