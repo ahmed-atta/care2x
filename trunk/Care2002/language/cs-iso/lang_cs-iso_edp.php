b0f80515699b4904757d7c637d78006a330af70a
roupec <roupec@8154e661-2335-47f1-af25-7e1110f3596a>
2003-01-12 05:32:25 +0000

    By Petr Roupec --- CZECH Version

---
+++ trunk/Care2002/language/cs-iso/lang_cs-iso_edp.php
@@ -0,0 +1,193 @@
+<?php
+$LDClose="Zavøít";
+$LDCancel="Zru¹it";
+$LDResetEntry="Smazat polo¾ky";
+$LDSave="Ulo¾it";
+$LDReset="Vyèistit formuláø";
+
+
+$LDEnterData="Zadávat data";
+$LDClk2See="Prohlí¾et data";
+$LDFoundPatient="Bylo nalezeno <b>~nr~</b> pacientù";
+$LDWildCards="Co jsou to zástupné znaky a jak se pou¾ívají";
+$LDNewSearch="Hledat znovu";
+$LDSearch="Hledat";
+
+$LDOClock="hodin";
+$LDContinue="Pokraèovat";
+$LDBack="Zpìt";
+
+$LDOn="Dne";
+$LDAt="v";
+$LDClk2Edit="Pro editaci reportu kliknìte zde";
+
+$LDNew="Nový";
+$LDEdit="Editovat";
+$LDCreate="Vytvoøit";
+$LDValue="Hodnota";
+
+$LDNews="Zprávy";
+$LDNewsTxt="Èíst nebo editovat èlánky od administrátorù systému";
+$LDMemo="Poznámka";
+$LDMemoTxt="Èíst nebo napsát poznámku";
+$LDSearchPat="Hledat pacienta";
+$LDCategory="Kategorie";
+$LDPast3Days="Poslední tøi dny";
+$LDPast3Months="Poslední tøi mìsíce";
+$LDPastAll="V¹e";
+$LDSelect="Výbìr";
+
+$LDOrderArchive="Archív";
+
+
+
+$LDNoDataFound="Bylo nalezeno <font color=red><b>no</b></font> záznamù.";
+$LDClk2SeeInfo="Prosím zvolte polo¾ku a kliknìte na ní k zobrazení více informací";
+/********************** Do not erase or replace the ~nr~ *****************************/
+$LDFoundNrData="Podle zadaných kriterií bzlo nalezeno <font color=red><b>~nr~</b></font> záznamù.";
+$LDOpenInfo="Zobrazit kompletní informaci o ";
+
+$LDGoodMorning="Dobré ráno";
+$LDGoodDay="Ahoj rád tì vidím ";
+$LDGoodEvening="Dobrý veèer";
+
+$LDDate="Datum";
+$LDDept="Oddìlení";
+$LDPrio="Dùle¾itost";
+$LDSearchIn="Potom hledat";
+
+
+$LDPlsEnterMore="Prosím zadejte podrobnìj¹í informace a zkuste nové hledání.";
+$LDNoSingleChar="Zadání jednoho znaku bude ignorováno.";
+
+$LDPlsInformDept="Prosím informujte ~tagword~  oddìlení. Dìkuji";
+
+$LDReports="Reporty";
+$LDReportsTxt="Vytváøet, hledat a  èíst reporty";
+$LDInfo="Informace";
+$LDInfoTxt="Vyhledávat informace z oddìlení Administrátorù";
+$LDManage="spáva";
+$LDUpdateOk="Zmìna provena  <b>úspì¹nì</b>:";
+$LDDataSaved="Ní¾e uvedená data byla ulo¾ena:";
+$LDDataNoUpdate="Zmìna  <b>selhala</b>. Prosím ovìøte znovu v¹echny polo¾ky.";
+$LDDataNoSaved="Ulo¾ení dat <b>selhalo</b>.Prosím ovìøte znovu v¹echny polo¾ky .";
+$LDBack2Menu="Zpìt na hlavní menu";
+$LDPageTop="Zpìt na zaèátek dokumentu.";
+$LDPreview="Náhled";
+$LDUpdateData="Zmìnit ";
+$LDRemoveFromDb="Odstranit z databéze";
+$LDDataRemoved="Produkt byl odstranìn z databáze!";
+
+$LDConfirmDelete="Skuteènì si pøejete <b>odstranit / smazat</b> ní¾e uvedené informace z databáze ?";
+$LDAlertDelete="<b>VAROVÁNÍ!</b> Data budou odstranìna permanentnì. Data ji¾ nepùjde obnovit!";
+$LDNoDelete="Mazání dat selhalo<br>Prosím informujete administrátora systému.";
+$LDYesDelete="Ano jsem si jist. Sma¾ pøístupová práva.";
+$LDNoBack="Ne, bì¾ zpìt.";
+$LDClk2Ack="Kliknutím na ¹ipku potvrdíte objednávku.";
+$LDOK="OK";
+$LDManageAccess="Pøístupová práva";
+$LDManageAccessTxt="Správa, vytváøení, blokování, odstraòování nebo zmìna pøístupových práv.";
+$LDSqlDb="SQL Databáze";
+$LDSqlDbTxt="Pøímý SQL pøístup do databáze. <b>VAROVÁNÍ </b>pouze pro experty.";
+$LDSysOpLogin="Pøihlá¹ení správce systému";
+$LDSysOpLoginTxt="Pøihlásit jako správce systému";
+$LDEDP="Administrátor";
+$LDNewAccess="Vytvoøit nová pøístupová práva";
+$LDListActual="Výpis aktuálních pøístupových práv";
+$LDName="Jméno";
+$LDPassword="Heslo";
+$LDUserId="U¾ivatelské jméno ";
+$LDArea="Oblast";
+$LDAllowedArea="U¾ivatel má pøístup do ní¾e uvedených èástí systému:";
+$LDActualAccess="Aktuální pøístupová práva";
+$LDAccessDeleted="Pøístupová práva zru¹ena úspì¹nì.";
+$LDFfActualAccess="Ní¾e jsou uvedena aktuální pøístupová práva.";
+
+$LDAccessIndex=array(
+				"Jméno",
+			 	"Pøihla¹ovací jméno",
+			 	"Heslo",
+				"",
+			 	"Oblsti s oprávnìním pøístupu",
+			 	"Datum / Èas",
+			 	"Zaznamenal",
+			 	"Mo¾nosti"
+			 	);
+$LDChange="Modifikovat";
+$LDInitChange="M";
+$LDLock="Zmrazit";
+$LDInitLock="Z";
+$LDUnlock="Odemknout";
+$LDInitUnlock="O";
+$LDDelete="Smazat";
+$LDInitDelete="S";
+$LDUpdateRight="Upravit pøístupová práva";
+$LDInputError="Zadána ¹patná data nebo nebyly zadány v¹echny potøebné údaje. Políèka oznaèená èervenì neobsahují spravné nebo ¾ádné údaje.";
+$LDAccessRight="Pøístupová práva";
+$LDSureLock="Jistì si pøejete ZMRAZIT pøístupová práva do systému?";
+$LDSureUnlock="Jistì si pøejete ODEMKNOUT pøístupová práva do systému?";
+$LDSureDelete="Jistì si pøejete SMAZAT pøístupová práva do systému?";
+$LDYesSure="Ano, jsem si jist(á).";
+$LDKeywordPrompt="Zadejte klíèové slovo pro hledání";
+$LDSystemAdmin="Správce systému";
+$LDMySQLManage="Správa SQL databáze s pomocí PHP MySQLAdmin";
+$LDSpexFunctions="Jiné speciální funkce";
+$LDWelcome="Vítejte";
+$LDForeWord="Máte nejvy¹¹í pøístupová práva.<br>Následující funkce jsou pøístupné bez jakéhokoliv omezení. <br><b>Prosím pracujte s maximální opatrností.</b>";
+
+$LDSetDateFormat="Nastavit formát datumu";
+$LDSelectDateFormat="Prosím vyberte po¾adovaný formát datumu:";
+/* Do not translate $LDDateFormats */
+$LDDateFormats=array("dd.MM.yyyy","yyyy-MM-dd","MM/dd/yyyy");
+
+$LDDateFormatsTxt= array("Pøíklad: 01.10.2002 (01 Øíjna 2002)",
+                                         "Pøíklad: 2002-10-01 (2002 Øíjen 01)",
+										 "Pøíklad: 10/01/2002 (Øíjen 01, 2002)");
+
+$LDNewDateFormatSaved="Nový formát data je platný od tohoto okam¾iku.";
+
+$LDSetCurrency="Nastavit mìnu";
+$LDNewCurrencySet="Nová mìna je aktivní.";
+$LDPlsSelectCurrency="Prosím vyberte po¾adovanou mìnu.";
+$LDAddCurrency="Pøidat nový typ mìny";
+$LDPlsAddCurrency="Prosím zadejte infrmace o po¾adované mìnì a stisknìte tlaèítko \"Ulo¾it\".";
+$LDAddedNewCurrency="Informace o nové mìnì ulo¾eny.";
+$LDmain="hlavní mìna";
+$LDClk2AddCurrency="Pro nastavení nové mìny stiknìte stisknìte zde.";
+$LDCurrencyShortName="Symbolické nebo krátké jméno mìny:";
+$LDCurrencyLongName="Popis mìny:";
+$LDCurrencyInfo="Dal¹í informace:";
+$LDClk2SetCurrency="Pro nastavení hlavní mìny kliknìte zde.";
+$LDCurrencyUpdated="Zmìnìné informace byly ulo¾eny.";
+$LDUpdateCurrencyInfo="Modifikovat informace o mìnì";
+$LDPlsEnterUpdate="Prosím upravte informace o mìnì a stisknìte tlaèítko \"Ulo¾it\".";
+
+$LDEditInfo="Modifikovat";
+$LDCurrencyAdmin="Správa mìny";
+
+/**
+* The following lines must be modified according to the examples:
+* english:
+* day = d , month = m, year = y
+* result => dd.mm.yyyy
+*
+* german:
+* day = t, month = m, year = j
+* result => tt.mm.jjjj
+*
+* indonesian:
+* day = h, month = b, year = t
+* result => hh.bb.tttt
+*
+* BEGIN */
+$LD_ddpMMpyyyy="dd.mm.yyyy";
+$LD_yyyyhMMhdd="yyyy-mm-dd";
+$LD_MMsddsyyyy="mm/dd/yyyy";
+/* END */
+/* 2002-10-22 EL */
+$LDUserInfoSaved='Nový u¾icatel byl úspì¹nì vytvoøen';
+$LDUserInfoNoSave='Vytvoøení pøístupových práv selhalo.Prosím ovìøte zadané informace';
+$LDNoAreas='Nebyla vybrána ¾ádná oblast!';
+$LDUserDouble='U¾ivatel ji¾ existuje v systému.Prosím zadejte jiné u¾ivatelské jméno.';
+$LDEnterNewUser='Vytvoøení nového u¾ivatelského úètu';
+?>
