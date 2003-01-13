7f0cd924fa234a71e0eaf63e8f54392cedad98ef
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-01-13 20:57:45 +0000

    Added credit notes

--- trunk/Care2002/language/cs-iso/cs-iso_copyrite.php
+++ trunk/Care2002/language/cs-iso/cs-iso_copyrite.php
@@ -1,16 +1,6 @@
-<?php
-if(!isset($path_root)||$path_root=="") $path_root="../";
+<?php 
+if(!isset($path_root)||$path_root=="") $path_root="../"; 
 
-require($path_root.'include/inc_credits_openwin.php');
-
+require($path_root.'include/inc_credits_openwin.php'); 
 ?>
-<FONT    SIZE=1  FACE="Arial" color=gray>
- This application is powered by <a href="http://www.care2x.com" target=_new>CARE 2002 beta version 1.0.03</a>. An open source software available with GPL license category.
- CARE 2002 is written and distributed for free under the GNU General Public License. For details about this licence see the 
- <a href="<?php echo $path_root ?>legal_gnu_gpl.htm" target=_new>copy of the license</a>. 
- For further inquiries please contact Elpidio Latorilla at <a href=mailto:elpidio@latorilla.com>elpidio@latorilla.com</a>.
- <p>
-<font size=1 face="arial">
-[ <a href="<?php echo $path_root ?>language/en/en_privacy.htm" target="pp"> Our Privacy Policy </a> ] 
-[ <a href="javascript:openCreditsWindow()"> Credits </a> ]<br></font>
-</FONT>
+<FONT    SIZE=1  FACE="Arial" color=gray>This Czech version is currently being developed by Petr Roupec. This application is powered by <a href="http://www.care2x.com" target=_new>CARE 2002 beta version 1.0.03</a>. An open source software available with GPL license category. CARE 2002 is written and distributed for free under the GNU General Public License. For details about this licence see the  <a href="<?php echo $path_root ?>legal_gnu_gpl.htm" target=_new>copy of the license</a>.  For further inquiries please contact Elpidio Latorilla at <a href=mailto:elpidio@latorilla.com>elpidio@latorilla.com</a>. <p><font size=1 face="arial">[ <a href="<?php echo $path_root ?>language/en/en_privacy.htm" target="pp"> Our Privacy Policy </a> ] [ <a href="javascript:openCreditsWindow()"> Credits </a> ]<br></font></FONT>
