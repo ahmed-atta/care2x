36b7005dfc5fe8398ba4db3e3d1ca533aaface05
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-01-19 20:54:03 +0000

    removed trailing whitespace after the "?>" which caused a bug

--- trunk/Care2002/language/no/lang_no_db_msg.php
+++ trunk/Care2002/language/no/lang_no_db_msg.php
@@ -1,8 +1,15 @@
 <?php
+
 $LDDbNoSave="Lagringsforsøk mot databasen feilet!";
+
 $LDDbNoRead="Leseforsøk til databasen feilet!";
+
 $LDDbNoLink="Får ikke kontakt med databasen!";
+
 $LDDbNoUpdate="Forsøk på å oppdatere databasen feilet";
+
 $LDDbNoLastData="Siste data ble ikke funnet!";
+
 $LDDbNoDelete="Sletting feilet!";
+
 ?>
