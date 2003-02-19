36e6ebabfd2a4750dcf6f1182e008fcdb0468e41
alfrod <alfrod@8154e661-2335-47f1-af25-7e1110f3596a>
2003-02-19 17:49:29 +0000

    *** empty log message ***

---
+++ trunk/Care2002/language/pt/lang_pt_indexframe.php
@@ -0,0 +1,27 @@
+<?php
+$indextag=array('Página Inicial',
+				'Admissão',
+				'Ambulatório',
+				'Documentação Médica',
+				'Médicos',
+				'Enfermagem',
+				'Bloco Operatório',
+				'Laboratórios',
+				'Radiologia',
+				'Farmácia',
+				'Medicamentos',
+				'Directório',
+				'Suporte Técnico',
+				'Admininistração do Sistema',
+				'Intranet Email',
+				'Internet Email',
+				'Funcionalidades Especiais',
+				'Iniciar Sessão'
+				);
+
+$VersionChgTarget='_top';
+$LDLanguage='Língua';
+$LDPlsWaitInit='Aguarde por favor. A executar procedimentos de inicialização....';
+
+$LDChange='Mudar';
+?>
