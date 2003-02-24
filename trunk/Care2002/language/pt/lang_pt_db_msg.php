fc80b401dcb43b8c2f61d2473ff70718ff891c33
alfrod <alfrod@8154e661-2335-47f1-af25-7e1110f3596a>
2003-02-24 11:29:48 +0000

    *** empty log message ***

---
+++ trunk/Care2002/language/pt/lang_pt_db_msg.php
@@ -0,0 +1,8 @@
+<?php
+$LDDbNoSave="Tentativa de guardar na Base de Dados falhou !";
+$LDDbNoRead="Tentativa de ler da Base de Dados falhou !";
+$LDDbNoLink="Tentativa de ligar à Base de Dados falhou !";
+$LDDbNoUpdate="Tentativa de actualizar a Base de Dados falhou !";
+$LDDbNoLastData="Os últimos dados não foram encontrados !";
+$LDDbNoDelete="Tentativa de apagar falhou !";
+?>
