b8ac6e3a68f63ef051f3501bbbacdde183ee08d0
roupec <roupec@8154e661-2335-47f1-af25-7e1110f3596a>
2003-03-17 18:28:41 +0000

    By Petr Roupec -- Czech Version

--- trunk/Care2002/language/cs-iso/lang_cs-iso_ambulatory.php
+++ trunk/Care2002/language/cs-iso/lang_cs-iso_ambulatory.php
@@ -3,7 +3,7 @@
 $LDDepartments='Ambulatní o¹etøení';
 $LDPendingRequest='Nevyøízený po¾adavek';
 $LDPendingRequestTxt='Pøíjem, Výpis, Tisk výsledkù a Hledání nevyøízených po¾adavkù';
-$LDGeneralSurgery='Obecné o¹tøení';
+$LDGeneralSurgery='Obecné o¹etøení';
 $LDInternalMed='Interní medicína';
 $LDEmergency='Urgentní ambulance';
 $LDSonography='Sonografie';
