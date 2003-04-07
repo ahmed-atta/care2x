49c71442e802c1506941ae4b76037c8dec6db1b9
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-04-07 16:59:36 +0000

    new method for appointment scheduling module

--- trunk/Care2002/include/care_api_classes/class_appointment.php
+++ trunk/Care2002/include/care_api_classes/class_appointment.php
@@ -9,8 +9,10 @@ require_once($root_path.'include/care_api_classes/class_core.php');
 class Appointment extends Core {
 
     var $tb='care_appointment';
+	var $tb_person='care_person';
 	var $pid;
 	var $result;
+	var $count;
 	var $tabfields=array('nr',
 									'pid',
 									'date',
@@ -56,7 +58,7 @@ class Appointment extends Core {
 				else return false;
 		}
 	}
-	function getPersonsAppointmentsObj($pid){
+	function getPersonsAppointmentsObj($pid=''){
 		global $db;
 		if(empty($pid)) return false;
 		if($this->result=$db->Execute("SELECT * FROM $this->tb WHERE pid=$pid ORDER BY date DESC")){
@@ -64,5 +66,28 @@ class Appointment extends Core {
 				else return false;
 		}
 	}
-			
+	function getAllByDateObj($y='',$m='',$d=''){
+		global $db;
+		if(empty($y)) $y=date('Y');
+		if(empty($m)) $y=date('m');
+		if(empty($d)) $y=date('d');
+		$sql="SELECT a.*,p.name_last,p.name_first,p.date_birth 
+				FROM $this->tb AS a LEFT JOIN $this->tb_person AS p ON a.pid=p.pid
+				WHERE a.date='$y-$m-$d' ORDER BY a.date DESC";
+		if($this->result=$db->Execute($sql)){
+			if($this->count=$this->result->RecordCount()) return $this->result;
+				else {return false;}
+		}else {echo $sql; return false;}
+	}
+	function cancelAppointment($nr='',$reason='',$by=''){	
+		if(empty($nr)) return false;
+		$buffer['history']="CONCAT(history,'Cancel: ".date('Y-m-d H:i:s')." : ".$by."\n')";
+		$buffer['appt_status']='cancelled';
+		$buffer['cancel_reason']=$reason;
+		$this->setDataArray($buffer);
+		$this->where=' nr='.$nr;
+		if($this->updateDataFromInternalArray($nr)) {
+			return true;
+		}else return false;
+	}
 }
