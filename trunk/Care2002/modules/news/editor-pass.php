8b26e583180500ac3e4e3150d1e378b25e518808
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-04-21 09:54:32 +0000

    patch for navigation bugs

--- trunk/Care2002/modules/news/editor-pass.php
+++ trunk/Care2002/modules/news/editor-pass.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 require_once('./roots.php');
 require_once($root_path.'include/inc_environment_global.php');
 
