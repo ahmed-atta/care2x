44ff6b1019797e426fca354b3f45efd9c69c8915
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-04-22 19:50:28 +0000

    added for legal notices and infos

---
+++ trunk/Care2002/language/de/lang_de_legal.php
@@ -0,0 +1,4 @@
+<?php
+$LDSickConfirm='Der angegebene Krankenkasse wird unverzüglich eine Bescheinigung über die Arbeitsunfähigkeit mit Angaben über die
+Diagnose sowie die voraussichtliche Dauer der Arbeitsunfähigkeit übersandt';
+?>
---
+++ trunk/Care2002/language/en/lang_en_legal.php
@@ -0,0 +1,4 @@
+<?php
+$LDSickConfirm='A copy of this confirmation of inability to work with the diagnoses and the estimated number of days of 
+inability will be transmitted immediately to the insurer given on the left.';
+?>
