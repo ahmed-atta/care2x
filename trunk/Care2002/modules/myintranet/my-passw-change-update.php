fad4eb5dba8fb4d9e3daefd9cf12f6cfd45152ee
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-06-13 17:09:17 +0000

    revised versions moved to separate module

---
+++ trunk/Care2002/modules/myintranet/my-passw-change-update.php
@@ -0,0 +1,46 @@
+<?php
+error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+require('./roots.php');
+require($root_path.'include/inc_environment_global.php');
+/**
+* CARE 2002 Integrated Hospital Information System beta 1.0.04 - 2003-03-31
+* GNU General Public License
+* Copyright 2002 Elpidio Latorilla
+* elpidio@latorilla.com
+*
+* See the file "copy_notice.txt" for the licence notice
+*/
+define('LANG_FILE','');
+define('NO_2LEVEL_CHK',1);
+require_once($root_path.'include/inc_front_chain_lang.php');
+				/* Establish db connection */
+if(!isset($db)||!$db) include($root_path.'include/inc_db_makelink.php');
+if($dblink_ok) 
+					{	
+								$m=date('m');
+								$y=date('Y');
+								$m=$m+3;
+								if($m>12)
+								{
+									$m=$m-12;
+									$y++;
+								} 
+								if($m<10) $m='0'.$m;
+								$xd="$y-$m-".date('d');
+						
+								$sql="UPDATE care_users SET	password='$n', expire_date='$xd'  WHERE login_id='$userid'";
+							
+								if($db->Execute($sql))
+								{ 
+								//echo "ok";
+									header("location:my-passw-change.php?sid=$sid&lang=$lang&mode=pwchg");
+									exit;
+								}
+								else
+								{
+								   echo "$sql<br>$LDDbNoSave";
+								 }
+						}
+header ("location:my-passw-change.php?sid=$sid&lang=$lang&userid=$userid&keyword=$keyword");
+exit;
+?>
