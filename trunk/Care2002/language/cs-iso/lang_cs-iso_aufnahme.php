861352f053caa27f19e01a46a7a8ee6ebb20ac6e
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-06-19 21:34:12 +0000

    additional items

--- trunk/Care2002/language/cs-iso/lang_cs-iso_aufnahme.php
+++ trunk/Care2002/language/cs-iso/lang_cs-iso_aufnahme.php
@@ -334,4 +334,13 @@ $LDRegisterNewPerson='Register a new person';
 /* 2003-05-17 EL */
 $LDEnterPersonSearchKey='Enter search keyword: e.g. PID, first name, or family name';
 $LDPersonData='Personal data';
+/* 2003-05-26 EL*/
+$LDDiagnoses='Diagnoses';
+$LDCreateNewForm='Create a form for';
+$LDOtherRecords='Other records';
+/*2003-06-17 El*/
+$LDFullForm='Full form';
+$LDAllContents='All contents';
+$LDAllText='Dynamic contents only';
+$LDDataOnly='Encounter relevant data only';
 ?>
---
+++ trunk/Care2002/language/en/lang_en_prompt.php
@@ -0,0 +1,7 @@
+<?php
+$LDPlsEnterEndDate='Please enter end date';
+$LDPlsEnterStartDate='Please enter start date';
+$LDPlsEnterConfirmDate='Please enter confirm date';
+$LDPlsEnterDiagnosis='Please enter the diagnosis';
+$LDPlsSelectStyle='Please select a style';
+?>
