2ff74c35fb194e902ea7a18540391bf82c39e28b
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-06-27 23:10:19 +0000

    added methods for saving uploaded image

--- trunk/Care2002/include/care_api_classes/class_image.php
+++ trunk/Care2002/include/care_api_classes/class_image.php
@@ -7,7 +7,10 @@ require_once($root_path.'include/care_api_classes/class_core.php');
 
 class Image extends Core{
 
-	var $tb_image='care_encounter_image';
+	var $tb_image='care_encounter_image'; # table for encounter's images
+	var $mimefilter='jpg,gif,png,bmp'; # allowed mime types
+	var $ul_img_ext=''; # uploaded img file's extension/mime type
+	var $def_root_path='fotos/encounter/'; # default root path for images
 	
 	var $fld_image=array('nr',
 											'encounter_nr',
@@ -22,6 +25,9 @@ class Image extends Core{
 											'modify_time',
 											'create_id',
 											'create_time');
+	/**
+	* Constructor
+	*/
 	function Image(){
 		$this->coretable=$this->tb_image;
 		$this->ref_array=$this->fld_image;
@@ -52,7 +58,6 @@ class Image extends Core{
 			$this->data_array['notes']="CONCAT(notes,'[[".date('Y-m-d')."]]\n [[".$this->data_array['modify_id']."]]\n ".$this->data_array['notes']."\n')";
 			return $this->updateDataFromInternalArray($nr);
 		}
-		
 	}
 	/**
 	* Gets the image' s data from the table
@@ -88,6 +93,12 @@ class Image extends Core{
 		    } else { return false;}
 		} else { return false;}
 	}
+	/**
+	* saveImageData() saves all data pertinent to the image
+	* public
+	* @param $data (array) = the data in array
+	* return last insert ID / false
+	*/
 	function saveImageData(&$data){
 	    global $db;
 		$nr;
@@ -103,6 +114,13 @@ class Image extends Core{
 			return false;
 		}
 	}
+	/**
+	* getAllImageData() gets all data from the care_encounter_image table which are pertinent to the encounter
+	* public
+	* @param $enc_nr (int) = encounter number
+	* @param $shot_date = the shot date of the image
+	* return ADODB record object
+	*/
 	function getAllImageData($enc_nr,$shot_date=0){
 		global $db;
 		if($shot_date){
@@ -113,8 +131,45 @@ class Image extends Core{
 		if($this->result=$db->Execute($this->sql)) {
 		    if($this->result->RecordCount()) {
 				return $this->result;
-		    } else { return false;}
-		} else { return false;}
+		    }else{ return false;}
+		}else{ return false;}
+	}
+	/**
+	* isValidUploadedImage() checks whether the uploaded image file is valid or not
+	* public
+	* the file's extension/mime type will be extracted and saved in the objects ul_img_ext variable
+	* @param $img (array) (pass by reference) the image pointer, usually $HTTP_POST_FILES
+	* @param $mfilter (str)  optional filter mime types, if empty, default will be used
+	* return true/false
+	*/
+	function isValidUploadedImage(&$img,$mfilter=''){
+		if(empty($mfilter)) $mfilter=$this->mimefilter;
+		if(is_uploaded_file($img['tmp_name']) && $img['size']){
+			$this->ul_img_ext=substr($img['name'],strrpos($img['name'],'.')+1);
+			if(stristr($mfilter,$this->ul_img_ext)){
+				return true;
+		    }else{ return false;}
+		}else{ return false;}
+	}
+	/**
+	* UploadedImageMimeType() returns the mime type of the uploaded image file
+	* public
+	* return the mime type/file extension
+	*/
+	function UploadedImageMimeType(){
+		return $this->ul_img_ext;
+	}
+	/**
+	* saveUploadedImage() saves the uploaded image with the given filename into a given path
+	* public
+	* @param $img (array) (pass by reference) the image file in array (usually $HTTP_POST_FILES)
+	* @param $fpath (string) the complete path for the stored image. If empty, the default will be used
+	* @param $fname (string) thefilename of the stored image. If empty, the original file name will be used
+	*/
+	function saveUploadedImage(&$img,$fpath='',$fname=''){
+		if(empty($fpath)) $fpath=$this->def_root_path;
+		if(empty($fname)) return copy($img['tmp_name'],$fpath.$img['name']);
+	 		else return copy($img['tmp_name'],$fpath.$fname);
 	}
 }
 ?>
