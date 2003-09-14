f95301115e6707e360a0e4641ef105f9592a3e75
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-09-14 18:06:25 +0000

    v1007 data for new care_insurance_firm table structure

--- trunk/Care2002/install/sql/test_data/insurance_firms.sql
+++ trunk/Care2002/install/sql/test_data/insurance_firms.sql
@@ -1,69 +1,59 @@
-# phpMyAdmin MySQL-Dump
-# version 2.3.2
-# http://www.phpmyadmin.net/ (download page)
-#
-# Host: localhost
-# Generation Time: Apr 21, 2003 at 12:31 AM
-# Server version: 3.22.34
-# PHP Version: 4.0.4pl1
-# Database : xxxxxx
-
 #
 # Dumping data for table care_insurance_firm
 #
 
-INSERT INTO care_insurance_firm VALUES ('de', 1, 'A&M Aachener und Münchener', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202205, '', 20021228202205);
-INSERT INTO care_insurance_firm VALUES ('de', 2, 'Adler', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202232, '', 20021228202232);
-INSERT INTO care_insurance_firm VALUES ('de', 3, 'Advance Bank', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202310, '', 20021228202310);
-INSERT INTO care_insurance_firm VALUES ('de', 4, 'Aegon', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202331, '', 20021228202331);
-INSERT INTO care_insurance_firm VALUES ('de', 5, 'Aggripina', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202354, '', 20021228202354);
-INSERT INTO care_insurance_firm VALUES ('de', 6, 'Albingia', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202414, '', 20021228202414);
-INSERT INTO care_insurance_firm VALUES ('de', 7, 'Allgemeine Rentenanstalt', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202643);
-INSERT INTO care_insurance_firm VALUES ('de', 8, 'Allianz', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202727);
-INSERT INTO care_insurance_firm VALUES ('de', 9, 'Alte Leipziger', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202752);
-INSERT INTO care_insurance_firm VALUES ('de', 10, 'Alte Oldenburger', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202917);
-INSERT INTO care_insurance_firm VALUES ('de', 11, 'AOK', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202938);
-INSERT INTO care_insurance_firm VALUES ('de', 12, 'Arag', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202959);
-INSERT INTO care_insurance_firm VALUES ('de', 13, 'Aspecta', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203025);
-INSERT INTO care_insurance_firm VALUES ('de', 14, 'Asstel', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203223);
-INSERT INTO care_insurance_firm VALUES ('de', 15, 'Atlanticlux', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203425);
-INSERT INTO care_insurance_firm VALUES ('de', 16, 'Auxilia', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203518);
-INSERT INTO care_insurance_firm VALUES ('de', 17, 'Axa Colonia', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203551);
-INSERT INTO care_insurance_firm VALUES ('de', 18, 'Barmenia', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203615);
-INSERT INTO care_insurance_firm VALUES ('de', 19, 'Basler', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203820);
-INSERT INTO care_insurance_firm VALUES ('de', 20, 'Bayern Versicherung', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203947);
-INSERT INTO care_insurance_firm VALUES ('de', 21, 'Bayrische Beamten LV', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204051);
-INSERT INTO care_insurance_firm VALUES ('de', 22, 'BBV', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204155);
-INSERT INTO care_insurance_firm VALUES ('de', 23, 'Berlinische', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204254);
-INSERT INTO care_insurance_firm VALUES ('de', 24, 'Berlin-Kölnische', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204458);
-INSERT INTO care_insurance_firm VALUES ('de', 25, 'BHW', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204514);
-INSERT INTO care_insurance_firm VALUES ('de', 26, 'Central Krankenversicherung', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204600);
-INSERT INTO care_insurance_firm VALUES ('de', 27, 'Concordia', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204630);
-INSERT INTO care_insurance_firm VALUES ('de', 28, 'Condor', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204658);
-INSERT INTO care_insurance_firm VALUES ('de', 29, 'Continentale', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204748);
-INSERT INTO care_insurance_firm VALUES ('de', 30, 'CosmosDirekt', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204919);
-INSERT INTO care_insurance_firm VALUES ('de', 31, 'Danv (HM)', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204954);
-INSERT INTO care_insurance_firm VALUES ('de', 32, 'DBV Winterthur', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205017);
-INSERT INTO care_insurance_firm VALUES ('de', 33, 'Debeka', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205105);
-INSERT INTO care_insurance_firm VALUES ('de', 34, 'Delfin', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205153);
-INSERT INTO care_insurance_firm VALUES ('de', 35, 'Delta', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205239);
-INSERT INTO care_insurance_firm VALUES ('de', 36, 'DEURAG', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205255);
-INSERT INTO care_insurance_firm VALUES ('de', 37, 'DA DIREKT', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205332);
-INSERT INTO care_insurance_firm VALUES ('de', 38, 'Deutsche Ärzteversicherung', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205415);
-INSERT INTO care_insurance_firm VALUES ('de', 39, 'Deutscher Herold', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205455);
-INSERT INTO care_insurance_firm VALUES ('de', 40, 'Deutscher Ring', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205513);
-INSERT INTO care_insurance_firm VALUES ('de', 41, 'DEVK', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205550);
-INSERT INTO care_insurance_firm VALUES ('de', 42, 'DEVK Allgemeine', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205613);
-INSERT INTO care_insurance_firm VALUES ('de', 43, 'Dialog', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205631);
-INSERT INTO care_insurance_firm VALUES ('de', 44, 'Die Alternative', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205657);
-INSERT INTO care_insurance_firm VALUES ('de', 45, 'DKV', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205728);
-INSERT INTO care_insurance_firm VALUES ('de', 46, 'Europa', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205815);
-INSERT INTO care_insurance_firm VALUES ('de', 47, 'Generali', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205834);
-INSERT INTO care_insurance_firm VALUES ('de', 48, 'Generali Lloyd', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205856);
-INSERT INTO care_insurance_firm VALUES ('de', 49, 'Gerling Konzern Allgemeine', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205932);
-INSERT INTO care_insurance_firm VALUES ('de', 50, 'Gerling E+L', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205953);
-INSERT INTO care_insurance_firm VALUES ('de', 51, 'Gerling Leben', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210012);
-INSERT INTO care_insurance_firm VALUES ('de', 52, 'Globale Kranken', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210104);
-INSERT INTO care_insurance_firm VALUES ('de', 53, 'Gothaer', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210120);
-INSERT INTO care_insurance_firm VALUES ('de', 54, 'Gutingia', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210221);
-INSERT INTO care_insurance_firm VALUES ('de', 55, 'Haftpflichtkasse Darmstadt', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210335);
+INSERT INTO care_insurance_firm VALUES ('1', 'A&M Aachener und Münchener', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202205, '', 20021228202205);
+INSERT INTO care_insurance_firm VALUES ('2', 'Adler', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202232, '', 20021228202232);
+INSERT INTO care_insurance_firm VALUES ('3', 'Advance Bank', '', 'Leinfelden', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030617011357, '', 20021228202310);
+INSERT INTO care_insurance_firm VALUES ('4', 'Aegon', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202331, '', 20021228202331);
+INSERT INTO care_insurance_firm VALUES ('5', 'Aggripina', '', 'Esslingen', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030617011316, '', 20021228202354);
+INSERT INTO care_insurance_firm VALUES ('6', 'Albingia', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20021228202414, '', 20021228202414);
+INSERT INTO care_insurance_firm VALUES ('7', 'Allgemeine Rentenanstalt', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202643);
+INSERT INTO care_insurance_firm VALUES ('8', 'Allianz', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202727);
+INSERT INTO care_insurance_firm VALUES ('9', 'Alte Leipziger', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202752);
+INSERT INTO care_insurance_firm VALUES ('10', 'Alte Oldenburger', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202917);
+INSERT INTO care_insurance_firm VALUES ('11', 'AOK', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202938);
+INSERT INTO care_insurance_firm VALUES ('12', 'Arag', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228202959);
+INSERT INTO care_insurance_firm VALUES ('13', 'Aspecta', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203025);
+INSERT INTO care_insurance_firm VALUES ('14', 'Asstel', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203223);
+INSERT INTO care_insurance_firm VALUES ('15', 'Atlanticlux', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203425);
+INSERT INTO care_insurance_firm VALUES ('16', 'Auxilia', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203518);
+INSERT INTO care_insurance_firm VALUES ('17', 'Axa Colonia', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203551);
+INSERT INTO care_insurance_firm VALUES ('18', 'Barmenia', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203615);
+INSERT INTO care_insurance_firm VALUES ('19', 'Basler', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203820);
+INSERT INTO care_insurance_firm VALUES ('20', 'Bayern Versicherung', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228203947);
+INSERT INTO care_insurance_firm VALUES ('21', 'Bayrische Beamten LV', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204051);
+INSERT INTO care_insurance_firm VALUES ('22', 'BBV', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204155);
+INSERT INTO care_insurance_firm VALUES ('23', 'Berlinische', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204254);
+INSERT INTO care_insurance_firm VALUES ('24', 'Berlin-Kölnische', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204458);
+INSERT INTO care_insurance_firm VALUES ('25', 'BHW', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204514);
+INSERT INTO care_insurance_firm VALUES ('26', 'Central Krankenversicherung', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204600);
+INSERT INTO care_insurance_firm VALUES ('27', 'Concordia', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204630);
+INSERT INTO care_insurance_firm VALUES ('28', 'Condor', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204658);
+INSERT INTO care_insurance_firm VALUES ('29', 'Continentale', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204748);
+INSERT INTO care_insurance_firm VALUES ('30', 'CosmosDirekt', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204919);
+INSERT INTO care_insurance_firm VALUES ('31', 'Danv (HM)', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030437, '', 20021228204954);
+INSERT INTO care_insurance_firm VALUES ('32', 'DBV Winterthur', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205017);
+INSERT INTO care_insurance_firm VALUES ('33', 'Debeka', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205105);
+INSERT INTO care_insurance_firm VALUES ('34', 'Delfin', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205153);
+INSERT INTO care_insurance_firm VALUES ('35', 'Delta', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205239);
+INSERT INTO care_insurance_firm VALUES ('36', 'DEURAG', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205255);
+INSERT INTO care_insurance_firm VALUES ('37', 'DA DIREKT', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205332);
+INSERT INTO care_insurance_firm VALUES ('38', 'Deutsche Ärzteversicherung', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205415);
+INSERT INTO care_insurance_firm VALUES ('39', 'Deutscher Herold', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205455);
+INSERT INTO care_insurance_firm VALUES ('40', 'Deutscher Ring', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205513);
+INSERT INTO care_insurance_firm VALUES ('41', 'DEVK', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205550);
+INSERT INTO care_insurance_firm VALUES ('42', 'DEVK Allgemeine', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205613);
+INSERT INTO care_insurance_firm VALUES ('43', 'Dialog', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205631);
+INSERT INTO care_insurance_firm VALUES ('44', 'Die Alternative', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205657);
+INSERT INTO care_insurance_firm VALUES ('45', 'DKV', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205728);
+INSERT INTO care_insurance_firm VALUES ('46', 'Europa', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205815);
+INSERT INTO care_insurance_firm VALUES ('47', 'Generali', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205834);
+INSERT INTO care_insurance_firm VALUES ('48', 'Generali Lloyd', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205856);
+INSERT INTO care_insurance_firm VALUES ('49', 'Gerling Konzern Allgemeine', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205932);
+INSERT INTO care_insurance_firm VALUES ('50', 'Gerling E+L', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228205953);
+INSERT INTO care_insurance_firm VALUES ('51', 'Gerling Leben', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210012);
+INSERT INTO care_insurance_firm VALUES ('52', 'Globale Kranken', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210104);
+INSERT INTO care_insurance_firm VALUES ('53', 'Gothaer', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210120);
+INSERT INTO care_insurance_firm VALUES ('54', 'Gutingia', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210221);
+INSERT INTO care_insurance_firm VALUES ('55', 'Haftpflichtkasse Darmstadt', '', '', 0, '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', 20030216030438, '', 20021228210335);   
