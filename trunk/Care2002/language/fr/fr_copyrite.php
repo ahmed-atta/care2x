a02eb8a4c30bc1cb9657782948f01436649c55bf
matejh <matejh@8154e661-2335-47f1-af25-7e1110f3596a>
2003-09-20 00:13:35 +0000

    *** empty log message ***

--- trunk/Care2002/language/fr/fr_copyrite.php
+++ trunk/Care2002/language/fr/fr_copyrite.php
@@ -3,7 +3,7 @@ require($root_path.'include/inc_credits_openwin.php');
 ?>
 <FONT    SIZE=1  FACE="Arial" color=gray>
  <a href="http://www.care2x.com" target=_new>CARE 2002 beta 1.0.07</a> :: <a href="<?php echo $root_path ?>legal_gnu_gpl.htm" target=_new> License</a> :: 
- <a href=mailto:info@care2x.com>Contact</a> <font size=1 face="arial"> :: <a href="<?php echo $root_path ?>language/fr/fr_privacy.htm" target="pp">Notre politique de discrétion</a> :: 
+ <a href=mailto:info@care2x.com>Contact</a> <font size=1 face="arial"> :: <a href="<?php echo $root_path ?>language/fr/fr_privacy.htm" target="pp">Notre politique de discr&eacute;tion</a> :: 
  <a href="<?php echo $root_path ?>docs/show_legal.php?lang=<?php echo $lang ?>" target="lgl"> Legal </a> ::</font>
- <a href="javascript:openCreditsWindow()"> Crédits </a> ::.<br></font>
+ <a href="javascript:openCreditsWindow()"> Cr&eacute;dits </a> ::.<br></font>
 </FONT>
--- trunk/Care2002/language/fr/lang_fr_billing.php
+++ trunk/Care2002/language/fr/lang_fr_billing.php
@@ -1,23 +1,23 @@
 <?php
 /* 2003-06-21 El */
-$TXT['CreateHospitalServiceItem']='Créer un article du service de l\'hôpital';
-$TXT['CreateLabTestItem']='Créer un article du test de laboratoire';
-$TXT['EditHospitalServiceItems']='Editer les articles du service de l\'hôpital';
+$TXT['CreateHospitalServiceItem']='Cr&eacute;er un article du service de l\'h&ocirc;pital';
+$TXT['CreateLabTestItem']='Cr&eacute;er un article du test de laboratoire';
+$TXT['EditHospitalServiceItems']='Editer les articles du service de l\'h&ocirc;pital';
 $TXT['EditLabTestItems']='Editer les articles du test de laboratoire';
 $TXT['SearchPatient']='Rechercher un patient';
 $TXT['Billing']='eComBill';
 
-$TXT['EnterNameHS']='Entrez le nom d\'un service de l\'hôpital';
-$TXT['EnterServiceCode']='Entrez le code/numéro du service';
-$TXT['EnterPriceUnit']='Entrez le prix par unité';
+$TXT['EnterNameHS']='Entrez le nom d\'un service de l\'h&ocirc;pital';
+$TXT['EnterServiceCode']='Entrez le code/num&eacute;ro du service';
+$TXT['EnterPriceUnit']='Entrez le prix par unit&eacute;';
 $TXT['EnterDiscount']='Enter Discount allowed on this service';
-$TXT['EnterValuePrice']='Entrez la valeur numérique du prix';
-$TXT['EnterValueDiscount']='Entrez la valeur numérique de la solde';
-$TXT['HospitalServiceItem']='Article du service de l\'hôpital';
+$TXT['EnterValuePrice']='Entrez la valeur num&eacute;rique du prix';
+$TXT['EnterValueDiscount']='Entrez la valeur num&eacute;rique de la solde';
+$TXT['HospitalServiceItem']='Article du service de l\'h&ocirc;pital';
 $TXT['PlsEnterFF']='Entrez le suivantPlease enter the following, s\'il vous plaît';
-$TXT['NameHS']='Nom du service de l\'hôpital';
+$TXT['NameHS']='Nom du service de l\'h&ocirc;pital';
 $TXT['TestCode']='Service/Code test/no.';
-$TXT['PriceUnit']='Prix par unité';
+$TXT['PriceUnit']='Prix par unit&eacute;';
 $TXT['EnterValueDiscount']='Solde(%)';
 $TXT['NameLT']='Nom du test laboratoire';
 $TXT['LabTestItem']='Article du test laboratoire';
@@ -25,7 +25,7 @@ $TXT['NameLT']='Nom du test laboratoire';
 $TXT['Discount']='Solde*';
 $TXT['Code']='Code';
 $TXT['Name']='Nom';
-$TXT['CostPerUnit']='Coût par unité';
-$TXT['HospitalServiceItems']='Articles du service de l\'hôpital';
+$TXT['CostPerUnit']='Co&ucirc;t par unit&eacute;';
+$TXT['HospitalServiceItems']='Articles du service de l\'h&ocirc;pital';
 $TXT['LabTestItems']='Articles du test laboratoire';
 ?>
