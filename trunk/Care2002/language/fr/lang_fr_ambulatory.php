736d0b0e2e1db757ef79756041d4bd3ac1e08a8e
clinique-alizes <clinique-alizes@8154e661-2335-47f1-af25-7e1110f3596a>
2003-11-27 16:02:33 +0000

    MAJ Jean-Philippe

--- trunk/Care2002/language/fr/lang_fr_ambulatory.php
+++ trunk/Care2002/language/fr/lang_fr_ambulatory.php
@@ -3,19 +3,19 @@
 $LDDepartments='Ambulatoire';
 $LDPendingRequest='Demande en attente';
 $LDPendingRequestTxt='Réception, Liste, Imprimer, Entrer découvertes & résultats';
-$LDGeneralSurgery='Ambulatoire général';
+$LDGeneralSurgery='Ambulatoire générale';
 $LDInternalMed='Ambulatoire médecine interne';
 $LDEmergency='Urgence ambulatoire';
 $LDSonography='Sonographie';
-$LDNuclearMed='Diagnostiques Nucléaires';
+$LDNuclearMed='Diagnostics Nucléaires';
 $LDAdmission='Admission';
 $LDAdmissionTxt='Admission/Renvoi de l\'ambulatoire, recherche, archive';
 $LDNews='Nouvelles';
 $LDNewsTxt='Nouvelles spécifiques de la clinique, mémo, info, etc.';
 # 2003-08-01 EL
 $LDAppointments='Rendez-vous';
-$LDAppointmentsTxt='Rendez-vous patient pour ce département';
-$LDPatientWaitingList='Liste des patiens en attente';
+$LDAppointmentsTxt='Rendez-vous des patients pour ce département';
+$LDPatientWaitingList='Liste des patients en attente';
 $LDPWListTxt='Patient admis à la clinique aujourd'hui';
 $LDOutpatientClinic='Externat';
 $LDGeneralAmbulatory='Ambulatoire Générale';
