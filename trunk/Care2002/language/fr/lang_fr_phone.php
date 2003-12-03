bfb4cd81373a84933bd79f760d6e9c5b152592c9
clinique-alizes <clinique-alizes@8154e661-2335-47f1-af25-7e1110f3596a>
2003-12-03 08:41:38 +0000

    MAJ Jean-Philippe

--- trunk/Care2002/language/fr/lang_fr_phone.php
+++ trunk/Care2002/language/fr/lang_fr_phone.php
@@ -1,97 +1,97 @@
 <?php
 $fieldname=array(
-							"Family name",
-							"Name",
-							"Phone 1",
-							"Phone 2",
-							"Phone 3",
+							"Nom de famille",
+							"Nom",
+							"Téléphone 1",
+							"Téléphone 2",
+							"Téléphone 3",
 							"Beeper 1",
 							"Beeper 2",
-							"Private 1",
-							"Private 2"
+							"Privé 1",
+							"Privé 2"
 							);
 							
-$LDPhoneDir="Phone directory";
-$LDSearch="Search";
-$LDDir="Directory";
-$LDNewData="New data";
-$LDSEARCH="SEARCH";
+$LDPhoneDir="Répertoire téléphonique";
+$LDSearch="Recherche";
+$LDDir="Répertoire";
+$LDNewData="Nouvelle donnée";
+$LDSEARCH="Recherche";
 
 /**************** note the " ~nr~ " must not be erased it will be replaced by the script with the number of search results ******/
-$LDPhoneFound="Search found <font color=red><b> ~nr~ </b></font> relevant data.";
+$LDPhoneFound="La recherche a trouvée <font color=red><b> ~nr~ </b></font> donnée(s) pertinente(s).";
 
-$LDKeywordPrompt="Enter search keyword. For example: a name, or a department, or a shortform, etc.";
-$LDShowDir="Show the complete directory";
-$LDNewEntry="Enter new phone or beeper data";
+$LDKeywordPrompt="Entrez un mot clef. Par exemple : un nom, un département ou un shortform, etc.";
+$LDShowDir="Afficher le répertoire complet";
+$LDNewEntry="ENtrez un nouveau numéro de téléphone ou beeper";
 
 $LDExtFields=array(
 								"Nr.",
-								"Title",
-								"Family name",
-								"Name",
+								"Titre",
+								"Nom de famille",
+								"Nom",
 								"Profession",
 								"Dept.1",
 								"Dept.2",
 				 				"Tel.1",
 								"Tel.2",
 								"Tel.3",
-								"Private 1",
-								"Private 2",
+								"Privé 1",
+								"Privé 2",
 								"Beeper 1",
 								"Beeper 2",
-								"Room Nr.",
+								"Chambre Nr.",
 								"Date",
-								"Time",
-								"Encoder");
+								"Heure",
+								"Encodage");
 $LDEditFields=array(
 								"Nr.",
-								"Title",
-								"Family name",
-								"Name",
+								"Titre",
+								"Nom de famille",
+								"Nom",
 								"Profession",
 								"Department 1",
 								"Department 2",
-				 				"Telephone (internal) 1",
-								"Telephone (internal) 2",
-								"Telephone (internal) 3",
-								"Private (external) 1",
-								"Private (external) 2",
+				 				"Telephone (interne) 1",
+								"Telephone (interne) 2",
+								"Telephone (interne) 3",
+								"Privé (externe) 1",
+								"Privé (externe) 2",
 								"Beeper 1",
 								"Beeper 2",
-								"Room Nr.",
+								"Chambre Nr.",
 								"Date",
-								"Time",
-								"Encoder");
+								"Heure",
+								"Encodage");
 $LDOK=" OK ";
-$LDCancel="Cancel";
-$LDSave="Save";
-$LDEdit="Edit";
-$LDDelete="Delete";
-$LDReset="Reset";
-$LDNewPhoneEntry="Enter new phone information";
-$LDShow="Show";
-$LDActualDir="Current contents of directory";
-$LDMoreInfo="More directory info";
-$LDMaxItem="Total number of directory entries";
-$LDUpdateOk="The information was updated succesfully";
-$LDRows="Rows";
+$LDCancel="Annuler";
+$LDSave="Sauvegarder";
+$LDEdit="Editer";
+$LDDelete="Effacer";
+$LDReset="Réinitialiser";
+$LDNewPhoneEntry="Entrez de nouvelles coordonnées téléphoniques";
+$LDShow="Afficher";
+$LDActualDir="Enregistrement actuel du répertoire";
+$LDMoreInfo="Plus d\'info sur le répertoire";
+$LDMaxItem="Nombre total d\'enregistrement du répertoire";
+$LDUpdateOk="Les informations ont été mise à jour avec sucès";
+$LDRows="Lignes";
 
-$LDGoodMorning="Good Morning!";
-$LDGoodDay="Hi! Nice to see you!";
-$LDGoodEvening="Good Evening";
+$LDGoodMorning="Bonjour!";
+$LDGoodDay="Ho! Ravi de vous voir!";
+$LDGoodEvening="Bonsoir";
 
-$LDShowActualDir="Show current directory entries";
-$LDYesDelete="Yes, delete";
-$LDNoCancel="NO! CANCEL";
-$LDDeleteEntry="Delete entry";
-$LDNoData="You did not enter anything. Please enter the data before clicking the \"Save\" button.";
-$LDReallyDelete="Do you really want to erase the following entry?";
+$LDShowActualDir="Afficher les enregistrements actuels du répertoire";
+$LDYesDelete="Oui, effacer";
+$LDNoCancel="NON! ANNULER";
+$LDDeleteEntry="Effacer l\'entrée";
+$LDNoData="Vous n\'avez rien saisi. Svp, saisissez d\'abord les données avant de cliquer sur le bouton \"Sauvegarder\".";
+$LDReallyDelete="Voulez-vous vraiement effacer les entrées suivantes?";
 
-$LDHowManage="How to manage the directory";
-$LDHow2OpenDir="How to open the entire directory";
-$LDHowEnter="How to enter new phone information";
-$LDHow2SearchPhone="How to search for a phone number";
-$LDUpdate="Update data";
+$LDHowManage="Comment gèrer le répertoire";
+$LDHow2OpenDir="Comment ouvrir le répertoire en entier";
+$LDHowEnter="Comment entrer de nouvelles coordonnées téléphoniques";
+$LDHow2SearchPhone="Comment rechercher des coordonées téléphoniques";
+$LDUpdate="Mettre à jour les données";
 
-$LDDirData="Edit directory entry nr. <font color=\"red\">~nr~</font>";
+$LDDirData="Editer les coordonnées nr. <font color=\"red\">~nr~</font>";
 ?>
