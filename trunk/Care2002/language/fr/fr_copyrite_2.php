9a20f727e8feecbe5fabaf5f81d5c826772e85b6
matejh <matejh@8154e661-2335-47f1-af25-7e1110f3596a>
2003-12-03 19:25:37 +0000

    *** empty log message ***

--- trunk/Care2002/language/fr/fr_copyrite_2.php
+++ trunk/Care2002/language/fr/fr_copyrite_2.php
@@ -2,13 +2,13 @@
 
 require($root_path.'include/inc_credits_openwin.php');
 ?>
- <font size="1" face="Arial" color="gray"> La version française est développée et managée par Matej Hausenblas.
+ <font size="1" face="Arial" color="gray"> La version française est développée et managée par Mat&#283;j Hausenblas.
 Veuillez bien diriger toutes les questions concernant la version française 
 vers lui. Version Beta 1.0.08 </font> <font size="1" face="Arial" color="gray">CARE 2002 est écrite et
 distribuée librement sous la licence GNU General Public License. Pour
 plus de détail sur cette licence, allez voir  <a
  href="../legal_gnu_gpl.htm" target="_new">la copie de la licence</a> (en
-anglais).   Pour plus d'investigation, veuillez-vous contacter 
+anglais).   Pour plus d'investigation, veuillez contacter 
 <a href="mailto:info@care2x.com">info@care2x.com</a>. </font></p>
  
 <p> <font size="1" face="Arial" color="gray"><font size="1" face="arial">
