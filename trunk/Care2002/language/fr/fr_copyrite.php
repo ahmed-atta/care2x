b3cf4d13b33a1d03def0283b8597829c0a9c9a8c
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2003-12-19 17:20:35 +0000

    pre-deployment 1.0.10

--- trunk/Care2002/language/fr/fr_copyrite.php
+++ trunk/Care2002/language/fr/fr_copyrite.php
@@ -2,8 +2,8 @@
 require($root_path.'include/inc_credits_openwin.php');
 ?>
 <FONT    SIZE=1  FACE="Arial" color=gray>
- <a href="http://www.care2x.com" target=_new>CARE 2002 beta 1.0.09</a> :: <a href="<?php echo $root_path ?>legal_gnu_gpl.htm" target=_new> License</a> :: 
+ <a href="http://www.care2x.com" target=_new>CARE 2X pre-deployment 1.0.10</a> :: <a href="<?php echo $root_path ?>legal_gnu_gpl.htm" target=_new> License</a> :: 
  <a href=mailto:info@care2x.com>Contact</a> <font size=1 face="arial"> :: <a href="<?php echo $root_path ?>language/fr/fr_privacy.htm" target="pp">Notre politique de discrétion</a> :: 
  <a href="<?php echo $root_path ?>docs/show_legal.php?lang=<?php echo $lang ?>" target="lgl"> Legal </a> ::</font>
- <a href="javascript:openCreditsWindow()"> Remerciement </a> ::.<br></font>
+ <a href="javascript:openCreditsWindow()"> Crédits </a> ::.<br></font>
 </FONT>
