b4279992dd446e0021909747c3fd435ab0630259
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2004-02-07 21:12:31 +0000

    Patch in the link to patient  label generator

--- trunk/Care2002/modules/laboratory/labor_test_request_printpop.php
+++ trunk/Care2002/modules/laboratory/labor_test_request_printpop.php
@@ -233,12 +233,7 @@ if($show_print_button) echo '<a href="javascript:window.print()"><img '.createLD
 /* Load the form for printing out */
 if($subtarget=='chemlabor' || $subtarget=='baclabor')
 {
-    if($lang=='ar'||$lang=='fa'){// Modified on ( 22/01/2004) By Walid Fathalla
-	echo '<img src="'.$root_path.'main/imgcreator/barcode_label_single_large_ar.php?sid='.$sid.'&lang='.$lang.'&fen='.$full_en.'&en='.$pn.'&batch_nr='.$batch_nr.'&child_img=1&subtarget='.$subtarget.'" >';
-	}else{
-	echo '<img src="'.$root_path.'main/imgcreator/barcode_label_single_large.php?sid='.$sid.'&lang='.$lang.'&fen='.$full_en.'&en='.$pn.'&batch_nr='.$batch_nr.'&child_img=1&subtarget='.$subtarget.'" >';
-	}    
-	//echo '<img src="'.$root_path.'main/imgcreator/barcode_label_single_large.php?sid='.$sid.'&lang='.$lang.'&fen='.$full_en.'&en='.$pn.'&batch_nr='.$batch_nr.'&child_img=1&subtarget='.$subtarget.'" >';
+    echo '<img src="'.$root_path.'main/imgcreator/barcode_label_single_large.php?sid='.$sid.'&lang='.$lang.'&fen='.$full_en.'&en='.$pn.'&batch_nr='.$batch_nr.'&child_img=1&subtarget='.$subtarget.'" >';
 }
 else
 {
