6cf41aff1853af770edbd448e594407b3b2cb8d2
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2004-02-07 20:38:47 +0000

    Walid Fathalla - modifications for arabic TTF rendering

--- trunk/Care2002/include/inc_ttf_check.php
+++ trunk/Care2002/include/inc_ttf_check.php
@@ -6,19 +6,28 @@
 # ttf font rendering. By default, ttf rendering is disabled due to inconsequent results from different php versions.
 
 $ttf_render=FALSE;	
+$ttf_render=TRUE;
+
+# If the language is arabic or farsi, force TTF font to true 
+if($lang=='ar'||$lang=='fa') $ttf_render=TRUE;
+
+# Set the font type here
+
+$ttf_fonttype='arial.ttf';
 
 if($ttf_render){
+
 	$font_path=$root_path.'main/imgcreator/';
 	$ttf_ok=FALSE;
 	# Check if TTF text possible
 	if(function_exists(ImageTTFText)){
 		# Workaround to avoid upper/lower case error
-		if(file_exists($font_path.'arial.ttf')){
+		if(file_exists($font_path.$ttf_fonttype)){
 			$ttf_ok=TRUE;
-			$arial=$font_path.'arial.ttf';
-		}elseif(file_exists($font_path.'ARIAL.TTF')){
+			$arial=$font_path.$ttf_fonttype;
+		}elseif(file_exists($font_path.strtoupper($ttf_fonttype))){
 			$ttf_ok=TRUE;
-			$arial=$font_path.'ARIAL.TTF';
+			$arial=$font_path.strtoupper($ttf_fonttype);
 		}
 	}
 }else{
