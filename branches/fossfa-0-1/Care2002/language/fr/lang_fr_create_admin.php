6062e44987c3ac1f1c5f79dff2b8ac4de01d70c1
nobody <nobody@8154e661-2335-47f1-af25-7e1110f3596a>
2004-03-30 07:02:45 +0000

    This commit was manufactured by cvs2svn to create branch 'fossfa-0-1'.

---
+++ branches/fossfa-0-1/Care2002/language/fr/lang_fr_create_admin.php
@@ -0,0 +1,13 @@
+<?php
+$LDCreateAdmin='Crèer les droits d\'accès administrateur';
+$LDLoginExists='Le nom d\'utilisateur/mot de passe existe déjà. Svp, utiliser un nom d\'utilisateur/mot de passe différent';
+$LDAdminOk='L\'administrateur système a maintenant été crèé. Svp, effacez le script create_admin.php du répertoire racine de l\'application, sinon Care2x refusera de démarrer.';
+$LDDeleteWarning='Très important!  Vous devez effacer le script create_admin.php après avoir crèer avec succès un accès administrateur!';
+$LDCreateAdminWarning='Vous n\'avez pas effacéle script create_admin.php!  
+Vous devez effacer le script create_admin.php  sinon Care2x refusera de démarrer!';
+$LDWarning='Attention!';
+$LDInstallWarning='Vous n\'avez pas effacé le script "install/initialize.php"!  
+Vous devez effacer le script "install/initialize.php" maintenant sinonune autre personne pourrais changer vos informations d\'accès à la base de données!';
+$LDMD5Warning='Vous n\'avez pas encore effacé le script "install/encode_pw_md5.php"!  
+Vous devez effacer le script "install/encode_pw_md5.php"maintenant sinon une autre personne rendre vos droits d\'utilisateur inutilisable en executant à nouveau ce script!';
+?>
---
+++ branches/fossfa-0-1/Care2002/main/imgcreator/roots.php
@@ -0,0 +1,5 @@
+<?php
+$root_path='../../';
+/*------begin------ This protection code was suggested by Luki R. luki@karet.org ---- */
+if (eregi('roots.php',$HTTP_SERVER_VARS['PHP_SELF'])) die('<meta http-equiv="refresh" content="0; url='.$root_path.'">');
+?>
