024523c937bc1c8b3763a8cfe15d9545ba1a0c37
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2004-05-20 10:13:27 +0000

    Patch D-1.1.0.1 for deploypment D-1.1

--- branches/G1_DEPLOYMENT_1_1/Care2002/help/de/help_de_dept_config.php
+++ branches/G1_DEPLOYMENT_1_1/Care2002/help/de/help_de_dept_config.php
@@ -3,7 +3,7 @@
 <p>
 <font size=2 face="verdana,arial" >
 
-<form>
+
 <font size=2 face="verdana,arial" >
 <a name="sel"><img <?php echo createComIcon('../','frage.gif','0') ?>> <font color="#990000"><b></a>
 Wie kann ich mir die detaillierten Informationen zu  einer Abteilung anzeigen lassen?</b></font>
@@ -14,7 +14,14 @@ Wie kann ich mir die detaillierten Informationen zu  einer Abteilung anzeigen la
 <a name="sel"><img <?php echo createComIcon('../','frage.gif','0') ?>> <font color="#990000"><b></a>
 Wie ändere ich den Status einer Abteilung?</b></font>
 <ul>
-	<b>Schritt: </b>Klicken Sie auf den "Ändern" -Link der Abteilung.<p>
+	<b>Schritt: </b>Klicken Sie auf den "<font color="blue">Ändern</font>" -Link der Abteilung.<p>
 </ul>
-</form>
+
+<a name="sel"><img <?php echo createComIcon('../','frage.gif','0') ?>> <font color="#990000"><b></a>
+Geht es auch schneller?</b></font>
+<ul>
+	<b>Ja </b>
+	<p>Bitte das <font color="blue">[x]  </font> anklicken um von "Aktiv" auf "Inaktiv" umzustellen.
+	<p>Bitte das <font color="blue">[x]  </font> anklicken um von "Sichtbar" auf "Versteckt" umzustellen.
+</ul>
 
