9bf2a0ade11eb15a40d693ba11634c55ad6da5cd
latorilla <latorilla@8154e661-2335-47f1-af25-7e1110f3596a>
2004-06-30 11:39:59 +0000

    Modifs foir plugin version

--- trunk/Care2002/plugins/system_new_module/includes/Grundgeruest_dbform.php
+++ trunk/Care2002/plugins/system_new_module/includes/Grundgeruest_dbform.php
@@ -10,11 +10,11 @@ $tabellentitel="Testdatenbank";
 $suchfeld="sort_nr";
 
 //include DBFORM
-include_once("../../../dbForm/formFields.inc");
-include_once("../../../dbForm/tempxxxlate.inc");
-include_once("../../../dbForm/dbForm.inc");
+include_once("./dbForm/formFields.inc");
+include_once("./dbForm/tempxxxlate.inc");
+include_once("./dbForm/dbForm.inc");
 
 //include ADODB
-include_once("../../../adodb/adodb.inc.php");
+include_once("./adodb/adodb.inc.php");
 //include_once($root_path."classes/adodb/adodb.inc.php");
 ?>
