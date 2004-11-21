5ba371866571e8494bb31882d7b5294b64a82d50
nobody <nobody@8154e661-2335-47f1-af25-7e1110f3596a>
2004-11-21 11:42:41 +0000

    This commit was manufactured by cvs2svn to create branch 'elc_tanzania-0-2'.

---
+++ branches/elc_tanzania-0-2/Care2002/language/ar/lang_ar_konsil.php
@@ -0,0 +1,271 @@
+<?php
+$LDEntryDate='Êַׁם־ ַבַֿ־ַב';
+$LDJournalNumber='ׁÞד ַב׃ּב';
+$LDBlockNumber='Úֿֿ ַבÞזַבָ';
+$LDDeepCuts='Þזַבָ ÚדםÞֹ';
+$LDSpecialDye='Special stain';
+$LDImmuneHistoChem='Immune Histo-chemistry';
+$LDHormoneReceptors='Hormone receptors';
+$LDSpecials='Specials';
+$LDSend='ַׁ׃ַב';
+$LDSendLater='ַׁ׃ַב בַֽÞַ';
+$LDReset='ÊױÝםׁ';
+$LDClose='ַÛבַÞ';
+$LDSpeedTest='Êֽבםב Úַּב.';
+$LDDiagnosticTest='״בָ Êֽבםב Êװ־םױם';
+$LDRelayResult='Êֽׁםב ַבÊװ־םױ Úָׁ ַבוַÊÝ/ּוַׂ ַבהֱַֿ';
+$LDSpeedCut='ּֽׁ ֿÞםÞ.';
+$LDDate='ַבÊַׁם־';
+$LDOpDate='Êַׁם־ ַבÚדבםֹ';
+$LDDoctor='ַב״ָםָ';
+$LDDept='ַבÞ׃ד';
+$LDTel='ַבוַÊÝ:';
+$LDMatType='הזÚ ַבÚםהֹ';
+$LDPE='Biopsy, sample excision';
+$LDSpecimen='Op specimen';
+$LDShave='ÊּׁםÝ';
+$LDCytology='Úבד ַב־בַםַ';
+$LDLocalization='ַבדזײÚ';
+$LDClinicalQuestions='Êװ־םױַÊ ַבÚםַֹֿ, ÊÞַׁםׁ, Ê׃ִַבַÊ:';
+$LDExtraInfo='דÚבזדַÊ ד׃ַÚֹֿ';
+$LDExtraInfoSample='(e.g. important Lab results, after Radiation in excision area, after Chemotherapy)';
+$LDForGynTests='Êֽבםב ַבדזׁÊַÊ:';
+$LDLastPeriod='ַ־ׁ ֿזֹׁ:';
+$LDPeriodType='הזÚ ַבֿזֹׁ:';
+$LDGravidity='ַֽדב:';
+$LDMenopauseSince='דה׀ ׃ה ַבםֶַ׃:';
+$LDHormoneTherapy='Úבַּ וׁדזהם:';
+$LDContraceptive='ÚÞד:';
+$LDIUD='IUD:';
+$LDRepeatedTest='בַÚַֹֿ ַבÊֽבםב:';
+$LDRepeatedTestPls='ַֿ־ב ׁÞד ַב׃ּב ַבדֽׁב דÚ ַבÊַׁם־';
+$LDHysterectomy='Hysterectomy:';
+$LDClinicalInfo='דÚבזדַÊ ַבÚםַֹֿ';
+$LDReqTest='Êֽבםב ַבÊװ־םױ ַבד״בזָ';
+$LDNotesTempReport='דבַֽÙַÊ / הÊֶַּ ַזבםֹ:';
+$LDRequestingDoc='״ָםָ ַב״בָ';
+$LDReportingDoc='״ָםָ ַבÊÞׁםׁ';
+$LDXrayNumber='X-ray ׁÞד:';
+$LD_r_cm2='r cm² :';
+$LDXrayTechnician='X-ray Ýהם';
+$LDYes='הÚד';
+$LDNo='בַ';
+$LDXrayTest='X-ray Êװ־םױ';
+$LDMammograph='Mammography';
+$LDSonograph='Sonography';
+$LDCT='ׁ׃ד ״ָÞם ַָבßדָםזÊׁ';
+$LDNuclear='ַב״ָ ַבהזזם';
+$LDMRT='ַבׁ׃ד ַב״ָÞם ַָבׁהםה ַבדÛהַ״ם׃ם';
+
+$LDPatMobile='דזָםב ַבדׁםײ?';
+$LDAllergyKnown='ֽ׃ַ׃םֹ דÚׁזÝֹ?';
+$LDHyperthyreosisKnown='Hyperthyreosis known?';
+$LDPregnantPossible='ַדßַהםֹ ַבֽדב?';
+
+$LDDiagnosesInquiries='Êװ־םױַÊ / ַ׃ÊÚבַדַÊ:';
+$LDDeptReport='ÊÞׁםׁ ַבÞ׃ד:';
+$LDRequestTo='ד״בזָ בÛַםֹ';
+$LDDepartment='ַבÞ׃ד';
+$LDVisitRequested='ׂםַֹׁ ד״בזָֹ';
+$LDPatCanBeOrdered='םדßה ״בָ ַבדׁםײ';
+
+$LDShortMonth=array('',
+                                   'םַ',
+								   'Ýָ',
+								   'דַ',
+								   'ַָ',
+								   'דַ',
+								   'םז',
+								   'םז',
+								   'ַÛ',
+								   '׃ָ',
+								   'ַß',
+								   'הז',
+								   'ֿם');
+$LDBatchNumber='ׁÞד ַבֿÝÚֹ';
+$LDMaterial='ַבדַֹֿ:';
+$LDDiagnosis='ַבÊװ־םױ:';
+$LDImmuneSupp='Immune supp.';		
+
+$LDRequestedTest='ַבÊֽבםב ַבד״בזָ';
+$LDLabel='Label';					
+$LDCentralLab='ַבדÚדב ַבדׁßׂם';
+
+$LDRequestOf='ד״בזָ בַּב ';
+$LDTelephone='ַבוַÊÝ';
+$LDToBloodBank='ַבל ָהß ַבֿד';
+$LDWithMatchTest='with match test sample(s)';
+$LDByBloodBank='דה ָהß ַבֿד:';
+
+$LDBloodGroup='Ýױםבֹ ַבֿד:';
+$LDRhFactor='Rh-factor:';
+$LDKell='Kell';
+$LDDateProtNumber='ַבÊַׁם־ ז ׁÞד ַבָׁÊזßזב ַב־ַױ ַָבÊֽבםב:';
+$LDBloodSpecimen='Úםהֹ';
+$LDCount='Count';
+$LDPureBlood='Pure blood';
+$LDRedBloodCon='Red blood cell concentrate';
+$LDLeukoLessRedBlood='Leukocytedepleted red blood cell';
+$LDWashedRedBlood='Washed red blood cells';
+$LDPRP='Platelet rich plasma';
+$LDThromboCon='Platelet concentrate';
+$LDFFP='Fresh frozen plasma</font>';
+$LDTransfusionDevice='ּוַׂ הÞב ַבֿד';
+$LDTransfusionDate='Êַׁם־ הÞב ַבֿד:';
+$LDNotesRequests='דבַֽÙַÊ/״בַָÊ ־ַױֹ:';
+$LDDoctorNotice='<b>ַב״ָםָ ַב״ַבָ ד׃ִב Úה ו׀ַ ַב״בָ!</b><br>בֿזַÚם ַב׃ׁםֹ, בה ÊÞָב ַהַָםָ ÚםהַÊ ַבַ דÚ ַבַ׃ד ַבßַדב ז Êַׁם־ ַבדםבַֿ.';
+
+$LDPB='PB'; /* Pure blood */
+$LD350='350';
+$LDRB='RB'; /* Red blood cells */
+$LDLLRB='LRB'; /* Leukocyteless Red blood cells */
+$LDWRB='WRB'; /* washed red blood cells */
+$LDPRP_Initial='PRP'; /* Platelet rich plasma*/
+$LDTC='TC'; /* Platelet - Thrombocytes concentrate*/
+$LDFFP_Initial='FFP'; /* Fresh frozen plasma*/
+$LDLabServices='ֽ׃ַָ ־ֿדַÊ ַבדÚדב';
+$LDServiceCode='S-CODE';
+$LDBloodGroupCode='1360';
+$LDA_SubgroupCode='1342';
+$LDExtraBGFactorsCode='1391';
+$LDCoombsTestCode='1343';
+$LDAntibodyTestCode='1355';
+$LDCrossTestCode='1365';
+$LDAntibodyDiffCode='1355';
+$LDA_Subgroup='A-subgroup';
+$LDExtraBGFactors='other blood group factors';
+$LDCoombsTest='Dir. Coombstest';
+$LDAntibodyTest='Antibody search test';
+$LDCrossTest='Cross test';
+$LDAntibodyDiff='Antibody differentiation';
+$LDTotalAmount='ַבַּדַבם';
+$LDPrice='ַב׃Úׁ';
+$LDConserveNrPaste='(Paste product number here immediately)';
+$LDLabTimeStamp='(Blood bank time stamp)';
+$LDReleaseVia='Release via';
+$LDReceiptAck='Receipt acknowledgement';
+$LDSignature='ַבÊזÞםÚ';
+$LDLabLogBook='ׁÞד ַב׃ּב:';
+$LDLabNumber='ׁÞד ַבדÚדב';
+$LDBookedOn='ֿזה Ýם';
+
+$LDFillByLab='םדבַ דה ״ׁÝ ָהß ַבֿד ÝÞ״!';
+$LDFillByWard='דבַ Ýם ַבּהַֽ!';
+
+/* 2002-09-03 EL */							  
+$LDSearchPatient='ָֻֽ Úה דׁםײ';
+$LDPlsSelectPatientFirst='ַָֻֽ Úה ַבדׁםײ ַזבַ.';
+/* 2002-09-07 EL*/
+$LDAlertQuickCut='Êד ַ־Êםַׁ ַבÞ״Ú ַב׃ׁםÚ.';
+$LDAlertQuickDiagnosis='Êד ַ־Êםַׁ ַבÊװ־םױ ַב׃ׁםÚ.';
+$LDPlsEnterPhone='ַֿ־ב ׁÞד ַבוַÊÝ ַז ּוַׂ ַבהֱַֿ.';
+$LDPlsEnterOpDate='ַֿ־ב Êַׁם־ ַבÚדבםֹ.';
+$LDPlsEnterDoctorName='ַֿ־ב ַ׃ד ַב״ָםָ.';
+
+/* 2002-09-08 EL */
+$LDPlsEnterBloodGroup='ַֿ־ב Ýױםבֹ ַבֿד.';
+$LDPlsEnterRhFactor='Please enter the Rh-factor.';
+$LDPlsEnterKell='Please enter the Kell value.';
+$LDPlsEnterBloodPcs='ַֿ־ב Úֿֿ Þ״Ú דהÊַּÊ ַבֿד';
+$LDPlsEnterDate='ַֿ־ב ַבÊַׁם־.';
+/* 2002-09-09 EL */
+$LDPlsEnterDiagnosisQuiry='ַֿ־ב ַבÊװ־םױַÊ ַז ַבַ׃ÊÚבַדַÊ';
+$LDPlsSelectDept='ַ־ÊַַׁבÞ׃ד.';
+$LDPlsSelectDeptShort='ַ־Êַׁ ַבÞ׃ד';
+/* 2002-12-09 EL */
+$LDPendingTestRequest='״בָ Êֽבםב Ýם ַבַהÊÙַׁ';
+/* 2002-09-13 EL */
+$LDPrevRequest='ַב״בָ ַב׃ַָÞ';
+$LDNextRequest='ַב״בָ ַבÊַבם';
+$LDEnterResult='ַßÊָ ַבדזּזַֿÊ/ַבהÊֶַּ ב״בָ ַבÊֽבםב ו׀ַ';
+$LDPrintOut='ַ״ָÚ ו׀ַ ַבהדזּֿ';
+$LDSaveEntry='ַֽÝÙ ַבדֿ־בַÊ';
+/* 2002-09-14 EL */
+$LDNoPendingRequest='בַ םזּֿ ״בַָÊ Ýם ַבַהÊÙַׁ.';
+/* 2002-09-15 EL */
+$LDTestFindings='הÊֶַּ ַבÊֽבםב';
+$LDCaseNr='ׁÞד ַבַֽבֹ';
+$LDFamilyName='ַבבÞָ';
+$LDName='ַבַ׃ד';
+$LDBDay='Êַׁם־ ַבדםבַֿ';
+/* 2002-09-15 EL */
+$LDMACROFindings='Macro findings';
+$LDMicroFindings='Micro findings';
+$LDAddFindings='דבַֽÙַÊ ַײַÝםֹ';
+$LDXrayDate='Xray Êַׁם־';
+$LDPlsEnterTransfusionDate='ַֿ־ב Êַׁם־ הÞב ַבֿד';
+/* 2002-09-21 EL */
+$LDPlsEnterLEN='ַֿ־ב ׁÞד ֿ־זב ַבדÚדב';
+/* 2002-09-28 EL */
+$LDHospitalName='Úהַםֹ';
+
+/* 2002-09-29 EL */
+$LDHematology='Hematology';
+$LDCoagulation='Coagulation';
+$LDUrine='Urine';
+$LDSerum='Serum';
+$LDNuechtern='sob';
+$LDGlucose='Glucose';
+$LDBLP='BLP'; /* BLP = Blood plasma */
+$LDBS='BLS'; /* BLS = Blood sugar */
+$LDBS1='BLS 1';
+$LD900='9.00';
+$LD1500='15.00';
+
+/* 2002-10-14 EL */
+$LDDone='Êד ַבַהַּׂ! הÞב ַבהדזּֿ ַבל ַבַׁװםÝ';
+
+/* Note: the following arrays use strict medical terminology.
+*  If you are not sure about their translation, please leave the 
+*  english word untranslated
+*/
+$LDBacLabMaterialType = array(_mx_k_urin_=>'C.Urin',                                              
+                                              _mx_sputum_=>'Sputum',
+											  _mx_m_urin_=>'M.Urin',
+											  _mx_trachealsecrete_=>'Trac.scrt.',
+											  _mx_uricult_=>'Uricult',
+											  _mx_bronchiallavage_=>'B.lavage',
+											  _mx_wundabstrich_=>'W.smr',
+											  _mx_magensaft_=>'Gastrc.j.',
+											  _mx_augen_abstrich_right_=>'Ey.s.r.',
+											  _mx_secrete_=>'Secrete',
+											  _mx_augen_abstrich_left_=>'Ey.s.l.',
+											  _mx_exsudat_=>'Exsudat',
+											  _mx_ear_abstrich_right_=>'E.s.r.',
+											  _mx_punction_=>'Punctat',
+											  _mx_ear_abstrich_left_=>'E.s.l.',
+											  _mx_pleura_=>'Pleura',
+											  _mx_rachen_abstrich_=>'Th.smr',
+											  _mx_ascites_=>'Ascetis',
+											  _mx_tonsillen_abstrich_=>'Tons.s.',
+											  _mx_douglas_=>'Douglas',
+											  _mx_nose_abstrich_=>'N.smr',
+											  _mx_liquor_=>'Liquor',
+											  _mx_vaginal_abstrich_=>'Vag.s.',
+											  _mx_blood_culture_=>'Blood.C.');
+										  
+$LDBacLabTestType=array(_tx_special_body_material_=>'S.body.m.',
+                                        _tx_culture_aerob_=>'Aerob.C.',
+										_tx_op_material_=>'OP.Mat.',
+										_tx_culture_anaerob_=>'Anae.C.',
+										_tx_k_point_=>'Cat.tip',
+										_tx_fungus_culture_=>'Fungal.C.',
+										_tx_tubus_point_=>'Tube.tip',
+										_tx_stool_parasite_=>'St.f.paras.',
+										_tx_go_culture_=>'GO-Cult.',
+										_tx_stool_pathogen_=>'St.f.patho.',
+										_tx_hygiene_material_=>'Hygien.Mat.',
+										_tx_stool_dyspepsy_=>'St.f.Dysp.',
+										_tx_biopsy_material_=>'Biops.Mat.',
+										_tx_stool_clost_toxin_=>'St.f.C.Tox.',
+										_tx_stool_=>'Stool',
+										_tx_tbc_dye_=>'TBC.Stain',
+										_tx_stool_yersinia_=>'St.f.Yers.',
+										_tx_tbc_culture_=>'TBC.cult.',
+										_tx_stool_ehec_=>'St.f.EHEC',
+										_tx_liquor_antigen_=>'Liq.antigen',
+										_tx_own_blood_=>'Own blood',
+										_tx_gram_dye_=>'Gram.stain',
+										_tx_pharma_material_=>'Pharm.mat.',
+										_tx_go_dye_=>'GO-stain');
+?>
---
+++ branches/elc_tanzania-0-2/Care2002/language/ar/lang_ar_prompt.php
@@ -0,0 +1,91 @@
+<?php
+$LDPlsEnterEndDate='ÃÏÎá ÊÇÑíÎ ÇáäåÇíÉ';
+$LDPlsEnterStartDate='ÃÏÎá ÊÇÑíÎ ÇáÈÏÇíÉ';
+$LDPlsEnterConfirmDate='ÃÏÎá ÊÇÑíÎ ÇáÊÃßíÏ';
+$LDPlsEnterDiagnosis='ÃÏÎá ÇáÊÔÎíÕ';
+$LDPlsSelectStyle='ÃÎÊÇÑ ÇáäãØ';
+$LDCheckParamFirst='ÃÔÑ ÃæáÇ Úáì ÇáĞí ÊÑÛÈ İí ÚÑÖå';
+# Do not edit ~ward_id~
+$LDChkWardConflict='åĞÇ ÇáãÑíÖ ãÎÕÕ áÌäÇÍ ÇÎÑ (<b>~ward_id~</b>). æÖÍ åĞå ÇáãÓÇáÉ ãÚ ŞÓã ÇáŞÈæá ŞÈá ÇáÈÏÁ İí ÇÌÑÇÁÇÊ ŞÈæá åĞÇ ÇáãÑíÖ İí ÌäÇÍß ÔßÑÇ.';
+$LDSelectRoomBed='ÍÏÏ ÇáÍÌÑÉ æ ÇáÓÑíÑ ÇáÏí ÊÑíÏ ÊÎÕíÕåÇ ááãÑíÖ, ÔßÑÇ áß';
+$LDSureAssignRoomBed='åá ÃäÊ ãÊÃßÏ ãä ÑÛÈÊß İí ÊÎÕíÕ åĞå ÇáÍÌÑÉ æ ÇáÓÑíÑ ááãÑíÖ¿';
+$LDWhereToTransfer='Çáì Ãíä Óæİ íäŞá ÇáãÑíÖ¿';
+$LDErrorOccured='ÍÏÊ ÎØÇÁ';
+$LDTryOrNotifyEDP='ÍÇæá ãÑÉ ÃÎÑì, Ãæ ÇÚÇã ŞÓã ÅÏÇÑÉ ÇáäÙÇã ÇĞÇ ÃÓÊãÑÊ ÇáãÔßáÉ';
+# 2003-07-30 EL
+$LDPlsEnterDate='ÃÏÎá ÇáÊÇÑíÎ';
+$LDPlsEnterDoctor='ÃÏÎá ÃÓã ÇáØÈíÈ Ãæ ÇáÌÑÇÍ';
+$LDPlsEnterPurpose='ÃÎá ÇáÛÇíÉ';
+$LDAppointNotToday='åĞÇ ÇáãæÚÏ áã íÍÏÏ áåĞÇ Çáíæã!';
+$LDSureAdmitAppoint='åá ÃäÊ ãÊÃßÏ ãä ÑÛÈÊß İí ŞÈæá åĞÇ ÇáãæÚÏ Çáíæã¿';
+$LDPatientIsDischarged='áŞÏ ÎÑÌ åĞÇ ÇáãÑíÖ ÈÇáİÚá';
+$LDPersonIsAdmitted='åĞÇ ÇáÔÎÕ ŞÈá ';
+$LDEncounterClosed='Êã ÇÛáÇŞ åĞå ÇáãŞÇÈáÉ';
+# 2003-08-02 EL
+$LDNoOutpatients='áÇ íæÌÏ ãÑíÖ ÚíÇÏÉ ÎÇÑÌíÉ ÈÚÏ';
+$LDGoToAppointments='ÃÖÛØ åäÇ áãÑÇÌÚÉ ÇáãæÇÚíÏ ÇáãÚáŞÉ';
+$LDSureTransferPatient='åá ÃäÊ ãÊÃßÏ ãä ÑÛÈÊß İí äŞá åĞÇ ÇáãÑíÖ¿';
+$LDClkTransfer='ÃÖÛØ áäŞá ÇáãÑíÖ';
+$LDSureTakeOverPatient='åá ÇäÊ ãÊÃßÏ? åá ÊÑÛÈ İí ÇÎÏ åĞÇ ÇáãÑíÖ?';
+$LDChkClinicConflict='Êã ÊÓÌíá åĞÇ ÇáãÑíÖ İí ÚíÇÏÉ/ŞÓã (<b>~dept_id~</b>). 
+æÖÍ åĞå ÇáãÓÇáÉ ãÚ ŞÓã ÇáŞÈæá Çæ ÇáÚíÇÏÉ ÇáÇÎÑì ŞÈá ÇáÈÏÁ İí ÇÌÑÇÁÇÊ äŞá åĞÇ ÇáãÑíÖ Çáì ÚíÇÏÊß ÔßÑÇ';
+$LDSureToCancel='åá ÃäÊ ãÊÃßÏ¿ åá ÊÑíÏ ÅáÛÇÁ åĞÇ¿';
+$LDPlsEnterFullName='ÃÏÎá ÃÓãß ßÇãáÇ';
+$LDOk='ãæÇİŞ';
+$LDAdmissionCancelled='Êã ÅáÛÇÁ ÇáŞÈæá ÈäÌÇÍ';
+$LDCancelError='ÍĞË ÎØÇÁ! ÃÎİŞÊ ÚãáíÉ ÇáÅáÛÇÁ';
+$LDTellEdpIfPersist='ÃÚáã ŞÓã ÅÏÇÑÉ ÇáäÙÇã ÅĞÇ ÃÓÊãÑÊ ÇáãÔßáÉ';
+$LDPlsFindPersonFirst='ÃæÌÏ ÇáÔÎÕ ÃæáÇ';
+$LDEnterRelatedInfo='ÅĞÇ ããßä, ÃÏÎá ÇáãÚáæãÇÊ ÇáãÊÚáŞÉ åäÇ';
+$LDRelatedEncNr='ÑŞã ÇáãŞÇÈáÉ ÇáãÊÚáŞÉ';
+$LDRelatedDocsIds='ÊÚÑíİ ÇáæËÇÆŞ ÇáãÊÚáŞÉ';
+$LDSeparateComma='ÃİÕá ÈİÇÕáÉ';
+$LDPlsReadThis='ÃŞÑÇÁ åĞÇ';
+# 2003-08-11 EL
+$LDPersonIsEmployed='åĞÇ ÇáÔÎÕ ãæÙİ İí åĞÇ ÇáãÓÊÔİì!';
+# 2003-08-24 EL
+$LDCachedInfo='åĞå ÇáãÚáæãÇÊ ÇáãÎİíÉ';
+$LDClkNoCache='ÃÖÛØ åĞÇ áÊÍÕá Úáì ÇáãÚáæãÇÊ ÇáÛíÑ ãÎİíÉ';
+
+# 2003-08-25 EL
+$LDPlsEnterReport='ÃÏÎá ÊŞÑíÑß Ãæ ÇáãáÇÍÙÇÊ';
+$LDPlsEnterDosage='ÃÏÎá ÇáÌÑÚÉ';
+$LDPlsEnterMedicine='ÃÏÎá ÃÓã ÇáÏæÇÁ';
+$LDPlsEnterMedType='ÃÏÎá äæÚ ÇáÏæÇÁ';
+$LDPlsSelectAppType='ÇÎÊÇÑ äæÚ ÇáÊØÈíŞ';
+$LDPlsSelectPrescType='ÇÎÊÇÑ äæÚ ÇáæÕİÉ';
+$LDPlsEnterValue='ÇÏÎá ŞíãÉ';
+$LDPlsSelectUnitMsr='ÇÎÊÇÑ æÍÏÉ ÇáŞíÇÓ';
+
+# 2003-08-26 EL
+$LDEntryInvalidChar='ÇáŞíãÉ ÇáãÏÎáÉ ÛíÑ ÕÇáÍÉ!';
+$LDNotNegValue='ÇáŞíãÉ ÇáÓÇáÈÉ ÛíÑ ãÊÇÍÉ!';
+$LDOptional='ÇÎÊíÇÑí';
+$LDSaveAnyway='åá ÊÑíÏ ÍŞÇ ÍİÙ ÇáÈíÇäÇÊ Úáì ßá ÍÇá';
+#2003-08-27 EL
+$LDDeathDateIs='ÊÇÑíÎ ÇáæİÇÉ ÇáãÏÎá åæ';
+$LDIsCorrect='åá Ğáß ÕÍíÍ¿';
+$LDProceedSave='åá ÊÑíÏ Ãä ÊÈÇÔÑ ÈÇáÍİÙ¿';
+#2003-08-28 EL
+$LDPlsSelectWard='ÃÎÊÇÑ ÌäÇÍ';
+# 2003-09-20 EL
+$LDPlsEnterTherapy='ÃÏÎá ÇáÚáÇÌ';
+$LDPlsMedicalAdvice='æÖÍ ãÇ ÇĞÇ ÍÕá ÇáãÑíÖ Úáì ÇáÇÑÔÇÏÇÊ ÇáØÈíÉ Ãã áÇ';
+# 2003-09-23 EL
+$LDPlsEnterJobFunction='ÃÏÎá ãåÇã ÇáÚãá';
+#2003-04-10 EL
+$LDPlsFillInfo='ÃßÊÈ İí ÇáÍŞæá ÇáãÚáãÉ ÈÇááæä ÇáÇÍãÑ';
+$LDPlsEnterLocalization='ÇÏÎá ÇáãæÖÚ';
+$LDPlsEnterNotes='ÇÏÎá ãáÇÍÙÇÊ';
+$LDPlsEnterClassification='ÃÏÎá ÇáÊÕäíİÇÊ';
+$LDPlsEnterStartTime='ÃÏÎá Òãä ÇáÈÏÇíÉ';
+$LDPlsEnterEndTime='ÃÏÎá Òãä ÇáÇäÊåÇÁ';
+$LDPlsEnterScrubNurse='ÃÏÎá ãÚáæãÇÊ ÚäÕÑ ÇáÊãÑíÖ';
+$LDPlsEnterORNr='ÃÏÎá ÊÚÑíİ Ã, ÑŞã ÍÌÑÉ ÇáÚãáíÇÊ';
+$LDWrongLoginPW='ÇÓã ÇáãÓÊÎÏã Çæ ßáãÉ ÇáÓÑ ÛíÑ ÕÍíÍÉ';
+$LDPlsORNr='ÇÏÎá ÑŞã ÛÑİÉ ÇáÚãáíÇÊ';
+
+#2004-08-06 EL
+$LDInvalidDate ='The entered date was invalid.';
+$LDNoPastDate = 'Dates in the past are not accepted.';
+?>
