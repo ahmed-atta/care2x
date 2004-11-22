6d70ea8d1c0e78e8fefa06f37e7bed9d4662c8da
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2004-11-22 18:33:36 +0000

    *** empty log message ***

---
+++ branches/elc_tanzania-0-2/Care2002/gui/smarty_template/templates_c/mac1/%%190/%%1906192163/reg_row.tpl.php
@@ -0,0 +1,10 @@
+<?php /* Smarty version 2.6.0, created on 2004-07-03 16:08:01
+         compiled from registration_admission/reg_row.tpl */ ?>
+<tr>
+  <td class="reg_item" <?php echo $this->_tpl_vars['sColSpan1']; ?>
+><?php echo $this->_tpl_vars['sItem']; ?>
+</td>
+  <td class="reg_input" <?php echo $this->_tpl_vars['sColSpan2']; ?>
+><?php echo $this->_tpl_vars['sInput']; ?>
+</td>
+</tr>
