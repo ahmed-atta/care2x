98a0f2bdce831f075c0667d6f7245c28c8d5c4de
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2004-11-22 18:26:52 +0000

    *** empty log message ***

---
+++ branches/elc_tanzania-0-2/Care2002/gui/smarty_template/templates_c/mac1/%%106/%%1063835746/frameset_floatingmenu_ltr.tpl.php
@@ -0,0 +1,13 @@
+<?php /* Smarty version 2.6.0, created on 2004-07-15 20:59:59
+         compiled from common/frameset_floatingmenu_ltr.tpl */ ?>
+
+<frameset rows="25,*" border=0>
+  <frameset cols="9%,*" border=0>
+    <frame name="STARTPAGE" <?php echo $this->_tpl_vars['sStartFrameSource']; ?>
+>
+    <frame name="MENUBAR" <?php echo $this->_tpl_vars['sMenuFrameSource']; ?>
+ scrolling=no>
+  </frameset>
+  <FRAME NAME = "CONTENTS" <?php echo $this->_tpl_vars['sContentsFrameSource']; ?>
+>
+</frameset>
