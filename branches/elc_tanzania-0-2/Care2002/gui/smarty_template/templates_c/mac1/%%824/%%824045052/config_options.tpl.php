9a04c56360516457f825e97d6e271438da3cc962
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2004-11-22 18:33:38 +0000

    *** empty log message ***

---
+++ branches/elc_tanzania-0-2/Care2002/gui/smarty_template/templates_c/mac1/%%824/%%824045052/config_options.tpl.php
@@ -0,0 +1,77 @@
+<?php /* Smarty version 2.6.0, created on 2004-07-29 14:25:04
+         compiled from main/config_options.tpl */ ?>
+
+<table border=0 cellpadding=5>
+  <tbody>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDColorOptImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDColorOpt']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDColorOptTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDColorOptExtImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDColorOptExt']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDColorOptExtTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDMascotOptImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDMascotOpt']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDMascotOptTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDMainMenuTreeImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDMainMenuTree']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDMainMenuTreeTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDGUITemplateImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDGUITemplate']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDGUITemplateTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDControlButtonsImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDControlButtons']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDControlButtonsTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDComIconsImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDComIcons']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDComIconsTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDCssThemesImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDCssThemes']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDCssThemesTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDSmartyTemplateImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDSmartyTemplate']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDSmartyTemplateTxt']; ?>
+</font></td>
+    </tr>
+    <tr>
+      <td>&nbsp;&nbsp;<?php echo $this->_tpl_vars['LDDisplayImg']; ?>
+</td>
+      <td><font size=4><?php echo $this->_tpl_vars['LDDisplay']; ?>
+</font><br><font size=2><?php echo $this->_tpl_vars['LDDisplayTxt']; ?>
+</font></td>
+    </tr>
+  </tbody>
+</table>
