34477049f70b7f35784993588fb4091d6727ace1
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2004-11-22 18:26:35 +0000

    *** empty log message ***

---
+++ branches/elc_tanzania-0-2/Care2002/gui/smarty_template/templates_c/default/%%-88/%%-882767532/footer.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.0, created on 2004-07-03 16:05:20
+         compiled from common/footer.tpl */ ?>
+<!--  <?php echo $this->_tpl_vars['SmartyLogo']; ?>
+ -->
+</BODY>
+</HTML>
