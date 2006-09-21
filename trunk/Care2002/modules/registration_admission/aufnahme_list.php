12ec53eb689538d1895a3d42eaf7814b4ddf8eea
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-09-21 15:07:19 +0000

    Solve the problem of coming to patient.php page in English (not the selected language) after closing the aufnahme_list.php page. Credits to Dr. Bulent Potur.

--- trunk/Care2002/modules/registration_admission/aufnahme_list.php
+++ trunk/Care2002/modules/registration_admission/aufnahme_list.php
@@ -5,7 +5,7 @@ require($root_path.'include/inc_environment_global.php');
 /**
 * CARE2X Integrated Hospital Information System beta 2.0.1 - 2004-07-04
 * GNU General Public License
-* Copyright 2002,2003,2004,2005,2006 Elpidio Latorilla
+* Copyright 2002,2003,2004,2005 Elpidio Latorilla
 * elpidio@care2x.org, 
 *
 * See the file "copy_notice.txt" for the licence notice
@@ -211,7 +211,7 @@ $glob_obj->getConfig('patient%');
 $glob_obj->getConfig('person%');
 
 $thisfile=basename(__FILE__);
-$breakfile='patient.php';
+$breakfile='patient.php'.URL_APPEND;
 $newdata=1;
 $target='archiv';
 
