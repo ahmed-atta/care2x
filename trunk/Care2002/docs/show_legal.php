fa7b91fd3a4c9e77cc86206e3009984babfe52b6
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-09-28 20:56:35 +0000

    Fix broken link to the care2x logo on the legal page.

--- trunk/Care2002/docs/show_legal.php
+++ trunk/Care2002/docs/show_legal.php
@@ -1,4 +1,11 @@
 <?php
-if(file_exists('../language/'.$HTTP_GET_VARS['lang'].'/'.$HTTP_GET_VARS['lang'].'_legal.htm')) include('../language/'.$HTTP_GET_VARS['lang'].'/'.$HTTP_GET_VARS['lang'].'_legal.htm');
-	else include('../language/en/en_legal.htm');
+if(file_exists('../language/'.$HTTP_GET_VARS['lang'].'/'.$HTTP_GET_VARS['lang'].'_legal.htm')) {
+    $page = '../language/'.$HTTP_GET_VARS['lang'].'/'.$HTTP_GET_VARS['lang'].'_legal.htm';
+} else {
+	$page = '../language/en/en_legal.htm';
+}
+
+header("Location: $page");
+exit;
+	
 ?>
