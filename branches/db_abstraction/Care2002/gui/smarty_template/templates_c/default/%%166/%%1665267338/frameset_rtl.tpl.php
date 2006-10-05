88100e33c32c0fb5e7e9aa71b4c1a8f51d3b74ad
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-10-05 19:09:54 +0000

    Branching trunk for changes on improving the database abstraction.

---
+++ branches/db_abstraction/Care2002/gui/smarty_template/templates_c/default/%%166/%%1665267338/frameset_rtl.tpl.php
@@ -0,0 +1,11 @@
+<?php /* Smarty version 2.6.0, created on 2004-07-03 16:05:18
+         compiled from common/frameset_rtl.tpl */ ?>
+
+<frameset cols="*,19%" border=0>
+	<FRAME NAME = "CONTENTS" <?php echo $this->_tpl_vars['sContentsFrameSource']; ?>
+>
+		<frameset rows="*" border=0>
+			<frame name="STARTPAGE" <?php echo $this->_tpl_vars['sStartFrameSource']; ?>
+>
+	</frameset>
+</frameset>
---
+++ branches/db_abstraction/Care2002/language/it/lang_it_ambulatory.php
@@ -0,0 +1,24 @@
+<?php
+/* 2002-10-02 EL */
+$LDDepartments='Day Hospital';
+$LDPendingRequest='Richieste in coda';
+$LDPendingRequestTxt='Accettazione, Elenco, Stampa, Inserire diagnosi conclusiva';
+$LDGeneralSurgery='Day Hospital Generale';
+$LDInternalMed='Day Hospital di Medicina Interna';
+$LDEmergency='Pronto Soccorso';
+$LDSonography='Sonografico';
+$LDNuclearMed='Diagnostica nucleare';
+$LDAdmission='Accetazione';
+$LDAdmissionTxt='Accettazione/dimissione ambulatoriale, ricerca, archivio';
+$LDNews='News';
+$LDNewsTxt='News cliniche, memo, info, etc.';
+# 2003-08-01 EL
+$LDAppointments='Appuntamenti';
+$LDAppointmentsTxt='Appuntamenti del paziente in questo reparto';
+$LDPatientWaitingList='Lista d\'attesa pazienti';
+$LDPWListTxt='Pazienti accettati oggi in clinica';
+$LDOutpatientClinic='Day Hospital';
+$LDGeneralAmbulatory='Day Hospital Generale';
+$LDAmbulatory='Day Hospital';
+?>
+
