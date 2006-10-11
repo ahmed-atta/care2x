dc8f24118779ee8b0eb1a1d1c1f9b718678f1522
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-10-11 18:55:11 +0000

    This branch is deleted, because it has no been used since the source code was migrated to Subversion. The branch still exists in the read-only CVS.

--- branches/G2_DEPLOYMENT_2_1/Care2002/language/ar/lang_ar_help.php
+++
@@ -1,5 +0,0 @@
-<?php
-$LDHelp="ÊÚáíãÇÊ";
-$LDClose="ÅÛáÇÞ";
-$LDCloseHelpWin="ÃÛáÞ äÇÝÐÉ ÇáÊÚáíãÇÊ";
-?>
--- branches/G2_DEPLOYMENT_2_1/Care2002/language/ar/tags.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($lang=='ar'||$curr_lang=='ar') $lang_name='ÚÑÈí';
-	else $lang_name='Arabic';
-$lang_iso_code='ar';
-//$lang_charset='28596';
-$lang_charset='windows-1256';
-?>
