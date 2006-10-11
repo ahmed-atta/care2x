1672ea22d70b1d636666099df6feeca51e409def
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-10-11 18:53:10 +0000

    This branch is deleted, because it has no been used since the source code was migrated to Subversion. The branch still exists in the read-only CVS.

--- branches/fossfa-0-1/Care2002/language/es/lang_es_images.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
-* Traducido por: Dr. med. Daniel Hinostroza C.
-*/
-
-$LDTogglePreviewOrig='Dé clic para mostrarlo en su tamaño original';
-$LDToggleOrigPreview='Dé clic para mostrarlo en tamaño reducido';
-?>
