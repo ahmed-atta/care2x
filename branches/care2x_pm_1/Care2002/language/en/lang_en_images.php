370ca4b16e50807e88602105da78bf9d35e1d8d9
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-10-11 18:45:45 +0000

    This branch is deleted, because it has no been used since the source code was migrated to Subversion. The branch still exists in the read-only CVS.

--- branches/care2x_pm_1/Care2002/language/en/lang_en_images.php
+++
@@ -1,4 +0,0 @@
-<?php
-$LDTogglePreviewOrig='Click to show in original size';
-$LDToggleOrigPreview='Click to show in reduced size';
-?>
--- branches/care2x_pm_1/Care2002/language/pl/pl_copyrite.php
+++
@@ -1,9 +0,0 @@
-<?php
-require($root_path.'include/inc_credits_openwin.php');
-?>
-<FONT    SIZE=1  FACE="Arial" color=gray>
- <a href="http://www.care2x.com" target=_new>CARE 2X 1.0.10 wersja "pre-deployment"</a>::<a href="<?php echo $root_path ?>legal_gnu_gpl.htm" target=_new> Tre¶æ Licencji</a> :: 
- <a href=mailto:info@care2x.com> Kontakt</a> <font size=1 face="arial"> :: <a href="<?php echo $root_path ?>language/pl/pl_privacy.htm" target="pp">Statut naszej dzia³alnosci (our privacy policy)</a> :: 
- <a href="<?php echo $root_path ?>docs/show_legal.php?lang=<?php echo $lang ?>" target="lgl"> Legal</a> ::</font>
- <a href="javascript:openCreditsWindow()"> Kredyty</a> ::.<br></font>
-</FONT>
