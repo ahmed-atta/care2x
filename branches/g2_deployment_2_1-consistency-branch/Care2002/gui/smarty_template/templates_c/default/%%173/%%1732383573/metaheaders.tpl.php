6bfe6e78333fd26134193713f6a35979665696be
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-10-11 18:55:40 +0000

    This branch is deleted, because it has no been used since the source code was migrated to Subversion. The branch still exists in the read-only CVS.

--- branches/g2_deployment_2_1-consistency-branch/Care2002/gui/smarty_template/templates_c/default/%%173/%%1732383573/metaheaders.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2004-07-03 16:05:18
-         compiled from common/metaheaders.tpl */ ?>
-<meta name="Description" content="Hospital and Healthcare Integrated Information System - CARE2x">
-<meta name="Author" content="Elpidio Latorilla">
-<meta name="Generator" content="various: Quanta, AceHTML 4 Freeware, NuSphere, PHP Coder">
-<?php echo $this->_tpl_vars['sExtraMetaString']; ?>
