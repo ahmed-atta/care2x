981ad5024cdb867fef762d162d05a7266a3eac6d
kaloyan_raev <kaloyan_raev@8154e661-2335-47f1-af25-7e1110f3596a>
2006-10-11 19:00:56 +0000

    This branch is deleted, because it has no been used since the source code was migrated to Subversion. The branch still exists in the read-only CVS.

--- branches/trash_can/Care2002/req/email_domains_options.php
+++
@@ -1,7 +0,0 @@
-<? 
-
-$email_domains=array("intranet","plop","hnop","allgemein.chir",
-										"allgemein.chir.op","innere1","innere2","gyn.op",
-										"unfall.op","augen.op","augen","plastische","gyn",
-										"kst","kks");
-?>
