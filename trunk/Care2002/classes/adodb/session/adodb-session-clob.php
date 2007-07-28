0199ef4e31ba79a8017c6b31e0987fb75c416062
claudiotorbinio <claudiotorbinio@8154e661-2335-47f1-af25-7e1110f3596a>
2007-07-28 17:31:52 +0000

    Added items remotely
    C:\Users\holyghost\care2x\trunk\Care2002\COPYING
    C:\Users\holyghost\care2x\trunk\Care2002\CREDITS
    C:\Users\holyghost\care2x\trunk\Care2002\FAQ
    C:\Users\holyghost\care2x\trunk\Care2002\index.htm
    C:\Users\holyghost\care2x\trunk\Care2002\index.html
    C:\Users\holyghost\care2x\trunk\Care2002\index.php
    C:\Users\holyghost\care2x\trunk\Care2002\INSTALL
    C:\Users\holyghost\care2x\trunk\Care2002\legal_gnu_gpl.htm
    C:\Users\holyghost\care2x\trunk\Care2002\LICENSE
    C:\Users\holyghost\care2x\trunk\Care2002\license.htm
    C:\Users\holyghost\care2x\trunk\Care2002\NEWS
    C:\Users\holyghost\care2x\trunk\Care2002\README
    C:\Users\holyghost\care2x\trunk\Care2002\roots.php
    C:\Users\holyghost\care2x\trunk\Care2002\sublinker-static.js
    C:\Users\holyghost\care2x\trunk\Care2002\cache
    C:\Users\holyghost\care2x\trunk\Care2002\classes
    C:\Users\holyghost\care2x\trunk\Care2002\counter
    C:\Users\holyghost\care2x\trunk\Care2002\css
    C:\Users\holyghost\care2x\trunk\Care2002\docs
    C:\Users\holyghost\care2x\trunk\Care2002\fotos
    C:\Users\holyghost\care2x\trunk\Care2002\global_conf
    C:\Users\holyghost\care2x\trunk\Care2002\gui
    C:\Users\holyghost\care2x\trunk\Care2002\help
    C:\Users\holyghost\care2x\trunk\Care2002\include
    C:\Users\holyghost\care2x\trunk\Care2002\installer
    C:\Users\holyghost\care2x\trunk\Care2002\js
    C:\Users\holyghost\care2x\trunk\Care2002\language
    C:\Users\holyghost\care2x\trunk\Care2002\logs
    C:\Users\holyghost\care2x\trunk\Care2002\main
    C:\Users\holyghost\care2x\trunk\Care2002\med_depot
    C:\Users\holyghost\care2x\trunk\Care2002\modules
    C:\Users\holyghost\care2x\trunk\Care2002\pharma
    C:\Users\holyghost\care2x\trunk\Care2002\plugins
    C:\Users\holyghost\care2x\trunk\Care2002\radiology
    C:\Users\holyghost\care2x\trunk\Care2002\userconfig
    C:\Users\holyghost\care2x\trunk\Care2002\blank.htm
    C:\Users\holyghost\care2x\trunk\Care2002\blank.php
    C:\Users\holyghost\care2x\trunk\Care2002\chklang.php
    C:\Users\holyghost\care2x\trunk\Care2002\cookies.php
    C:\Users\holyghost\care2x\trunk\Care2002\copy_notice.txt

---
+++ trunk/Care2002/classes/adodb/session/adodb-session-clob.php
@@ -0,0 +1,24 @@
+<?php
+
+// $CVSHeader: care2002_tz_mero/classes/adodb/session/adodb-session-clob.php,v 1.1 2004/05/20 12:21:37 robert Exp $
+
+/*
+V4.01 23 Oct 2003  (c) 2000-2004 John Lim (jlim@natsoft.com.my). All rights reserved.
+         Contributed by Ross Smith (adodb@netebb.com). 
+  Released under both BSD license and Lesser GPL library license.
+  Whenever there is any discrepancy between the two licenses,
+  the BSD license will take precedence.
+	  Set tabs to 4 for best viewing.
+*/
+
+/*
+
+This file is provided for backwards compatibility purposes
+
+*/
+
+require_once dirname(__FILE__) . '/adodb-session.php';
+
+ADODB_Session::clob('CLOB');
+
+?>
