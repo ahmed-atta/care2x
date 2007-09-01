fc47e48b0d7e9da7cf3528f73c50ff5e57a2ee45
claudiotorbinio <claudiotorbinio@8154e661-2335-47f1-af25-7e1110f3596a>
2007-09-01 02:42:38 +0000

    deleted some items

--- trunk/Care2007/modules/system_admin/edv-system-admi-menu.php
+++ trunk/Care2007/modules/system_admin/edv-system-admi-menu.php
@@ -18,16 +18,7 @@ if(isset($ck_edv_admin_user)) setcookie('ck_edvzugang_user',$ck_edv_admin_user);
 $breakfile='edv.php'.URL_APPEND;
 
 # Set the db manager app here
-switch($dbtype){
-	case 'mysql': $strDbAdminItem = $LDPhpMyAdmin;
-							$strDbAdminUrl='phpmyadmin';
-							break;
-	case 'postgres':
-	case 'postgres7':
-							$strDbAdminItem = 'phpPgAdmin';
-							$strDbAdminUrl = 'phppgadmin';
-							break;
-}
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 3.0//EN" "html.dtd">
 <?php html_rtl($lang); ?>
@@ -166,16 +157,8 @@ require($root_path.'include/inc_css_a_hilitebu.php');
 	&nbsp;&nbsp;&nbsp;<img <?php echo createComIcon($root_path,'redpfeil.gif','0','absmiddle') ?>> <a href="<?php echo $root_path; ?>modules/or_admin/or_list_config.php<?php echo URL_APPEND; ?>" target="SYSADMIN_WFRAME"><?php echo $LDListConfig ?></a>
 	</td>
   </tr>
-<tr>
-	<td bgcolor="#e9e9e9"><FONT  color="#0000cc" FACE="verdana,arial" size=2><b><?php echo $LDDatabase ?></b> </FONT></td>
-  </tr>
-  <tr>
- 	<td bgcolor="#ffffff"><FONT  color="#0000cc" FACE="verdana,arial" size=2>&nbsp;&nbsp;&nbsp;<img <?php echo createComIcon($root_path,'redpfeil.gif','0','absmiddle') ?>> 
-	<a href="../<?php echo $strDbAdminUrl ?>/index.php?sid=<?php echo $sid."&lang=$lang" ?>" target="SYSADMIN_WFRAME"><?php  echo $strDbAdminItem ?>
-	</a></FONT></td>
-<!--  	<td bgcolor="#e9e9e9"><FONT  color="#0000cc" FACE="verdana,arial" size=2><b><a href="phpmyadmin-start.php?sid=<?php echo $sid."&lang=$lang" ?>"><?php echo $LDMySQLManage ?></a></b> </FONT></td>
- -->  
-  </tr>
+
+  
 
 <!--  
   <tr>
