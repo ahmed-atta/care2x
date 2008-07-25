59a80698c1b12c52994d472310d8a1808c05e845
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2008-07-25 13:25:01 +0000

    update to Smarty 2.6.19

--- trunk/Care2007/gui/smarty_template/smarty_care.class.php
+++ trunk/Care2007/gui/smarty_template/smarty_care.class.php
@@ -72,7 +72,7 @@ class smarty_care extends Smarty {
 			# If the global config template is not available, use hard coded template theme
 			if(!isset($GLOBAL_CONFIG['template_smarty'])||empty($GLOBAL_CONFIG['template_smarty'])){
 				if(isset($template_theme)) $this->templatedir=$template_theme; // use this theme if the global item is not available
-					else $this->templatedir = $default_template;
+					else $this->templatedir = default_template;
 			}else{
 				$this->templatedir=$GLOBAL_CONFIG['template_smarty'];
 			}
@@ -117,7 +117,7 @@ class smarty_care extends Smarty {
 		/**
 		* global configs
 		*/
-		$Logo = $this->root_path.'classes/Smarty-2.6.0/misc/smarty_icon.gif';
+		$Logo = $this->root_path.'classes/Smarty-2.6.19/misc/smarty_icon.gif';
 
 		//$this->assign("SmartyLogo","<a href='http://smarty.php.net/'><img src='$Logo' border='00' height='31' width='88' /></a>");
 		$this->debug = true;
