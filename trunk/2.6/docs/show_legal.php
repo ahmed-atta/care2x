f67e3998dd27618c68604a7b739c32f51e7a9d8d
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2009-07-27 15:30:16 +0000

    closes #12 - update superglobals

--- trunk/2.6/docs/show_legal.php
+++ trunk/2.6/docs/show_legal.php
@@ -1,6 +1,6 @@
 <?php
-if(file_exists('../language/'.$HTTP_GET_VARS['lang'].'/'.$HTTP_GET_VARS['lang'].'_legal.htm')) {
-    $page = '../language/'.$HTTP_GET_VARS['lang'].'/'.$HTTP_GET_VARS['lang'].'_legal.htm';
+if(file_exists('../language/'.$_GET['lang'].'/'.$_GET['lang'].'_legal.htm')) {
+    $page = '../language/'.$_GET['lang'].'/'.$_GET['lang'].'_legal.htm';
 } else {
 	$page = '../language/en/en_legal.htm';
 }
