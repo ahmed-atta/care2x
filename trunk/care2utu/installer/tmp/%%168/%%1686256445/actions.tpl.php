a23c0a0514ddacbfa49eba50f9957aaafd98ef50
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2009-10-16 07:25:51 +0000

    several changes (webERP) Outstanding: lab and front side billing / installation routine

--- trunk/care2utu/installer/tmp/%%168/%%1686256445/actions.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2008-03-20 11:01:09
-         compiled from C:%5Cxampp%5Chtdocs%5Ccare2002_tz_mero_vps%5Cinstaller/templates/actions.tpl */ ?>
-<form action="<?php echo $GLOBALS['HTTP_SERVER_VARS']['PHP_SELF']; ?>
-" method="POST">
-<input type="hidden" name="save_action" value="true">
-<?php echo $this->_tpl_vars['ACTION_HTML']; ?>
-
-</form>
