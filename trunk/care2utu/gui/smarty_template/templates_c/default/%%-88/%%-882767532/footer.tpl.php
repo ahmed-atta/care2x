96192aa67fecfce43d0d25799cafb56c9af49e2e
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2009-10-16 07:25:51 +0000

    several changes (webERP) Outstanding: lab and front side billing / installation routine

--- trunk/care2utu/gui/smarty_template/templates_c/default/%%-88/%%-882767532/footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2008-05-20 14:35:11
-         compiled from common/footer.tpl */ ?>
-<!--  <?php echo $this->_tpl_vars['SmartyLogo']; ?>
- -->
-</BODY>
-</HTML>
