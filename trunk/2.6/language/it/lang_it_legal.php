18fcfd998a903a2138179a8b14f2da64e6809e03
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-03-19 10:13:32 +0000

    finished svn restructuring

--- trunk/2.6/language/it/lang_it_legal.php
+++
@@ -1,5 +0,0 @@
-<?php
-$LDSickConfirm='Il certificato di ricovero redatto ai fini di legge verrà approntato su richiesta del paziente dalla Coordinatrice infermieristica di reparto';
-
-$LDPharmaPrompt='Non disponibile';
-?>
--- trunk/2.6/language/tr/lang_tr_legal.php
+++
@@ -1,6 +0,0 @@
-<?php
-$LDSickConfirm='Bu iþgöremezlik raporunun bir kopyasý, tanýlar ve tahmini iþgöremezlik gün sayýsý ile birlikte, soldaki sigortacýya derhal gönderilecektir.';
-
-$LDPharmaPrompt='"Kýrmýzý liste" veya "vademacum" gibi bir ilaç rehberi buraya entegre edilebilir.';
-//<p> Ancak halihazýr Alman yasalarý bir demo uyarlamasýnýn entegrasyonunu yasaklamaktadýr.';
-?>
