4d4cdb21da72d982a3f37e4607d3878b77bafc8f
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-05-04 09:01:41 +0000

    branching for gettext

---
+++ care2x/branches/gettext/language/en/lang_en_departments.php
@@ -0,0 +1,27 @@
+<?php
+
+$LDAdmission='Admission';
+$LDHeadline='Headline';
+$LDHeadlines='Headlines';
+$LDManagement='Management';
+$LDHealthTips='Health tips';
+$LDAdmission='Admission';
+$LDNursing='Nursing';
+/* 2003-04-27 EL */
+$LDMedical='MEdical';
+$LDSupport='Support';
+$LDNews='News';
+$LDDepartment='Department';
+$LDPressRelations='Press Relations';
+/* 2003-05-19 EL */
+$LDSelectDept='Select a Department';
+/*2003-06-15 EL*/
+$LDPlsSelectDept='Please Select a Department';
+$LD_AllMedicalDept='____All Medical Departments_____';
+$LDClinic='Clinic';
+
+#2003-10-23 EL
+$LDPlsNameFormal='Please write the Formal Name';
+$LDPlsDeptID='Please write the Department ID';
+$LDPlsSelectType='Please select the type';
+?>
