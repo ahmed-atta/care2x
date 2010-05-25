b4941e5eb3f8e7dc573657b54101203aa929b642
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-05-25 16:30:00 +0000

    not needed

--- care2utu/branches/care2x_tz_b2/gui/smarty_template/templates_c/default/%%-88/%%-882767532/footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2009-05-15 08:14:05
-         compiled from common/footer.tpl */ ?>
-<!--  <?php echo $this->_tpl_vars['SmartyLogo']; ?>
- -->
-</BODY>
-</HTML>
--- care2utu/branches/care2x_tz_b2/installer/tmp/%%707/%%707595198/field_password.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2008-03-20 10:28:55
-         compiled from C:%5Cxampp%5Chtdocs%5CCare2007%5Cinstaller/templates/field_password.tpl */ ?>
-<tr>
-    <td id="field_label"><?php echo $this->_tpl_vars['field']->label; ?>
-</td>
-    <td id="field_value"><input type='password' name='FIELDS[<?php echo $this->_tpl_vars['field']->name; ?>
-]' value='<?php echo $this->_tpl_vars['field']->default; ?>
-' size=30></td>
-</tr>
