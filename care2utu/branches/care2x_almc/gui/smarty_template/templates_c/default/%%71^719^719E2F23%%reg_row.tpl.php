04dc8dfefa8bd57b5b70b4b6e093caea03543cda
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-06-28 17:50:12 +0000

    version of arusha-clinic

---
+++ care2utu/branches/care2x_almc/gui/smarty_template/templates_c/default/%%71^719^719E2F23%%reg_row.tpl.php
@@ -0,0 +1,10 @@
+<?php /* Smarty version 2.6.22, created on 2009-12-28 10:44:14
+         compiled from registration_admission/reg_row.tpl */ ?>
+<tr>
+  <td class="reg_item" <?php echo $this->_tpl_vars['sColSpan1']; ?>
+><?php echo $this->_tpl_vars['sItem']; ?>
+</td>
+  <td class="reg_input" <?php echo $this->_tpl_vars['sColSpan2']; ?>
+><?php echo $this->_tpl_vars['sInput']; ?>
+</td>
+</tr>
---
+++ care2utu/branches/care2x_almc/gui/smarty_template/templates_c/default/%%CB^CB6^CB620954%%footer.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.22, created on 2010-03-08 18:10:27
+         compiled from common/footer.tpl */ ?>
+<!--  <?php echo $this->_tpl_vars['SmartyLogo']; ?>
+ -->
+</BODY>
+</HTML>
