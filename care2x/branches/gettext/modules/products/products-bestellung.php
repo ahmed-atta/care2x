e9313ea15dd47f8754dc9195357e6e210b613b0c
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-06-30 12:13:49 +0000

    see #54

--- care2x/branches/gettext/modules/products/products-bestellung.php
+++ care2x/branches/gettext/modules/products/products-bestellung.php
@@ -42,7 +42,7 @@ $smarty->assign ( 'sWindowTitle', '' );
 
 // Assign frameset source file
 
-$smarty->assign ( 'sHeaderSource', "src=\"products-bestell-hf.php?sid=$sid&lang=$lang&cat=$cat&userck=$userck\"" );
+$smarty->assign ( 'sHeaderSource', "src=\"products-order-header.php?sid=$sid&lang=$lang&cat=$cat&userck=$userck\"" );
 $smarty->assign ( 'sBasketSource', "src=\"products-bestellkorb.php?sid=$sid&lang=$lang&dept_nr=$dept_nr&order_nr=$order_nr&itwassent=$itwassent&cat=$cat&userck=$userck\"" );
 $smarty->assign ( 'sCatalogSource', "src=\"products-bestellkatalog.php?sid=$sid&lang=$lang&dept_nr=$dept_nr&order_nr=$order_nr&cat=$cat&userck=$userck\"" );
 
