117dde495daa6f0f46c13b1ce592b998eaf7377d
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-07-14 12:14:30 +0000

    see #60 - move imgcreator to includes

--- care2x/branches/gettext/include/imgcreator/barcode-wristbands.php
+++ care2x/branches/gettext/include/imgcreator/barcode-wristbands.php
@@ -20,7 +20,7 @@ require_once($root_path.'include/helpers/inc_front_chain_lang.php');
 </head>
 <body onLoad="if(window.focus) window.focus()">
 <?php if(file_exists($root_path."cache/barcodes/en_".$full_en.".png")) : ?>
-<a href="javascript:window.print()" title="<?php echo $LDClickImgToPrint ?>"><img src="./imgcreator/barcode_img_wristbands.php<?php echo URL_REDIRECT_APPEND."&en=$en" ?>" border=0 alt="<?php echo $LDClickImgToPrint ?>"></a>
+<a href="javascript:window.print()" title="<?php echo $LDClickImgToPrint ?>"><img src="barcode_img_wristbands.php<?php echo URL_REDIRECT_APPEND."&en=$en" ?>" border=0 alt="<?php echo $LDClickImgToPrint ?>"></a>
 <?php else : ?>
 <a href="javascript:window.print()" title="<?php echo $LDClickImgToPrint ?>"><img src="<?php echo $root_path ?>classes/barcode/image.php?<?php echo  URL_REDIRECT_APPEND."&code=$full_en&style=68&type=I25&width=180&height=50&xres=2&font=5&pn=$full_en&label=1&form_file=en" ?>" border=0 alt="<?php echo $LDClickImgToPrint ?>"></a>
 <?php endif ?>
