c91a271e3e8b0465f65dddcdb9c52a41e1da5ff6
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-07-17 20:30:58 +0000

    see #61 - move help to separate folder

---
+++ care2x/branches/gettext/include/help/help-router.php
@@ -0,0 +1,22 @@
+<?php 
+error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+ // globalize POST, GET, & COOKIE  vars
+require_once('../helpers/inc_environment_global.php');
+require_once('../helpers/inc_charset_fx.php') // load the charset functions
+?>
+<?php html_rtl($lang); ?>
+<head>
+<?php echo setCharSet(); ?>
+<title>?</title>
+
+</head>
+<frameset cols="20%,*">
+  <frame name="HELPINDEXFRAME" src="help-index.php?helpidx=<?php echo "$helpidx&src=$src&x1=$x1&x2=$x2&x3=$x3&lang=$lang" ?>">
+  <frame name="HELPINFOFRAME" src="help-info.php?helpidx=<?php echo "$helpidx&src=$src&x1=$x1&x2=$x2&x3=$x3&lang=$lang" ?>">
+<noframes>
+<body>
+</body>
+</noframes>
+</frameset>
+
+</html>
