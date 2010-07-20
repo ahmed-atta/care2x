5792141a53c2d5dc1e83a99daf4445bf05a7cc50
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-07-20 14:58:18 +0000

    see #74

--- care2x/branches/gettext/modules/registration_admission/patient_register_show.php
+++ care2x/branches/gettext/modules/registration_admission/patient_register_show.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 require('./roots.php');
 require($root_path.'include/helpers/inc_environment_global.php');
 /**
