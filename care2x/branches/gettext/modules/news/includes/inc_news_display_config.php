8b33ee22225e6578ed431361e130f11ed9a6afaa
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-07-20 17:08:26 +0000

    see #75 - class_config.php unused code, patch from Tim Schofield

--- care2x/branches/gettext/modules/news/includes/inc_news_display_config.php
+++ care2x/branches/gettext/modules/news/includes/inc_news_display_config.php
@@ -4,11 +4,7 @@ require_once($root_path.'include/core/class_globalconfig.php');
 require_once($root_path.'include/core/class_userconfig.php');
 
 
-if($_SESSION['sess_level2_logged'] && $_SESSION['sess_level2_user_id']) {
-    $user_id = $_SESSION['sess_level2_user_id'];
-} elseif ($_SESSION['sess_level1_logged'] && $_SESSION['sess_level1_user_id']) {
-    $user_id = $_SESSION['sess_level1_user_id'];
-} elseif ($_SESSION['sess_user_id']) {
+if ($_SESSION['sess_user_id']) {
     $user_id = $_SESSION['sess_user_id'];
 } else {
     $user_id = $_COOKIE['ck_config'];
