a53f3af2e074fbd8a5522f8f70045f0250c62b01
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-07-21 11:42:41 +0000

    see #76

--- care2x/branches/gettext/modules/pharmacy/pharmacy-pass.php
+++ care2x/branches/gettext/modules/pharmacy/pharmacy-pass.php
@@ -69,7 +69,6 @@ $errbuf="$LDPharmacy $title";
 $minimal=1;
 require($root_path.'include/helpers/inc_passcheck_head.php');
 ?>
-
 <BODY  <?php if (!$nofocus) echo 'onLoad="document.passwindow.userid.focus()"'; echo  ' bgcolor='.$cfg['body_bgcolor']; 
  if (!$cfg['dhtml']){ echo ' link='.$cfg['body_txtcolor'].' alink='.$cfg['body_alink'].' vlink='.$cfg['body_txtcolor']; } 
 ?>>
@@ -87,7 +86,6 @@ require($root_path.'include/helpers/inc_passcheck_head.php');
 <?php
 require($root_path.'include/helpers/inc_load_copyrite.php');
 ?>
-
 
 </FONT>
 
