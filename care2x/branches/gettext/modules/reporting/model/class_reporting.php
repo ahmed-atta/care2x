b4bfe418c8c8fbe9c6036e8d8398897acb77453f
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-08-06 14:56:10 +0000

    see #95 (ignition)

---
+++ care2x/branches/gettext/modules/reporting/model/class_reporting.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package care_api
+ */
+
+/**
+ */
+require_once($root_path.'include/core/class_core.php');
+/**
+ *  Billing Methods methods.
+ *  Note this class should be instantiated only after a "$db" adodb  connector object  has been established by an adodb instance.
+ * @author Robert Meggle
+ * @version beta 0.1
+ * @copyright 2009,2010 Robert Meggle
+ * @package care_api
+ */
+class report extends Core {
+
+	function __construct() {
+	   		return true;
+	   }
+		
+}
+
+?>
