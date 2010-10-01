d964059b9c809478f5a9b231015c7171249ade70
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-10-01 12:20:33 +0000

    fixes #88 - new module sample

--- care2x/branches/gettext/modules/address/citytown_search.php
+++ care2x/branches/gettext/modules/address/citytown_search.php
@@ -1,26 +1,17 @@
 <?php
 error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
-require('./roots.php');
-require($root_path.'include/helpers/inc_environment_global.php');
-/**
-* CARE2X Integrated Hospital Information System Deployment 2.1 - 2004-10-02
-* GNU General Public License
-* Copyright 2002,2003,2004,2005 Elpidio Latorilla
-* elpidio@care2x.org, 
-*
-* See the file "copy_notice.txt" for the licence notice
-*/
+require('../../include/helpers/inc_environment_global.php');
 
 # Default value for the maximum nr of rows per block displayed, define this to the value you wish
 # In normal cases this value is derived from the db table "care_config_global" using the "pagin_address_list_max_block_rows" element.
-define('MAX_BLOCK_ROWS',30); 
+define('MAX_BLOCK_ROWS',30);
 
 $lang_tables[]='search.php';
 define('LANG_FILE','place.php');
 $local_user='aufnahme_user';
-require_once($root_path.'include/helpers/inc_front_chain_lang.php');
+require_once(CARE_BASE.'/include/helpers/inc_front_chain_lang.php');
 # Load the insurance object
-require_once($root_path.'modules/address/model/class_address.php');
+require_once('model/class_address.php');
 $address_obj=new Address;
 
 $breakfile='address_manage.php'.URL_APPEND;
@@ -43,10 +34,11 @@ $GLOBAL_CONFIG=array();
 include_once($root_path.'include/core/class_globalconfig.php');
 $glob_obj=new GlobalConfig($GLOBAL_CONFIG);
 $glob_obj->getConfig('pagin_address_search_max_block_rows');
-if(empty($GLOBAL_CONFIG['pagin_address_search_max_block_rows'])) $GLOBAL_CONFIG['pagin_address_search_max_block_rows']=MAX_BLOCK_ROWS; # Last resort, use the default defined at the start of this page
+if(empty($GLOBAL_CONFIG['pagin_address_search_max_block_rows']))
+$GLOBAL_CONFIG['pagin_address_search_max_block_rows'] = MAX_BLOCK_ROWS; # Last resort, use the default defined at the start of this page
 
 #Load and create paginator object
-require_once($root_path.'include/core/class_paginator.php');
+require_once(CARE_BASE.'include/core/class_paginator.php');
 $pagen=new Paginator($pgx,$thisfile,$_SESSION['sess_searchkey'],$root_path);
 # Adjust the max nr of rows in a block
 $pagen->setMaxCount($GLOBAL_CONFIG['pagin_address_search_max_block_rows']);
@@ -54,7 +46,7 @@ $pagen->setMaxCount($GLOBAL_CONFIG['pagin_address_search_max_block_rows']);
 
 if(isset($mode)&&($mode=='search'||$mode=='paginate')&&!empty($searchkey)){
 
-	# Convert wildcards 
+	# Convert wildcards
 	$searchkey=strtr($searchkey,'*?','%_');
 	# Save the search keyword for eventual pagination routines
 	if($mode=='search') $_SESSION['sess_searchkey']=$searchkey;
@@ -82,137 +74,91 @@ $entry_border_bgcolor='#abcdef';
 $entry_body_bgcolor='#ffffff';
 
 # Start Smarty templating here
- /**
+/**
  * LOAD Smarty
  */
- # Note: it is advisable to load this after the inc_front_chain_lang.php so
- # that the smarty script can use the user configured template theme
+# Note: it is advisable to load this after the inc_front_chain_lang.php so
+# that the smarty script can use the user configured template theme
 
- require_once($root_path.'gui/smarty_template/smarty_care.class.php');
- $smarty = new smarty_care('system_admin');
+require_once(CARE_BASE.'gui/smarty_template/smarty_care.class.php');
+$smarty = new smarty_care('system_admin');
 
 # Title in toolbar
- $smarty->assign('sToolbarTitle',"$LDAddress :: $LDSearch");
+$smarty->assign('sToolbarTitle',"$LDAddress :: $LDSearch");
 $smarty->assign('LDBack', $LDBack);
- $smarty->assign('LDHelp', $LDHelp);
- $smarty->assign('LDClose', $LDClose);
- # href for help button
- $smarty->assign('pbHelp',"javascript:gethelp('address_search.php')");
+$smarty->assign('LDHelp', $LDHelp);
+$smarty->assign('LDClose', $LDClose);
+# href for help button
+$smarty->assign('pbHelp',"javascript:gethelp('address_search.php')");
 
- # href for close button
- $smarty->assign('breakfile',$breakfile);
+# href for close button
+$smarty->assign('breakfile',$breakfile);
 
- # Window bar title
- $smarty->assign('sWindowTitle',"$LDAddress :: $LDSearch");
- 
- # Body onload js
- $smarty->assign('sOnLoadJs','onLoad="document.searchform.searchkey.select()"');
+# Window bar title
+$smarty->assign('sWindowTitle',"$LDAddress :: $LDSearch");
+
+# Body onload js
+$smarty->assign('sOnLoadJs','onLoad="document.searchform.searchkey.select()"');
 
 # Buffer page output
 
 ob_start();
-?>
- <ul>
-
-&nbsp;
-<br>
-<!--  The search mask  -->
-	<table border=0 cellpadding=10 bgcolor="<?php echo $entry_border_bgcolor ?>">
-     <tr>
-       <td>
-	   <?php 
-	   		$searchprompt=$LDSearchPrompt;
-	    	include($root_path.'include/helpers/inc_searchmask.php'); 
-		?></td>
-     </tr>
-   </table>
-<br>
-<?php
-if(is_object($address)){
-	if ($linecount) echo str_replace("~nr~",$totalcount,$LDSearchFound).' '.$LDShowing.' '.$pagen->BlockStartNr().' '.$LDTo.' '.$pagen->BlockEndNr().'.';
-		else echo str_replace('~nr~','0',$LDSearchFound); 
-?>
-<table border=0 cellpadding=2 cellspacing=1>
-  <tr class="wardlisttitlerow">
-       <td><b>
-	  <?php 
-	  	if($oitem=='name') $flag=TRUE;
-			else $flag=FALSE; 
-		echo $pagen->SortLink($LDCityTownName,'name',$odir,$flag); 
-			 ?></b>
-	</td>
-
-      <td><b>
-	  <?php 
-	  	if($oitem=='iso_country_id') $flag=TRUE;
-			else $flag=FALSE; 
-		echo $pagen->SortLink($LDISOCountryCode,'iso_country_id',$odir,$flag); 
-			 ?></b>
-	</td>
-	
-      <td><b>
-	  <?php
-	  	if($oitem=='unece_locode_type') $flag=TRUE;
-			else $flag=FALSE; 
-		echo $pagen->SortLink($LDUNECELocalCode,'unece_locode_type',$odir,$flag); 
-			 ?></b>
-	</td>
-
-      <td><b>
-	  <?php 
-	  	if($oitem=='info_url') $flag=TRUE;
-			else $flag=FALSE; 
-		echo $pagen->SortLink($LDWebsiteURL,'info_url',$odir,$flag); 
-			 ?></b>
-	</td>
-  </tr> 
-<?php
-	$toggle=0;
-	while($addr=$address->FetchRow()){
-		if($toggle) $bgc='wardlistrow2';
-			else $bgc='wardlistrow1';
-		$toggle=!$toggle;
-?>
-  <tr  class="<?php echo $bgc ?>">
-    <td><a href="citytown_info.php<?php echo URL_APPEND.'&retpath=search&nr='.$addr['nr']; ?>"><?php echo $addr['name']; ?></a></td>
-    <td><?php echo $addr['iso_country_id']; ?></td>
-    <td><?php echo $addr['unece_locode']; ?></td>
-    <td><a href="<?php echo $addr['info_url']; ?>"><?php echo $addr['info_url']; ?></a></td>
-</td>
-  </tr> 
-<?php
-	}
-	echo '
-	<tr><td colspan=3>'.$pagen->makePrevLink($LDPrevious).'</td>
-	<td align=right>'.$pagen->makeNextLink($LDNext).'</td>
-	</tr>';
-?>
-  </table>
-<?php
-}else{
-	echo str_replace('~nr~','0',$LDSearchFound);
-} 
-?>
-<p>
-
-<form action="citytown_new.php" method="post">
-<input type="hidden" name="lang" value="<?php echo $lang ?>">
-<input type="hidden" name="sid" value="<?php echo $sid ?>">
-<input type="hidden" name="retpath" value="search">
-<input type="submit" value="<?php echo $LDNeedEmptyFormPls ?>">
-</form>
-</ul>
-<?php
-
-$sTemp = ob_get_contents();
+$searchprompt=$LDSearchPrompt;
+include(CARE_BASE.'include/helpers/inc_searchmask.php');
+$searchMask= ob_get_contents();
 ob_end_clean();
+$smarty->assign('searchMask',$searchMask);
 
-# Assign page output to the mainframe template
+	if( is_object($address) ) {
+		$smarty->assign('foundCity',1);
+		$smarty->assign('URL_APPEND',URL_APPEND);
+		
+		if ($linecount) 
+			$smarty->assign('recordsFound',str_replace("~nr~",$totalcount,$LDSearchFound).' '.$LDShowing.' '.$pagen->BlockStartNr().' '.$LDTo.' '.$pagen->BlockEndNr().'.');
+		else
+			$smarty->assign('recordsFound',str_replace('~nr~','0',$LDSearchFound));
 
-$smarty->assign('sMainFrameBlockData',$sTemp);
- /**
- * show Template
- */
- $smarty->display('common/mainframe.tpl');
+		if($oitem=='name') 
+			$flag=TRUE;
+		else 
+			$flag=FALSE;
+		$smarty->assign('name',$pagen->SortLink($LDCityTownName,'name',$odir,$flag));
 
+		if($oitem=='iso_country_id') $flag=TRUE; else $flag=FALSE;
+		$smarty->assign('iso_country_id',$pagen->SortLink($LDISOCountryCode,'iso_country_id',$odir,$flag));
+		
+		if($oitem=='unece_locode_type') $flag=TRUE; else $flag=FALSE;
+		$smarty->assign('unece_locode_type',$pagen->SortLink($LDUNECELocalCode,'unece_locode_type',$odir,$flag));
+
+		if($oitem=='info_url') $flag=TRUE; else $flag=FALSE;
+		$smarty->assign('info_url',$pagen->SortLink($LDWebsiteURL,'info_url',$odir,$flag));
+
+		$result = array();
+		while( $addr = $address->FetchRow() ) {
+			$result[] = array(
+				'nr' 				=> $addr['nr'],
+				'name'				=> $addr['name'],
+				'iso_country_id'	=> $addr['iso_country_id'],
+				'unece_locode'		=> $addr['unece_locode'],
+				'info_url'		=> $addr['info_url'],
+			);
+		}
+		$smarty->assign('result',$result);
+		
+		$smarty->assign('prevLink',$pagen->makePrevLink($LDPrevious));
+		$smarty->assign('nextLink',$pagen->makeNextLink($LDNext));
+	}else{
+		$smarty->assign('NothingFound',str_replace('~nr~','0',$LDSearchFound));
+	}
+
+
+$smarty->assign('lang',$lang);	
+$smarty->assign('sid',$sid);	
+$smarty->assign('LDNeedEmptyFormPls',$LDNeedEmptyFormPls);	
+	
+// assign page output to the mainframe template
+$smarty->assign('sMainBlockIncludeFile',__DIR__ . '/view/citytown_search.tpl');
+
+//$smarty->compile_check = true; $smarty->debugging = true; $smarty->display('debug.tpl');
+$smarty->display('common/mainframe.tpl');
 ?>
