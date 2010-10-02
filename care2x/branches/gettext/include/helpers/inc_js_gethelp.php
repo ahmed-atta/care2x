20cbaec9f48b538cab5a0b5c5fb2fc9c143b3ad6
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-10-02 16:59:10 +0000

    compatibility with old help system

--- care2x/branches/gettext/include/helpers/inc_js_gethelp.php
+++ care2x/branches/gettext/include/helpers/inc_js_gethelp.php
@@ -1,9 +1,9 @@
 <script language="javascript" >
 <!-- 
-function gethelp(x,s,x1,x2,x3,x4,module)
+function gethelp(x,s,x1,x2,x3,x4)
 {
 	if (!x) x="";
-	urlholder="<?php echo CARE_GUI ; ?>include/help/help-router.php<?php echo URL_REDIRECT_APPEND ?>&helpidx="+x+"&src="+s+"&x1="+x1+"&x2="+x2+"&x3="+x3+"&x4="+x4+"&module="+module;
+	urlholder="<?php echo CARE_GUI ; ?>include/help/help-router.php<?php echo URL_REDIRECT_APPEND ?>&helpidx="+x+"&src="+s+"&x1="+x1+"&x2="+x2+"&x3="+x3+"&x4="+x4;
 	helpwin=window.open(urlholder,"helpwin","width=790,height=540,menubar=no,resizable=yes,scrollbars=yes");
 	window.helpwin.moveTo(0,0);
 }
