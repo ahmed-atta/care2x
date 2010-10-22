f95075d70b150ede9f86d28d47b5e7a60e82288a
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-10-22 12:30:36 +0000

    working on PHP 5.3 compatibility on this version (non stable, development)

--- care2x_tz/trunk/index.php
+++ care2x_tz/trunk/index.php
@@ -1,33 +1,33 @@
 <?php
 /*
 CARE 2X Integrated Information System for Hospitals and Health Care Organizations and Services
-Care 2002, Care2x, Copyright (C) 2002,2003,2004,2005  Elpidio Latorilla
+Care 2002, Care2x, Copyright (C) 2002,2003,2004,2005,2006  Elpidio Latorilla
 
-Deployment 2.1 - 2004-10-02
-
+Deployment 2.2 - 2006-07-10
+								
 This script(s) is(are) free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation; either
 version 2 of the License, or (at your option) any later version.
-
+																  
 This software is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
-
+											   
 You should have received a copy of the GNU General Public
 License along with this script; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+																		 
 Copy of GNU General Public License at: http://www.gnu.org/
-
+													 
 Source code home page: http://www.care2x.org
 Contact author at: elpidio@care2x.org
 
 This notice also applies to other scripts which are integral to the functioning of CARE 2X within this directory and its top level directory
 A copy of this notice is also available as file named copy_notice.txt under the top level directory.
 */
-//error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 define('FROM_ROOT',1);
 
 if(!isset($mask)) $mask=false;
@@ -39,7 +39,7 @@ if(!isset($sid)) $sid='';
 require('./roots.php');
 require('./include/inc_environment_global.php');
 
-//$db->debug=1;
+//$db->debug=FALSE;
 
 # Register global session variables
 if(!session_is_registered('sess_user_name')) session_register('sess_user_name');
@@ -93,9 +93,9 @@ function configNew(&$bn,&$bv,&$f,$i,&$uid)
   # We disable the error reporting, because Konqueror 3.0.3 causes a  runtime error output that stops the program.
   #  could be a bug in phpsniff .. hmmm?
   $old_err_rep= error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
-
+  
   # Function rewritten by Thomas Wiedmann to use phpSniff class
-
+  
   # initialize some vars
   if(!isset($UA)) $UA = '';
   if(!isset($cc)) $cc = '';
@@ -122,12 +122,12 @@ function configNew(&$bn,&$bv,&$f,$i,&$uid)
   $uid=uniqid('');
   $f='CFG'.$uid.microtime().'.cfg';
 
-   # Return previous error reporting
+   # Return previous error reporting 
    error_reporting($old_err_rep);
 }
 
 /**
-* Create simple session id (sid), save a encrpyted  sid to a cookie with a dynamic name
+* Create simple session id (sid), save a encrpyted  sid to a cookie with a dynamic name 
 * consisting of concatenating "ck_sid" and the sid itself.
 * For more information about the encryption class, see the proper docs of the pear's "hcemd5.php" class.
 */
@@ -138,7 +138,8 @@ $ck_sid_buffer='ck_sid'.$sid;
 include('include/inc_init_crypt.php'); // initialize crypt
 $ciphersid=$enc_hcemd5->encodeMimeSelfRand($sid);
 setcookie($ck_sid_buffer,$ciphersid);
-$HTTP_COOKIE_VARS[$ck_sid_buffer]=$ciphersid;
+$_COOKIE[$ck_sid_buffer]=$ciphersid;
+
 
 #
 # Simple counter, counts all hits including revisits
@@ -147,18 +148,11 @@ $HTTP_COOKIE_VARS[$ck_sid_buffer]=$ciphersid;
 #
 // include('./counter/count.php');
 
-include('include/inc_init_main.php');
 
-if( $dbname == '')
-{
-	header('Location:installer/install.php');
-}
-
-
-if((isset($boot)&&$boot)||!isset($HTTP_COOKIE_VARS['ck_config'])||empty($HTTP_COOKIE_VARS['ck_config'])) {
+if((isset($boot) && $boot)||!isset($_COOKIE['ck_config'])||empty($_COOKIE['ck_config'])) {
     configNew($bname,$bversion,$user_id,$ip,$cfgid);
 } else {
-    $user_id=$HTTP_COOKIE_VARS['ck_config'];
+    $user_id=$_COOKIE['ck_config'];
 }
 
 #
@@ -182,25 +176,41 @@ $glob_cfg=new GlobalConfig($GLOBALCONFIG);
 
 # Get the global config for language usage
 $glob_cfg->getConfig('language_%');
-# Get the global config for frames
+# Get the global config for frames 
 $glob_cfg->getConfig('gui_frame_left_nav_width');
-# Get the global config for lev nav border
+# Get the global config for lev nav border 
 $glob_cfg->getConfig('gui_frame_left_nav_border');
 
 $savelang=0;
 /*echo $GLOBALCONFIG['language_non_single'];
 while (list($x,$v)=each($GLOBALCONFIG)) echo $x.'==>'.$v.'<br>';
 */
-# Start checking language properties
+# Start checking language properties 
+
 if(!$GLOBALCONFIG['language_single']) {
     # We get the language code
     if($_chg_lang_&&!empty($lang)) {
 		    $savelang=1;
 	}else{
 		//echo $lang=$USERCONFIG['lang'];
-        if($USERCONFIG['lang']) $lang=$USERCONFIG['lang'];
-			    else  include('chklang.php');
-	 }
+		/* if($USERCONFIG['lang']) $lang=$USERCONFIG['lang'];
+		 *   else  include('chklang.php');
+		 * */
+		
+		 
+		 if(empty($USERCONFIG['lang']) || !isset($USERCONFIG['lang'])) {
+		 	// USERCONFIG contain a value
+		 	if (empty($lang) || !isset ($lang)){
+		 		// langugae selection is not given, set the default language:
+		 		$lang=$USERCONFIG['lang'];
+		 	} else {
+		 		// no language selection given, so try to find out what language is used on this server
+		 		include('chklang.php');
+		 	}
+		 	
+		 }
+			  
+	 } 
 }else{
 
     # If single language is configured, we get the user configured lang
@@ -212,40 +222,26 @@ if(!$GLOBALCONFIG['language_single']) {
             $lang=$GLOBALCONFIG['language_default'];
 		} else {
 	        $lang=LANG_DEFAULT; # Comes from inc_environment_global.php, the last chance, usually set to "en"
-	    }
+	    }	
 	}
 }
 
 #
 # After having a language code check if the critical scripts exist and set warning
 #
-$createwarn=file_exists('create_admin.php');
-$initwarn=file_exists('./install/initialize.php');
-$md5warn=file_exists('./install/encode_pw_md5.php');
-$installwarn=file_exists('./install/install.php');
-if($createwarn||$installwarn||$md5warn){
+$installerwarn=file_exists('./installer/install.php');
+if($installerwarn){
 	#
 	# Load necessary language tables
 	#
 	$lang_tables[]='create_admin.php';
 	include_once('./include/inc_load_lang_tables.php');
-	include_once('include/inc_charset_fx.php');
-	if($createwarn){
-		include('./include/inc_create_admin_warning.php');
-	}
-	if($initwarn){
-		include('./include/inc_init_warning.php');
-	}
-	if($md5warn){
-		include('./include/inc_md5_warning.php');
-	}
-	if($installwarn){
-		include('./include/inc_install_warning.php');
-	}
+	include_once('./include/inc_charset_fx.php');
+	include_once('./include/inc_installer_warning.php');
 	#
-	# exit to avoid running the program
+	# redirect to the installer page after timeout of 5 seconds 
 	#
-	exit;
+	die('<meta http-equiv="refresh" content="5; url=./installer/">');
 }
 
 #
@@ -266,14 +262,14 @@ if(file_exists($lang_file)) {
 #
 # The language detection is finished, we save it to session
 #
-$HTTP_SESSION_VARS['sess_lang']=$lang;
+$_SESSION['sess_lang']=$lang;
 
 /*$ck_lang_buffer='ck_lang'.$sid;
 setcookie($ck_lang_buffer,$lang);*/
 
-/*$HTTP_COOKIE_VARS[$ck_lang_buffer]=$lang;*/
+/*$_COOKIE[$ck_lang_buffer]=$lang;*/
 	 //echo $mask;
-if((isset($mask)&&$mask)||!$config_exists||$savelang) {
+if((isset($mask) && $mask)||!$config_exists||$savelang) {
 	if(!$config_exists) {
 
 		//$cfg_obj->getConfig('default');
@@ -305,13 +301,13 @@ if((isset($mask)&&$mask)||!$config_exists||$savelang) {
 #
 # save user_id to session
 #
-$HTTP_SESSION_VARS['sess_user_id']=$user_id;
-if(empty($HTTP_SESSION_VARS['sess_user_name'])) $HTTP_SESSION_VARS['sess_user_name']='default';
+$_SESSION['sess_user_id']=$user_id;
+if(empty($_SESSION['sess_user_name'])) $_SESSION['sess_user_name']='default';
 
 #
 # set the initial session timeout start value
 #
-$HTTP_SESSION_VARS['sess_tos']=date('His');
+$_SESSION['sess_tos']=date('His');
 
 #
 # Load character set fx
@@ -355,14 +351,13 @@ if($mask == 2){
 
 	if($lang=='ar'||$lang=='fa') $smarty->assign('sBaseFramesetTemplate','common/frameset_floatingmenu_rtl.tpl');
 		else $smarty->assign('sBaseFramesetTemplate','common/frameset_floatingmenu_ltr.tpl');
-
+	
 	$smarty->assign('sMenuFrameSource','src="main/menubar2.php"');
 	$smarty->assign('sStartFrameSource',"src=\"main/indexframe.php?boot=1&lang=$lang&egal=$egal&cookie=$cookie&sid=$sid&mask=2\"");
 
 }else{
-
 	$smarty->assign('sStartFrameSource',"src = \"main/indexframe.php?boot=1&mask=$mask&lang=$lang&cookie=$cookie&sid=$sid\"");
-
+	
 	#
 	# Assign frame dimensions
 	#
