afcd79a6f058ca82493a01e60b9f8db8477477b3
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-10-22 18:49:28 +0000

    recalibrated the version information

--- care2x_tz/trunk/modules/reporting_tz/gui/gui_OPD_diagnostic.php
+++ care2x_tz/trunk/modules/reporting_tz/gui/gui_OPD_diagnostic.php
@@ -174,7 +174,7 @@ $rep_obj->Display_OPD_Diagnostic($start, $end);
 					</script>
 
 	
-					 <a href="http://www.care2x.org" target=_new>CARE2X 2nd Generation pre-deployment 2.0.2</a> :: <a href="../../legal_gnu_gpl.htm" target=_new> License</a> ::
+					 <a href="http://www.care2x.org" target=_new>CARE2X 2nd Generation pre-deployment 2.0.5.3</a> :: <a href="../../legal_gnu_gpl.htm" target=_new> License</a> ::
 					 <a href=mailto:info@care2x.org>Contact</a>  :: <a href="../../language/en/en_privacy.htm" target="pp"> Our Privacy Policy </a> ::
 					 <a href="../../docs/show_legal.php?lang=$lang" target="lgl"> Legal </a> ::
 					 <a href="javascript:openCreditsWindow()"> Credits </a> ::.<br>
