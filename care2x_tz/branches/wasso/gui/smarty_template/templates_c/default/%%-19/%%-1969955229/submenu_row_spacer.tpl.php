7df4547e2a9e42ed83b64218f63720689ad585a9
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2010-11-08 14:15:44 +0000

    removing really old version - will be replaced with working soon by a working one.

--- care2x_tz/branches/wasso/gui/smarty_template/templates_c/default/%%-19/%%-1969955229/submenu_row_spacer.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2009-09-23 15:11:19
-         compiled from common/submenu_row_spacer.tpl */ ?>
-<TR  height=1>
-	<TD colSpan=3 class="vspace"><IMG height=1 src="../../gui/img/common/default/pixel.gif" width=5></TD>
-</TR>
--- care2x_tz/branches/wasso/gui/smarty_template/templates_c/default/%%-88/%%-882767532/footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2009-09-23 15:10:43
-         compiled from common/footer.tpl */ ?>
-<!--  <?php echo $this->_tpl_vars['SmartyLogo']; ?>
- -->
-</BODY>
-</HTML>
