caf5dd0c2149af8c2de9163360cf7f72a1b25188
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-27 22:49:51 +0000

    moved drg module to new language system

---
+++ care2x/branches/gettext/modules/drg/language/en/drg.php
@@ -0,0 +1,195 @@
+<?php
+$LDIcd10='ICD_10'; // ICD = International Codes of Diseases  !!
+$LDOps301='OPS_301'; // OPS = Opertionenschlssel - German code for surgical operations !!
+$LDBack2Logbook='Go back to logbook';
+$LDIcd10Search='ICD-10 Search';
+$LDSearch4ICD10='Search for ICD-10 Codes';
+$LDSearch4OPS301='Search for OPS-301 Codes';
+$LDDescription='Description';
+$LDMainAuxDiag='Main\Auxiliary';
+$LDMainAuxOp='Main\Auxiliary procedure';
+$LDDiagBy='Diagnosed by';
+$LDDoneBy='Done by';
+$LDInclusive='Inclusions';
+$LDExclusive='Exclusions';
+$LDNotes='Notes';
+$LDRemarks='Remarks';
+$LDExtraCodes='Extra codes or coding';
+$LDAddCodes='Additional notes or codes';
+$LDApplySelection='Apply selections';
+$LDSGBV='SGB_V';  // !! Do not translate !!
+$LDLocalization='Localization';
+$LDOpsIntern='Intern code';
+$LDSearchCodes='Search codes';
+$LDQuickList='Quicklist';
+$LDConvert2IcdOps='->ICD-ICPM';  // !! Do not translate !!
+
+$LDSearch='Search';
+$LDOperation='Operation';
+$LDAlertSureDelete='Are you sure you want to delete this entry?';
+
+$LDAux='A';   // A = Auxiliary
+$LDMain='Main'; // M = Main
+$LDRight='Right'; // R = Right
+$LDLeft='Left';   // L = Left
+$LDBoth='Both';  // B = Both or (both sides)
+
+$LDDeleteEntry='Delete entry';
+$LDNoQuickList='The quicklist is empty at the moment.';
+$LDClick2Search='Please click here to go to search.';
+$LDPossibleCodes='Possible ICD and ICPM Codes';
+/*2003-05-25 EL */
+$LDCategory='Category';
+$LDLoc='Loc.';
+$LDBy='By';
+$LDMostResponsible='Most responsible';
+$LDAssociated='Associated';
+$LDNosocomial='Nosocomial';
+$LDIatrogenic='Iatrogenic';
+$LDOther='Other';
+$LDMostResp_s='M';
+$LDAssociated_s='A';
+$LDNosocomial_s='N';
+$LDIatrogenic_s='I';
+$LDOther_s='O';
+
+$LDBothSides='Both sides';
+$LDLeft_s='S';
+$LDRight_s='R';
+$LDBothSides_s='B';
+/* 2003-05-29 EL*/
+$LDQMarks='??????';
+$LDNonSpecifiedGroup='Not-yet-grouped';
+$LDSpecifyGroup='Specify group';
+$LDCreateGroupName='Create new group name';
+$LDSelect='Select';
+/*2003-06-01 EL*/
+$LDUseToGroupItems='Use this to group the not-yet-grouped items';
+$LDItemsDegrouped='Deleting this group will unlink the items linked to it.';
+$LDOptions='Options';
+/*2003-06-01 EL*/
+
+$LDCreateInternDRGGroup='Create new internal DRG group';
+$LDInternalCodeNr='Internal code Nr.';
+$LDSynonyms='Synonyms or keywords';
+$LDSeparateCommas='Separate words with commas';
+$LDIsSubGroup='Is sub-group?';
+$LDParentCodeNr='Parent Code Nr.';
+$LDIfSubGroup='If group is sub-group';
+$LDAuxillaryNotes='Auxillary notes';
+$LDStdCodeNr='Standard code Nr.';
+$LDIfAvailable='If available';
+$LDYes='Yes';
+$LDNo='No';
+$LDExtraNotes='Extra notes';
+$LDAlertNoCode='Please enter the groups code.';
+$LDAlertNoDescription='Please enter the codes description.';
+/*2003-06-07 EL*/
+$LDCode='Code';
+/*2003-06-13 EL*/
+$LDAddGroupEncounter='Add group to encounter';
+$LDMain_s='M';
+$LDSupplemental='Supplemental';
+$LDSupp_s='S';
+$LDCreateNew='Create new';
+/*2006-09-03 Potur*/
+$LDSureToCreateNewGr='Are you sure you want to create a new group code?';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
+
+$LDPlsEnterEndDate='Please enter end date';
+$LDPlsEnterStartDate='Please enter start date';
+$LDPlsEnterConfirmDate='Please enter confirm date';
+$LDPlsEnterDiagnosis='Please enter the diagnosis';
+$LDPlsSelectStyle='Please select a style';
+$LDCheckParamFirst='Please check first the parameter you want to display';
+# Do not edit ~ward_id~
+$LDChkWardConflict='This patient was initially assigned to a different ward (~ward_id~). Please clarify this first with the
+admission desk before proceeding to admit this patient in your ward. Thank you.';
+$LDSelectRoomBed='Please select the room and bed below to assign to this patient. Thank you';
+$LDSureAssignRoomBed='Are you really sure, you want to assign this room and bed to the patient ?';
+$LDWhereToTransfer='Where should the patient be transferred to?';
+$LDErrorOccured='An error occured.';
+$LDTryOrNotifyEDP='Please try again or notify your EDP department if the problem persists. Thank you.';
+# 2003-07-30 EL
+$LDPlsEnterDate='Please enter a date';
+$LDPlsEnterDoctor='Please enter a  physicians or surgeons name';
+$LDPlsEnterPurpose='Please enter the purpose';
+$LDAppointNotToday='This appointment is NOT scheduled for today!';
+$LDSureAdmitAppoint='Are you sure you want to admit this appointment for today?';
+$LDPatientIsDischarged='This patient is already discharged';
+$LDPersonIsAdmitted='This person is currently admitted';
+$LDEncounterClosed='This encounter is now closed';
+# 2003-08-02 EL
+$LDNoOutpatients='There are no outpatients yet.';
+$LDGoToAppointments='Please click here to check the pending appointments.';
+$LDSureTransferPatient='Are you really sure, you want to transfer this patient ?';
+$LDClkTransfer='Click to transfer patient';
+$LDSureTakeOverPatient='Are you sure? You want to take over this patient?';
+$LDChkClinicConflict='This patient was preassigned to another clinic\department (~dept_id~).
+Please clarify this first with the admission desk or other clinic before proceeding to take over  this patient in your clinic. Thank you.';
+$LDSureToCancel='Are you sure? You want to cancel this?';
+$LDPlsEnterFullName='Please enter your full name';
+$LDOk='Ok';
+$LDAdmissionCancelled='Admission was cancelled successfully.';
+$LDCancelError='An error happened! Cancel was aborted.';
+$LDTellEdpIfPersist='Notify your EDP department if the problem persists. Thank you.';
+$LDPlsFindPersonFirst='Please find the person first.';
+$LDEnterRelatedInfo='If available, enter related information here';
+$LDRelatedEncNr='Related encounters nr.';
+$LDRelatedDocsIds='Related documents IDs';
+$LDSeparateComma='Separate with commas';
+$LDPlsReadThis='Please read this';
+# 2003-08-11 EL
+$LDPersonIsEmployed='This person is currently this hospitals employee!';
+# 2003-08-24 EL
+$LDCachedInfo='This is a cached information!';
+$LDClkNoCache='Click this to get the non-cached information.';
+
+# 2003-08-25 EL
+$LDPlsEnterReport='Please enter your report or notes';
+$LDPlsEnterDosage='Please enter the dosage';
+$LDPlsEnterMedicine='Please enter the name of medicine';
+$LDPlsEnterMedType='Please enter the type of medicine';
+$LDPlsSelectAppType='Please select the type of application';
+$LDPlsSelectPrescType='Please select the type of prescription';
+$LDPlsEnterValue='Please enter a value';
+$LDPlsSelectUnitMsr='Please select unit of measure';
+
+# 2003-08-26 EL
+$LDEntryInvalidChar='The entry has an invalid value!';
+$LDNotNegValue='Negative value is not allowed!';
+$LDOptional='Optional';
+$LDSaveAnyway='Do you really want to save data anyway';
+#2003-08-27 EL
+$LDDeathDateIs='The entered date of death is';
+$LDIsCorrect='Is that correct?';
+$LDProceedSave='Do you want to proceed save?';
+#2003-08-28 EL
+$LDPlsSelectWard='Please select a ward or station';
+# 2003-09-20 EL
+$LDPlsEnterTherapy='Please enter the therapy.';
+$LDPlsMedicalAdvice='Please specify whether the patient got medical advice or not.';
+# 2003-09-23 EL
+$LDPlsEnterJobFunction='Please enter the job function.';
+#2003-04-10 EL
+$LDPlsFillInfo='Please fill in the fields marked red';
+$LDPlsEnterLocalization='Please enter the localization';
+$LDPlsEnterNotes='Please enter notes';
+$LDPlsEnterClassification='Please enter the classifications';
+$LDPlsEnterStartTime='Please enter start time';
+$LDPlsEnterEndTime='Please enter end time';
+$LDPlsEnterScrubNurse='Please enter scrub nurse info';
+$LDPlsEnterORNr='Please enter the OR ID or number';
+$LDWrongLoginPW='Your login or password is wrong';
+$LDPlsORNr='Please enter the OR number';
+
+#2004-08-06 EL
+$LDInvalidDate ='The entered date was invalid.';
+$LDNoPastDate = 'Dates in the past are not accepted.';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
