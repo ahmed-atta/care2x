b4d5dd68c8d1f86c9270cd04ee8570fb40ff2fc4
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-28 13:19:37 +0000

    moved myintranet to new language system

---
+++ care2x/branches/gettext/modules/myintranet/language/en/myintranet.php
@@ -0,0 +1,437 @@
+<?php
+$LDUserPrompt='Username';
+$LDPwPrompt='Password';
+$LDSubmitBut='Continue...';
+$LDPwNeeded='Password needed';
+$LDIntroTo='Introduction to editing news articles in';
+$LDWhatTo='What and how can I edit news articles in';
+
+$LDWrongEntry='Wrong entries!';
+$LDNoAuth='You have no access permission!';
+$LDAuthLocked='Your access permission is locked!';
+
+$LDNewData='New Data';
+$LDEdit='Edit';
+$LDAdmit='Admit';
+$LDSearch='Search';
+$LDArchive='Archive';
+$LDAdmission='Patient Admission';
+$LDAdmTargetEntry='Admission';
+$LDAdmTargetSearch='Search Admitted Patients';
+$LDAdmTargetArchive='Research in Archive';
+$LDAdmWantEntry='I need to admit a patient';
+$LDAdmWantSearch='I am looking for a patient';
+$LDAdmWantArchive='I need to research in the archive';
+$LDAdmHow2Enter='How to admit a patient';
+$LDAdmHow2Edit='How to edit a patients data';
+$LDAdmHow2Search='How to find a certain patients data';
+$LDAdmHow2Archive='How to research in the archives';
+
+$LDMedocs='Medocs';
+$LDMedocsHow2Enter='How to start a new medocs document';
+$LDMedocsHow2Edit='How to edit a medocs document';
+$LDMedocsHow2Search='How to find a certain medocs document';
+$LDMedocsHow2Archive='How to research in the medocs archives';
+
+$LDIntro2='Introduction to';
+$LDWhat2Do='How to do what in';
+$LDHowManage='How to manage the directory';
+$LDHowEnter='How to enter new phone information';
+
+$LDPhoneDir='Phone directory';
+$LDMakeDutyPlan='Create dutyplan';
+$LDDocsList='Doctors list';
+$LDNursingManage='Ward management';
+
+$LDNursingStation='Nursing ward';
+$LDOrDocu='OR Documentation';
+$LDOrLogBook='OR nursing logbook';
+$LDOrNursing='OR nursing';
+$LDCreate='Create';
+$LDPersonList='staff list';
+$LDRepairbotActivate='Activate Repairbot';
+$LDQBotActivate='Activate Q-Bot';
+
+$LDMedLab='Medical laboratory';
+$LDCancel='Cancel';
+
+$LDPharmacy='Pharmacy';
+$LDOrderArchive='Archive';
+$LDPharmaOrder='Ordering';
+$LDOrderCat='My product catalog';
+$LDPharmaDb='Databank';
+$LDPharmaOrderBot='Activate OrderBot';
+
+$LDMedDepot='Medical Depot';
+$LDMediBotActivate='Activate Medibot';
+$LDSqlDb='SQL Databank';
+$LDManageAccess='Manage Access Rights';
+$LDListAll='List all';
+$LDSystemLogin='System Administrator Login';
+$LDSeeData='Display data';
+$LDNewData='Enter new data';
+
+$LDWelcome='Welcome';
+$LDLogin='Login';
+$LDChangeSaved='The changes were saved.';
+$LDPcID='Current identity of this computer';
+$LDDept='Department';
+$LDWard='Ward';
+$LDWardOR='Ward room or OR number';
+$LDPhoneNr='Rooms telephone number';
+$LDIntercomNr='Rooms intercom number';
+$LDPcIP='IP address of this computer';
+$LDSave='Save changes';
+$LDNoChange='Do not change current settings';
+$LDClose='Close';
+$LDLogout='Log out';
+$LDNewLogin='New login';
+$LDLogoutConfirm='Do you really want to log out?';
+$LDOK='OK';
+$LDYes='Yes';
+$LDNotReally='No, not really.';
+$LDDOCScheduler='DOC Scheduler';
+$LDORNOCScheduler='ORNOC Scheduler';
+$LDFotolab='Photolab';
+/* 2002-09-08 EL */
+$LDTestRequest='Diagnostic Test Request';
+
+$LDTestType['patho']='Pathology';
+$LDTestType['chemlabor']='Medical Laboratory';
+$LDTestType['baclabor']='Bacteriological Laboratory';
+$LDTestType['blood']='Blood Bank';
+$LDTestType['radio']='Radiology';
+$LDTestType['sono']='Sonography';
+$LDTestType['allamb']='General Ambulatory';
+$LDTestType['unfamb']='Emergency Ambulatory';
+$LDTestType['nuklear']='Nuclear Diagnostics';
+$LDTestType['inmed']='Internal Medicine';
+
+$LDBloodOrder='Request for blood products';
+/* 2002-09-22 EL */
+$LDPendingRequest='Pending request';
+/* 2002-10-06 EL*/
+$LDLoggedOut='Logged out';
+/* 2003-03-18 EL */
+$LDNursesList='Nurses List';
+$LDAppointments='Appointments';
+/* 2003-04-27 */
+$LDInsuranceCoManager='Insurance Company Manager';
+$LDAddressMngr='Address Manager';
+/* 2003-04-30 EL*/
+$LDBilling='eComBill';
+/* 2003-05-18 EL */
+$LDPlsContactEDP='Please contact the EDP department.';
+$LDPlsTryAgain='Please try again';
+/* 2003-07-11 EL */
+$LDAdministration='Administration';
+$LDOutpatientClinic='Outpatient clinic';
+$LDUploadDicom='Upload DICOM images';
+$LDViewDicom='View DICOM images';
+# 2003-08-22 EL
+$LDYellowList='Yellow List';
+$LDRedList='Red List';
+#2003-11-22 EL
+$LDAdvancedSearch='Advanced search';
+$LDNewPerson='Register new person';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
+$LDNews='News';
+$LDNewsTxt='Write headline news articles';
+$LDMemo='Memo';
+$LDMemoTxt='Read or write a memo';
+
+$LDClose='Close';
+$LDCancel='Cancel';
+$LDResetEntry='Erase entries';
+$LDSave='Save';
+$LDReset='Reset';
+
+$LDNewSearch='New search';
+$LDSearch='Search';
+
+$LDContinue='Continue';
+$LDBack='Go back';
+
+$LDOn='On';
+$LDAt='At';
+
+$LDNew='New';
+$LDEdit='Edit';
+$LDCreate='Create';
+
+$LDPastAll='All';
+$LDSelect='Select';
+
+$LDOrderArchive='Archive';
+
+$LDGoodMorning='Good morning';
+$LDGoodDay='Hello, nice to see you';
+$LDGoodEvening='Good evening';
+
+$LDDate='Date';
+$LDDept='Department';
+$LDPrio='Priority';
+
+
+$LDPageTop='Back to top.';
+$LDPreview='Preview';
+$LDUpdateData='Update or edit';
+
+$LDNoBack='No. Go back.';
+$LDOK='OK';
+$LDName='Name';
+$LDPassword='Password';
+$LDUserId='User login name';
+$LDChange='Change';
+$LDLock='Lock';
+$LDUnlock='Unlock';
+$LDDelete='Delete';
+$LDYesSure='Yes, I\'m sure.';
+$LDKeywordPrompt='Enter a search keyword';
+$LDWelcome='Welcome';
+
+
+$LDLogin='Login';
+
+$LDFirstName='Given name';
+
+$LDOptions='Options';
+$LDHelp='Help';
+$LDSent='Sent';
+$LDDrafts='Drafts';
+$LDDate='Date';
+$LDTime='Time';
+$LDSize='Size';
+$LDFrom='From';
+$LDTo='To';
+$LDSend='Send';
+$LDChange='Change';
+$LDInsertAddr='Insert';
+$LDRead='Read';
+$LDBack2='Back to';
+$LDReply2='Reply to';
+$LDPrint='Print';
+$LDMoreInfo='More info';
+$LDTakeOver='Take over';
+$LDJustReset='Reset';
+$LDMaximum='Maximum';
+
+$LDSpexFunctions='Special Functions';
+$LDDutyPlanOrg='Duty Plan Organizer';
+$LDDutyPlanOrgTxt='Duty planner and organizer, Bundy machine';
+$LDStandbyDuty='Standby duty';
+$LDStandbyDutyTxt='Documenting work performance in standby duty';
+$LDHandStat='Hand surgery statistics';
+$LDHandStatTxt='Documenting hand surgery operations';
+$LDCalendar='Calendar';
+$LDCalendarTxt='A multifunction calender';
+$LDBlackBoard='Blackboard';
+$LDBlackBoardTxt='Post or search any information';
+$LDForum='Forum';
+$LDForumTxt='Post, search, read discussion, ideas, or comments, etc.';
+$LDCalc='Calculator';
+$LDCalcTxt='An online calculator';
+$LDClock='Clock';
+$LDDigitalClock='A digital online clock';
+$LDColorOpt='Color options';
+$LDColorOptTxt='Set the colors for background or text.';
+$LDColorOptExt='Extended color options';
+$LDColorOptExtTxt='Set the colors for hyperlinks, mouseovers, etc.';
+$LDMyIntranet='myIntranet';
+$LDMyIntranetTxt='My personal planner, organizer, diary, memo, calendar, etc.';
+$LDAccessPw='Access password';
+$LDAccessPwTxt='Change or update access password';
+$LDWebCam='Video Security';
+$LDWebCamTxt='Intranet webcamera security surveillance';
+$LDPhotoLab='Photolab';
+$LDPhotoLabTxt='Save, document, index photos of patients';
+$LDImmunizationMngr='Immunization';
+$LDImmunizationMngrTxt='Immunization';
+
+$LDBundyMachine='Bundy machine';
+$LDDutyPlanner='Duty planner';
+$LDPrevMonth='Previous month';
+$LDNextMonth='Next month';
+
+$monat[0]='';
+$monat[1]='January';
+$monat[2]='February';
+$monat[3]='March';
+$monat[4]='April';
+$monat[5]='May';
+$monat[6]='June';
+$monat[7]='July';
+$monat[8]='August';
+$monat[9]='September';
+$monat[10]='October';
+$monat[11]='November';
+$monat[12]='December';
+
+$LDEntry='Entry';
+$LDExit='Exit';
+$LDRemarks='Remarks';
+
+$tage[0]='S';
+$tage[1]='M';
+$tage[2]='T';
+$tage[3]='W';
+$tage[4]='T';
+$tage[5]='F';
+$tage[6]='S';
+
+$tagename[0]='Monday';
+$tagename[1]='Tuesday';
+$tagename[2]='Wednesday';
+$tagename[3]='Thursday';
+$tagename[4]='Friday';
+$tagename[5]='Saturday';
+$tagename[6]='Sunday';
+
+$LDTotal='Total';
+$LDClk4Options='Click for options';
+$LDMonth='Month';
+$LDYear='Year';
+$LDGO='GO';
+$LDPlus1Year='Plus 1 year';
+$LDMinus1Year='Minus 1 year';
+$LDDirectDial='Direct select';
+$LDFor='for';
+
+$LDShowMyCalendar='Show my duty in the calendar.';
+$LDShowMySched='Show my appointments.';
+$LDShowMyOvertime='Show when I worked overtime.';
+$LDShowMyWorkTime='Show details of my work schedule.';
+$LDPlanSched='Plan appointment on this day.';
+$LDPlanDuty='Plan duty on this day.';
+$LDORProgram='OR program';
+$LDDutyPerson='Nurse on duty';
+$LDDocsOnDuty='Doctors on duty';
+$LDORLogbook='OR logbook (Document new operation)';
+$LDORLogbookArch='OR logbook (Archive)';
+$LDORLogbookSearch='OR logbook (Search operated patient)';
+$LDPresent='Present';
+
+$LDBgColor='Background color';
+$LDTxtColor='Text color';
+$LDClk4BgColor='Click to select background color';
+$LDClk4TxtColor='Click to select text color';
+$LDApply='Apply';
+
+$LDColorMap='Color map';
+$LDPlsClkColor='Click the color you want';
+$LDMainFrame='Main frame';
+$LDTopFrame='Top frame';
+$LDBottomFrame='Bottom frame';
+
+$LDPWChange='Password change';
+$LDUserIdPWPrompt='Enter your user login name and actual password:';
+$LDNewPwPrompt='Now, enter the new password of your choice:';
+$LDNewPw='New password';
+$LDNewPw2='Repeat new password';
+$LDChangePw='Change password';
+$LDOops='Ooops! No, do not change.';
+
+/*$LDWrongEntry='Wrong entries!';
+$LDNoAuth='No access right';
+$LDAuthLocked='Access right frozen.';*/
+
+$LDAlertPwError='The new passwords are not the same. Please enter the new passwords again.';
+$LDPWChanged='The new password is saved.';
+
+$LDBday='Birthdate';
+$LDShotDate='Shotdate';
+$LDPatientNr='Encounter Nr';
+$LDSetShotDate='Set this date for all pictures.';
+$LDFotoLab='Photolab';
+$LDHowManyPics='How many pictures do you wish to save in the databank?';
+$LDNr='Number';
+$LDNewPics='pictures from a new patient';
+$LDMorePics='pictures from the same patient';
+$LDAdditional='Additional';
+$LDLinkBroken='Link to the photo server is broken.';
+$LDPicsSaved='The following photo files were renamed and saved in the databank:';
+$LDEditTitle='Headline';
+
+$LDAlertPhotoInfo='The information in the entry field';
+$LDAlertNoPhotoInfo=' is not complete. Please enter the complete information.';
+$LDAlertNoPatientData='The patient\'s data is not complete.';
+$LDAlertNumberOnly='Please enter a number or the word "main".';
+
+$LDStatistics='Statistics';
+$LDDisplay1='Display 1';
+$LDDisplay1Txt='Switch over to the display with the menu always on the left frame';
+$LDDisplay2='Display 2';
+$LDDisplay2Txt='Switch over to the display with the menu in a floating frame. ';
+
+$LDNewPwDiffer='The new passwords differ from each other.<br> Please enter the new password again.';
+/* 2003-03-22 EL*/
+$LDstaffMngmnt='staff Manager';
+$LDstaffMngmntTxt='Tools and functions for managing staff';
+/* 2003-04-25 EL */
+$LDBilling='eCombill';
+$LDBillingTxt='Bill patient, create billable items, generate bill reports, etc.';
+$LDInsuranceCoMngr='Insurance Co. Manager';
+$LDInsuranceCoMngrTxt='Enter new, edit insurance company data';
+$LDAddressMngr='Address Manager';
+$LDAddressMngrTxt='Enter, list, edit, & update address data';
+/* 2003-05-18 EL */
+$LDUserConfigOpt='Configuration options';
+$LDUserConfigOptTxt='User options for background & text colors, mascots, avatar, etc.';
+$LDMascotOpt='Mascot options';
+$LDMascotOptTxt='Hide, show, or select prompt mascots.';
+$LDMascot='Mascot';
+$LDSampleMascot='Sample mascots';
+/* 2003-04-24 EL*/
+$LDGUITemplate='GUI Template option';
+$LDGUITemplateTxt='Select a GUI (graphic user interface) template';
+$LDGUITheme='GUI Theme';
+$LDTheme='Theme';
+# 2002-08-10
+$LDLastName='Family name';
+$LDWantUpload='I want to upload';
+$LDImage='image(s)';
+# 2002-01-18 EL
+$LDTicketedSupport='Ticketed Support';
+$LDTicketedSupportTxt='Send a ticketed request to the project\'s online support';
+$LDNewsgroup='Newsgroup';
+$LDNewsgroupTxt='Newsgroup mirroring the developers\' mailing list.';
+# 2002-08-19 EL
+$LDPharmaIndex='Pharma Index';
+$LDPharmaIndexTxt='Pharmaceutical indexes like Redlist, IFAP, NIMS, or PIMS';
+
+#2004-05-23 EL
+$LDPlugins='Plugins';
+$LDNoPluginsPrompt='There are no plugins installed yet. If you want to get a Care2x plugin module please go to http://www.care2x-plugins.org.';
+$LDPluginsTxt='Collection of plugin tools and modules';
+
+#2004-06-13 EL
+$LDCssThemes = 'CSS Theme Styles';
+$LDCssThemesTxt= 'Select design for border color, sizes, decoration, font types & sizes, backgrounds, etc.';
+$LDDescription = 'Description';
+$LDScreenshot = 'Screenshot';
+
+#2004-06-30
+$LDSmartyTemplate = 'Smarty Template Styles';
+$LDSmartyTemplateTxt = 'Select smarty based GUI template style';
+
+#2004-07-08
+$LDControlButtons = 'Control Buttons Styles';
+$LDControlButtonsTxt = 'Select control buttons style';
+$LDComIcons = 'Common Icons Styles';
+$LDComIconsTxt = 'Select decorative icons style <br>(note: in combination with some CSS themes, the icons are overridden by the CSS design)';
+$LDDevDesigner = 'Developer/Designer';
+$LDNoIcon = 'No icons';
+$LDNoIconTxt = 'Shows no icons in submenus. Display of icons is suppressed';
+
+#2004-07-29
+$LDMainMenuTree = 'Main Menu Styles';
+$LDMainMenuTreeTxt = 'Select the main menu style';
+$LDStyle = 'Style';
+$LDRefreshBrowser = 'Refresh your browser to see the change(s)';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
