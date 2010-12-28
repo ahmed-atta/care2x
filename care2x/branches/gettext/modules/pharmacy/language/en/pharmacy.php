2a29fcbd03a569e0dcf7460ab7bc8e90885699b5
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-28 17:41:03 +0000

    moved pharmacy to new language system

---
+++ care2x/branches/gettext/modules/pharmacy/language/en/pharmacy.php
@@ -0,0 +1,444 @@
+<?php
+$LDClose='Close';
+$LDCancel='Cancel';
+$LDResetEntry='Erase entries';
+$LDSave='Save';
+$LDReset='Reset';
+
+$LDPharmacy='Pharmacy';
+$LDMedDepot='Medical Depot';
+
+$LDSearchWordPrompt='Enter a search keyword, for example: an order number, a product number, or a product name, etc.';
+$LDEnterData='Click to enter data';
+$LDClk2See='Click to see data';
+
+/**************** note the ' ~nr~ ' must not be erased it will be replaced by the script with the number of search results ******/
+$LDFoundPatient='The search found <b>~nr~</b> patients';
+$LDWildCards='What are wildcards and how to use them';
+$LDNewSearch='New search';
+$LDSearch='Search';
+
+$LDOClock='o\'clock';
+$LDContinue='Continue';
+$LDBack='Go back';
+
+$LDOn='On';
+$LDAt='At';
+$LDClk2Edit='Click to edit this report';
+
+$LDNew='New';
+$LDEdit='Edit';
+$LDCreate='Create';
+$LDValue='Value';
+
+$LDNews='News';
+$LDNewsTxt='Read or write news articles pertaining to pharmacy';
+$LDMemo='Memo';
+$LDMemoTxt='Read or write a memo';
+$LDSearchPat='Search patient';
+$LDCategory='Category';
+$LDPast3Days='Past 3 days';
+$LDPast3Months='Past 3 months';
+$LDPastAll='All';
+$LDSelect='Select';
+
+$LDOrderArchive='Archive';
+$LDOrderArchiveTxt='View, edit, archive processed orders';
+$LDPharmaOrder='Ordering';
+$LDPharmaOrderTxt='Create and/or send order list';
+$LDHow2Order='How to order?';
+$LDHow2OrderTxt='How, what, when should I order?';
+$LDOrderCat='My product catalog';
+$LDOrderCatTxt='Create, edit, update, remove, etc.';
+$LDPharmaDb='Databank';
+$LDPharmaDbTxt='Managing product information';
+$LDRedList='Index of Medical Specialties';
+$LDRedListTxt='Pharmaceutical index (needs internet line)';
+$LDOrderBotActivate='OrderBot';
+$LDOrderBotActivateTxt='Activate automatic reception of order lists.';
+
+$LDNotSent='Not yet sent!';
+$LDcatindex[0]='';
+$LDcatindex[1]='Article';
+$LDcatindex[2]='Pcs.';
+$LDcatindex[3]='';
+$LDcatindex[4]='Order nr.';
+$LDcatindex[5]='';
+$LDcatindex[6]='';
+
+$LDFinindex[0]='';
+$LDFinindex[1]='Article';
+$LDFinindex[2]='Pcs.';
+$LDFinindex[3]='';
+$LDFinindex[4]='Order nr.';
+
+$LDListindex[0]='';
+$LDListindex[1]='';
+$LDListindex[2]='Created on';
+$LDListindex[3]='Time';
+$LDListindex[4]='Created by';
+$LDListindex[5]='';
+
+$LDGenindex[0]='';
+$LDGenindex[1]='';
+$LDGenindex[2]='';
+$LDGenindex[3]='Article';
+$LDGenindex[4]='Generic';
+$LDGenindex[5]='Description';
+$LDGenindex[6]='Ordernr.';
+
+$LDCindex[0]=' ';
+$LDCindex[1]=' ';
+$LDCindex[2]='Article';
+$LDCindex[3]='Pcs.';
+$LDCindex[4]=' ';
+$LDCindex[5]='Order nr.';
+$LDCindex[6]=' ';
+$LDCindex[7]=' ';
+
+$LDMCindex[0]='';
+$LDMCindex[1]='Article';
+$LDMCindex[2]='Pcs.proorder';
+$LDMCindex[3]='Order nr.';
+$LDMCindex[4]='';
+
+$LDMSRCindex[0]='';
+$LDMSRCindex[1]='Order nr';
+$LDMSRCindex[2]='Article nr.';
+$LDMSRCindex[3]='License nr.';
+$LDMSRCindex[4]='Article';
+$LDMSRCindex[5]='Generic';
+$LDMSRCindex[6]='Description';
+$LDMSRCindex[7]='';
+
+$LDBasketEmpty='The basket is empty at the moment.';
+$LDWasSent='Somebody has either already sent the order to the pharmacy or erased it! Please create a new order list. Thank you.';
+$LDFinalizeList='Finalize order list';
+$LDClk2SeeEdit='To see and/or edit it please click the arrow button.';
+$LDListNotSentMany='The following order lists are not yet sent.';
+$LDListNotSent='The following order list is not yet sent.';
+$LDEditList='Edit list';
+$LDRemoveList='Erase list';
+
+$LDBasket='Basket';
+$LDCatalog='Order catalog';
+$LDPutNBasket='Put in the basket';
+$LDRemoveArticle='Remove article';
+$LDPut2BasketAway='Put in the basket right away';
+$LDNoDataFound='The search found <font color=red><b>no</b></font> data.';
+$LDClk2SeeInfo='Please click the right one to see the complete information';
+/********************** Do not erase or replace the ~nr~ *****************************/
+$LDFoundNrData='The search found <font color=red><b>~nr~</b></font> data that corresponded to the search keyword.';
+$LDSearchArticle='Find article';
+$LDSearchKey='Search keyword';
+$LDPut2Catalog='Put this article in the catalog';
+$LDActualOrder='Actual order';
+$LDOrderSent='The following order list was sent to the pharmacy. Thank you!';
+$LDNormal='Normal';
+$LDUrgent='Urgent';
+$LDPassword='Password';
+$LDValidatedBy='Validated by';
+$LDSendOrder='Send this order list.';
+$LDBack2Edit='Go back and edit list.';
+$LDEndOrder='End ordering.';
+$LDCreateBasket='Start a new order list.';
+$LDAlertNoValidator='The validator is needed.';
+$LDAlertNoPassword='The validator\'s password is needed.';
+$LDOpenInfo='Show complete information about ';
+
+$LDSearch4OrderList='Search for order lists';
+$LDGoodMorning='Good morning';
+$LDGoodDay='Hello, nice to see you';
+$LDGoodEvening='Good evening';
+
+$LDDate='Date';
+$LDDept='Department';
+$LDPrio='Priority';
+$LDSearchIn='Search after';
+
+$LDArchindex[0]=' ';
+$LDArchindex[1]=' ';
+$LDArchindex[2]='Order list from:';
+$LDArchindex[3]='Received on:';
+$LDArchindex[4]='at:';
+$LDArchindex[5]=' ';
+$LDArchindex[6]='Processed on:at:';
+$LDArchindex[7]='by:';
+
+$LDArchValindex[0]='Order from';
+$LDArchValindex[1]='on';
+$LDArchValindex[2]='at';
+$LDArchValindex[3]='Sent by';
+$LDArchValindex[4]='Validated by';
+$LDArchValindex[5]='Sent on';
+$LDArchValindex[6]='at';
+$LDArchValindex[7]='';
+
+$LDOrderIndex[0]=' ';
+$LDOrderIndex[1]=' ';
+$LDOrderIndex[2]='Order from';
+$LDOrderIndex[3]='Arrived on';
+$LDOrderIndex[4]='at';
+$LDOrderIndex[5]='Status';
+
+$LDPlsEnterMore='Please enter some more information and try it again.';
+$LDNoSingleChar='A single character will be ignored.';
+
+$LDListFound='The following is the order list in the archive that corresponded to the search keyword.';
+$LDListFoundMany='The following are the order lists in the archive that corresponded to the search keyword.';
+$LDDataRemoved='The information could have been removed from the pharmacy\'s databank.';
+$LDPlsInformDept='Please notify the ~tagword~ department and eventually the EDP department. Thank you.';
+
+$LDOrderedArticle='Ordered article';
+$LDOrderedArticleMany='Ordered articles';
+$LDNewProduct='New product';
+$LDNewProductTxt='Enter new pharmacy product in the databank';
+$LDReports='Reports';
+$LDReportsTxt='Create, search, read reports, etc.';
+$LDInfo='Information';
+$LDInfoTxt='Search and read information pertaining to pharmacy';
+$LDManage='Management';
+$LDSearchDb='Search in the pharmacy databank';
+$LDUpdateOk='Update was <b>successful</b>:';
+$LDDataSaved='The following data was successully saved:';
+$LDDataNoUpdate='Update  <b>failed</b>. Please check the entries.';
+$LDDataNoSaved='Save <b>failed</b>. Please check the entries.';
+$LDClk2EnterNew='If you want to enter a new product, please click this.';
+$LDOrderNr='Order number';
+$LDArticleName='Article name';
+$LDGeneric='Generic name';
+$LDDescription='Description';
+$LDPacking='Packaging';
+$LDCAVE='CAVE';
+$LDMinOrder='Minimum order';
+$LDMaxOrder='Maximum order';
+$LDPcsProOrder='No. of pcs. per order';
+$LDIndustrialNr='Industrial number';
+$LDLicenseNr='License number';
+$LDPicFile='Picture file';
+$LDBack2Menu='Go back to databank menu';
+$LDPageTop='Back to top.';
+$LDPreview='Preview';
+$LDUpdateData='Update or edit';
+$LDRemoveFromDb='Remove from the databank';
+$LDDataRemoved='The product was removed from the databank!';
+$LDAlertNoOrderNr='Order number is missing!';
+$LDAlertNoArticleName='Article name is missing!';
+$LDAlertNoDescription='Description is missing!';
+
+$LDConfirmDelete='Do you really want to <b>delete</b> or <b>remove</b> the following data from the databank ?';
+$LDAlertDelete='<b>ATTENTION!</b> Delete or remove <b>CANNOT</b> be undone!';
+$LDNoDelete='Deletion of the data failed!<br>Please notify the EDP department.';
+$LDYesDelete='Yes, I am dead sure. Delete data.';
+$LDNoBack='No, do not delete. Go back.';
+$LDIm='I am the';
+$LDClk2Ack='Click the arrow button to acknowledge and/or print the order list.';
+$LDOrderArrived='A new order list has arrived !';
+$LDShowOrder='Show order list';
+$LDNewOrder='The following order list has just arrived.';
+$LDNewOrderMany='The following order lists have just arrived.';
+$LDOK='OK';
+$LDAck='Acknowledgement';
+$LDOrderAck='Acknowledge and print out';
+$LDOrderPrint='Print out order list';
+$LDOrder2Archive='Move order list to the archive';
+$LDProcessedBy='Processed by';
+$LDDataNoFoundTxt='The information cannot be found. An error occured somewhere. Please click the \'Close\' button and try to
+reopen the order list. If this problems occurs even after several trials, please notify the pharmacy and the EDP departments. Thank you.';
+$LDCreatedBy='Created by';
+$LDAlertEnterName='Please enter your name in the \'Created by\' field.';
+$LDMediBotActivate='Medibot';
+$LDOpenHelp='Open help window';
+$LDOrderListKey='Search keyword for order list';
+
+$LDReport='Report';
+$LDWriteReport='Write report';
+$LDReportSent='The report was sent';
+$LDSend='Send';
+$LDstaffNr='staff Nr.';
+$LDResetAll='Reset all';
+$LDReporter='Reporter';
+
+$LDAlertName='Enter your name please!';
+$LDAlertPersonNr='Enter your staff number please!';
+$LDAlertReport='Please write your report!';
+
+$LDOrderNrExists='The order number exists already! Please use a different number.';
+/* 2002-10-11 EL */
+$LDPharmaOrderBot='Pharma Orderbot';
+$LDDepotOrderBot='Medibot';
+/* 2002-10-15 EL */
+$LDCreatedOn='Created on';
+$LDTime='Time';
+/* 2002-11-5 EL */
+$LDInvalidPassword='Invalid password!';
+$LDUnknownValidator='Validator unknown!';
+$LDPlsEnterInfo='Please enter the correct info.';
+# 2003-08-17 EL
+$LDAlertInvalidMinorder='The minimum order has invalid character';
+$LDAlertInvalidMaxorder='The maximum order has invalid character';
+$LDAlertInvalidProorder='The nr.of pcs. per order has invalid character';
+$LDAlertMinHigherMax='Minimum greater than maximum is not allowed';
+$LDAlertNoNegativeOrder='Negative value is currently not allowed';
+# 2003-08-22 EL
+$LDYellowList='Yellow List';
+$LDYellowListTxt='German pharmaceutical index (needs internet line)';
+$LDClickSelectDept='Please click this to select a department.';
+$LDNoDepartmentAssociation='You are not associated with any department / pharmacy. Please ask your system administrator for help';
+
+$LDSupplier='Supplier';
+$LDSupplierTxt='Create Modify Supplier';
+
+$LDAdresa='Address';
+$LDTelefoni='Tel';
+$LDFax='Fax';
+$LDKodiPostar='ZIP Code';
+
+$LDSupplyTxt='Suppliers list';
+$LDSupply='Supplier';
+$LDClickSelectSupplier='Select Supplier';
+$LDSelectSupplier='Select Supplier';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
+
+$LDUserPrompt='Username';
+$LDPwPrompt='Password';
+$LDSubmitBut='Continue...';
+$LDPwNeeded='Password needed';
+$LDIntroTo='Introduction to editing news articles in';
+$LDWhatTo='What and how can I edit news articles in';
+
+$LDWrongEntry='Wrong entries!';
+$LDNoAuth='You have no access permission!';
+$LDAuthLocked='Your access permission is locked!';
+
+$LDNewData='New Data';
+$LDEdit='Edit';
+$LDAdmit='Admit';
+$LDSearch='Search';
+$LDArchive='Archive';
+$LDAdmission='Patient Admission';
+$LDAdmTargetEntry='Admission';
+$LDAdmTargetSearch='Search Admitted Patients';
+$LDAdmTargetArchive='Research in Archive';
+$LDAdmWantEntry='I need to admit a patient';
+$LDAdmWantSearch='I am looking for a patient';
+$LDAdmWantArchive='I need to research in the archive';
+$LDAdmHow2Enter='How to admit a patient';
+$LDAdmHow2Edit='How to edit a patients data';
+$LDAdmHow2Search='How to find a certain patients data';
+$LDAdmHow2Archive='How to research in the archives';
+
+$LDMedocs='Medocs';
+$LDMedocsHow2Enter='How to start a new medocs document';
+$LDMedocsHow2Edit='How to edit a medocs document';
+$LDMedocsHow2Search='How to find a certain medocs document';
+$LDMedocsHow2Archive='How to research in the medocs archives';
+
+$LDIntro2='Introduction to';
+$LDWhat2Do='How to do what in';
+$LDHowManage='How to manage the directory';
+$LDHowEnter='How to enter new phone information';
+
+$LDPhoneDir='Phone directory';
+$LDMakeDutyPlan='Create dutyplan';
+$LDDocsList='Doctors list';
+$LDNursingManage='Ward management';
+
+$LDNursingStation='Nursing ward';
+$LDOrDocu='OR Documentation';
+$LDOrLogBook='OR nursing logbook';
+$LDOrNursing='OR nursing';
+$LDCreate='Create';
+$LDPersonList='staff list';
+$LDRepairbotActivate='Activate Repairbot';
+$LDQBotActivate='Activate Q-Bot';
+
+$LDMedLab='Medical laboratory';
+$LDCancel='Cancel';
+
+$LDPharmacy='Pharmacy';
+$LDOrderArchive='Archive';
+$LDPharmaOrder='Ordering';
+$LDOrderCat='My product catalog';
+$LDPharmaDb='Databank';
+$LDPharmaOrderBot='Activate OrderBot';
+
+$LDMedDepot='Medical Depot';
+$LDMediBotActivate='Activate Medibot';
+$LDSqlDb='SQL Databank';
+$LDManageAccess='Manage Access Rights';
+$LDListAll='List all';
+$LDSystemLogin='System Administrator Login';
+$LDSeeData='Display data';
+$LDNewData='Enter new data';
+
+$LDWelcome='Welcome';
+$LDLogin='Login';
+$LDChangeSaved='The changes were saved.';
+$LDPcID='Current identity of this computer';
+$LDDept='Department';
+$LDWard='Ward';
+$LDWardOR='Ward room or OR number';
+$LDPhoneNr='Rooms telephone number';
+$LDIntercomNr='Rooms intercom number';
+$LDPcIP='IP address of this computer';
+$LDSave='Save changes';
+$LDNoChange='Do not change current settings';
+$LDClose='Close';
+$LDLogout='Log out';
+$LDNewLogin='New login';
+$LDLogoutConfirm='Do you really want to log out?';
+$LDOK='OK';
+$LDYes='Yes';
+$LDNotReally='No, not really.';
+$LDDOCScheduler='DOC Scheduler';
+$LDORNOCScheduler='ORNOC Scheduler';
+$LDFotolab='Photolab';
+/* 2002-09-08 EL */
+$LDTestRequest='Diagnostic Test Request';
+
+$LDTestType['patho']='Pathology';
+$LDTestType['chemlabor']='Medical Laboratory';
+$LDTestType['baclabor']='Bacteriological Laboratory';
+$LDTestType['blood']='Blood Bank';
+$LDTestType['radio']='Radiology';
+$LDTestType['sono']='Sonography';
+$LDTestType['allamb']='General Ambulatory';
+$LDTestType['unfamb']='Emergency Ambulatory';
+$LDTestType['nuklear']='Nuclear Diagnostics';
+$LDTestType['inmed']='Internal Medicine';
+
+$LDBloodOrder='Request for blood products';
+/* 2002-09-22 EL */
+$LDPendingRequest='Pending request';
+/* 2002-10-06 EL*/
+$LDLoggedOut='Logged out';
+/* 2003-03-18 EL */
+$LDNursesList='Nurses List';
+$LDAppointments='Appointments';
+/* 2003-04-27 */
+$LDInsuranceCoManager='Insurance Company Manager';
+$LDAddressMngr='Address Manager';
+/* 2003-04-30 EL*/
+$LDBilling='eComBill';
+/* 2003-05-18 EL */
+$LDPlsContactEDP='Please contact the EDP department.';
+$LDPlsTryAgain='Please try again';
+/* 2003-07-11 EL */
+$LDAdministration='Administration';
+$LDOutpatientClinic='Outpatient clinic';
+$LDUploadDicom='Upload DICOM images';
+$LDViewDicom='View DICOM images';
+# 2003-08-22 EL
+$LDYellowList='Yellow List';
+$LDRedList='Red List';
+#2003-11-22 EL
+$LDAdvancedSearch='Advanced search';
+$LDNewPerson='Register new person';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
