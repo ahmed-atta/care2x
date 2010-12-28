c2df81ee5f41288cc7de220f450bde04488187fc
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-28 13:16:06 +0000

    moved intranet_email  to new language system

--- care2x/branches/gettext/modules/intranet_email/intra-email-showaddr.php
+++ care2x/branches/gettext/modules/intranet_email/intra-email-showaddr.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 require('./roots.php');
-require($root_path.'include/helpers/inc_environment_global.php');
+require('../../include/helpers/inc_environment_global.php');
 /**
 * CARE2X Integrated Hospital Information System Deployment 2.1 - 2004-10-02
 * GNU General Public License
@@ -10,7 +10,8 @@ require($root_path.'include/helpers/inc_environment_global.php');
 *
 * See the file "copy_notice.txt" for the licence notice
 */
-define('LANG_FILE','intramail.php');
+define('MODULE','intranet_email');
+define('LANG_FILE_MODULAR','intranet_email.php');
 $local_user='ck_intra_email_user';
 require_once($root_path.'include/helpers/inc_front_chain_lang.php');
 // check the info params for completeness
