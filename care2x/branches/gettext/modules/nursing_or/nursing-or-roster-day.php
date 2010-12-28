7b2bfc3c79f549cfd2a1bc23920d80b7766ae2b4
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-28 17:38:12 +0000

    moved nursing or to new language system

--- care2x/branches/gettext/modules/nursing_or/nursing-or-roster-day.php
+++ care2x/branches/gettext/modules/nursing_or/nursing-or-roster-day.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 require('./roots.php');
-require($root_path.'include/helpers/inc_environment_global.php');
+require('../../include/helpers/inc_environment_global.php');
 /**
 * CARE2X Integrated Hospital Information System beta 1.0.03 - 2002-10-26
 * GNU General Public License
@@ -11,8 +11,8 @@ require($root_path.'include/helpers/inc_environment_global.php');
 * See the file "copy_notice.txt" for the licence notice
 */
 
-$lang_tables=array('doctors.php','departments.php');
-define('LANG_FILE','or.php');
+define('MODULE','nursing_or');
+define('LANG_FILE_MODULAR','nursing_or.php');
 define('NO_2LEVEL_CHK',1);
 require_once($root_path.'include/helpers/inc_front_chain_lang.php');
 
