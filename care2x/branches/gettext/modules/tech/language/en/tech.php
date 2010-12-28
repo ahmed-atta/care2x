0fbe068e980f2a998fb8ff1ace0de31b4def6e42
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-28 17:50:25 +0000

    tech to new language system

---
+++ care2x/branches/gettext/modules/tech/language/en/tech.php
@@ -0,0 +1,330 @@
+<?php
+$LDAdmission='Admission';
+$LDHeadline='Headline';
+$LDHeadlines='Headlines';
+$LDManagement='Management';
+$LDHealthTips='Health tips';
+$LDAdmission='Admission';
+$LDNursing='Nursing';
+/* 2003-04-27 EL */
+$LDMedical='Medical';
+$LDSupport='Support';
+$LDNews='News';
+$LDDepartment='Department';
+$LDPressRelations='Press Relations';
+/* 2003-05-19 EL */
+$LDSelectDept='Select a Department';
+/*2003-06-15 EL*/
+$LDPlsSelectDept='Please Select a Department';
+$LD_AllMedicalDept='____All Medical Departments_____';
+$LDClinic='Clinic';
+
+#2003-10-23 EL
+$LDPlsNameFormal='Please write the Formal Name';
+$LDPlsDeptID='Please write the Department ID';
+$LDPlsSelectType='Please select the type';
+
+$LDIsPharmacy='Is Pharmacy ?';
+$LDPharmacy='Select the Pharmacy this department will use.';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
+
+$LDClose='Close';
+$LDTech='Technical';
+$LDTechRepair='Technical repair';
+$LDTechMaint='Technical maintenance';
+$LDTechSupport='Technical support';
+$LDTechInfo='Technical information';
+
+$LDReRepair='Request repair support';
+$LDReRepairTxt='Report damage, compose and send request for a repair service.';
+$LDRepairbotActivate='Activate Repairbot';
+$LDRepairbotActivateTxt='A robot for automatic reception of repair job requests.';
+$LDRepairReport='Report repair';
+$LDRepairReportTxt='Report finished repair jobs.';
+$LDReportsArchive='Reports archive';
+$LDReportsArchiveTxt='Research in the technical reports archives';
+$LDQuestions='Inquiry, Questions';
+$LDQuestionsTxt='Compose, read, and send questions or inquiries';
+$LDQBotActivate='Activate Q-Bot';
+$LDQBotActivateTxt='A robot for automatic reception of question or inquiries.';
+$LDInfo='Information';
+$LDInfoTxt='Browse for information of technical nature.';
+
+$LDSendRequest='Send request';
+$LDSendReport='Send report';
+$LDSendInquiry='Send inquiry';
+$LDReset='Reset';
+$LDRepairArea='Localization of the damage';
+$LDReporter='Requested by';
+$LDstaffNr='staff nr.';
+$LDPhoneNr='Telephone nr. (for eventual inquiries)';
+$LDPlsDescribe='Please describe the nature of the damage';
+
+$LDAlertName='Please enter your name.';
+$LDAlertDept='Please enter your department or localisation of the damage.';
+$LDAlertDeptOnly='Please enter your department.';
+$LDAlertPNr='Please enter your staff number.';
+$LDAck='Acknowledgement';
+$LDYour='Your';
+$LDReceived='was received on';
+$LDAt='at';
+$LDAtTech='. Thanks. Your tech support.';
+$LDRequest='request';
+$LDReport='report';
+$LDThanksSir='Thank you Sir \ Madam';
+$LDPlsTypeReport='Please describe the repair job you have done.';
+$LDJobIdNr='Job ID nr.';
+$LDTechnician='Technician';
+$LDPlsDoneOnly='(Please report only finished repair jobs.)';
+$LDName='Name';
+$LDDept='Department';
+$LDEnterQuestion='Please type your question';
+$LDLogIn='Log in';
+$LDPlsNoRequest='(Do not type requests for repair here. If you want to report a damage and need a repair pls click this.';
+$LDLastQuestions='Last ~tagword~ question(s) or answer(s)';
+$LDFrom='from';
+$LDTo='to';
+$LDAlertQuestion='You did not type any question.';
+$LDOn='on';
+$LDOClock='oClock';
+$LDInquiry='Inquiry';
+$LDReply='Reply';
+$LDSearch='Search';
+$LDSearchReport='Search for reports';
+$LDDate='Date';
+$LDReportListMany='Following are the reports';
+$LDNotReadMany=' that have not been read or printed yet.';
+$LDReportList='Following is the report';
+$LDNotRead=' that has not been read or printed yet.';
+$LDClk2Read='Click the arrow button to read the content.';
+$LDLikeSearch=' that corresponds to the search keywords.';
+$LDLikeSearchMany=' that correspond to the search keywords.';
+
+$LDTelephoneNr='Phone nr.';
+
+$bcatindex[0]=' ';
+$bcatindex[1]=' ';
+$bcatindex[2]='Technician';
+$bcatindex[3]='Department';
+$bcatindex[4]='Receivedon';
+$bcatindex[5]='at';
+$bcatindex[6]=' ';
+
+$blistindex[0]='Report from';
+$blistindex[1]='on';
+$blistindex[2]='at';
+$blistindex[3]='Department';
+$blistindex[4]='Job ID nr.';
+
+$reportindex[0]=' ';
+$reportindex[1]=' ';
+$reportindex[2]='request from:';
+$reportindex[3]='Received at:';
+$reportindex[4]='at';
+$reportindex[5]=' ';
+
+$requestindex[0]='request from:';
+$requestindex[1]='at';
+$requestindex[2]='on';
+$requestindex[3]='Department';
+$requestindex[4]='Phonenr.';
+$requestindex[5]='Job ID nr.';
+
+$queryindex[0]=' ';
+$queryindex[1]=' ';
+$queryindex[2]='Inquiry from :';
+$queryindex[3]='Department';
+$queryindex[4]='Received on:';
+$queryindex[5]='at';
+$queryindex[6]=' ';
+
+$LDMarkRead='Mark as Read.';
+$LDPrint='Print';
+$LDGoBack='Go Back';
+$LDImRepairbot='I am the Repairbot';
+$LDImQBot='I am the Q-Bot';
+$LDNewReport='The following is the newly arrived request for repair.';
+$LDNewReportMany='The following are the newly arrived requests for repair.';
+$LDReportArrived='A request for repair has arrived!';
+$LDShowRequest='Show request';
+$LDNoDataFound='No data found. An error occured somewhere. Please click the Close button and try to open the list of requests again.
+If this problem persists despite several attempts, please inform the EDP and the Tech support departments.';
+$LDAckPrint='Acknowledge and print request';
+$LDPrintRequest='Print request';
+$LDArchiveRequest='Move request to archive';
+$LDAckBy='Acknowledged by';
+$LDAlertEnterName='Please enter your name in the field Acknowledged by.';
+$LDYourReply='Your reply';
+$LDNewInquiry='The following is the newly arrived inquiry.';
+$LDNewInquiryMany='The following are the newly arrived inquiries.';
+$LDClk2Reply='Click the arrow button to read and\or reply';
+$LDSendReply='Send reply';
+$LDMove2Archive='Move to archive';
+$LDShow='Show';
+$LDShowInquiry='Show inquiry';
+$LDInquiryArrived='A new inquiry has arrived!';
+$LDSearchWordPrompt='Enter a search keyword';
+$LDInfoCat[0]='Requesting for a repair';
+$LDInfoCat[1]='Reporting a damage';
+$LDInfoCat[2]='Heating';
+$LDInfoCat[3]='Disposal';
+$LDInfoCat[4]='EDP';
+$LDInfoCat[5]='Computer IP addresses';
+$LDInfoCat[6]='Printing';
+$LDInfoCat[7]='Printer';
+$LDInfoCat[8]='Environment management';
+$LDInfoCat[9]='Maintenance';
+$LDInfoCat[10]='Cleaning';
+$LDInfoCat[11]='Electricity';
+$LDInfoCat[12]='Telephone system';
+$LDInfoCat[13]='Water Supply';
+$LDNoFound='I found nothing that corresponds to the search keyword. Please try it again and enter some more information.';
+/* 2003-02-11*/
+$LDCancel='Cancel';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
+
+$LDUserPrompt='Username';
+$LDPwPrompt='Password';
+$LDSubmitBut='Continue...';
+$LDPwNeeded='Password needed';
+$LDIntroTo='Introduction to editing news articles in';
+$LDWhatTo='What and how can I edit news articles in';
+
+$LDWrongEntry='Wrong entries!';
+$LDNoAuth='You have no access permission!';
+$LDAuthLocked='Your access permission is locked!';
+
+$LDNewData='New Data';
+$LDEdit='Edit';
+$LDAdmit='Admit';
+$LDSearch='Search';
+$LDArchive='Archive';
+$LDAdmission='Patient Admission';
+$LDAdmTargetEntry='Admission';
+$LDAdmTargetSearch='Search Admitted Patients';
+$LDAdmTargetArchive='Research in Archive';
+$LDAdmWantEntry='I need to admit a patient';
+$LDAdmWantSearch='I am looking for a patient';
+$LDAdmWantArchive='I need to research in the archive';
+$LDAdmHow2Enter='How to admit a patient';
+$LDAdmHow2Edit='How to edit a patients data';
+$LDAdmHow2Search='How to find a certain patients data';
+$LDAdmHow2Archive='How to research in the archives';
+
+$LDMedocs='Medocs';
+$LDMedocsHow2Enter='How to start a new medocs document';
+$LDMedocsHow2Edit='How to edit a medocs document';
+$LDMedocsHow2Search='How to find a certain medocs document';
+$LDMedocsHow2Archive='How to research in the medocs archives';
+
+$LDIntro2='Introduction to';
+$LDWhat2Do='How to do what in';
+$LDHowManage='How to manage the directory';
+$LDHowEnter='How to enter new phone information';
+
+$LDPhoneDir='Phone directory';
+$LDMakeDutyPlan='Create dutyplan';
+$LDDocsList='Doctors list';
+$LDNursingManage='Ward management';
+
+$LDNursingStation='Nursing ward';
+$LDOrDocu='OR Documentation';
+$LDOrLogBook='OR nursing logbook';
+$LDOrNursing='OR nursing';
+$LDCreate='Create';
+$LDPersonList='staff list';
+$LDRepairbotActivate='Activate Repairbot';
+$LDQBotActivate='Activate Q-Bot';
+
+$LDMedLab='Medical laboratory';
+$LDCancel='Cancel';
+
+$LDPharmacy='Pharmacy';
+$LDOrderArchive='Archive';
+$LDPharmaOrder='Ordering';
+$LDOrderCat='My product catalog';
+$LDPharmaDb='Databank';
+$LDPharmaOrderBot='Activate OrderBot';
+
+$LDMedDepot='Medical Depot';
+$LDMediBotActivate='Activate Medibot';
+$LDSqlDb='SQL Databank';
+$LDManageAccess='Manage Access Rights';
+$LDListAll='List all';
+$LDSystemLogin='System Administrator Login';
+$LDSeeData='Display data';
+$LDNewData='Enter new data';
+
+$LDWelcome='Welcome';
+$LDLogin='Login';
+$LDChangeSaved='The changes were saved.';
+$LDPcID='Current identity of this computer';
+$LDDept='Department';
+$LDWard='Ward';
+$LDWardOR='Ward room or OR number';
+$LDPhoneNr='Rooms telephone number';
+$LDIntercomNr='Rooms intercom number';
+$LDPcIP='IP address of this computer';
+$LDSave='Save changes';
+$LDNoChange='Do not change current settings';
+$LDClose='Close';
+$LDLogout='Log out';
+$LDNewLogin='New login';
+$LDLogoutConfirm='Do you really want to log out?';
+$LDOK='OK';
+$LDYes='Yes';
+$LDNotReally='No, not really.';
+$LDDOCScheduler='DOC Scheduler';
+$LDORNOCScheduler='ORNOC Scheduler';
+$LDFotolab='Photolab';
+/* 2002-09-08 EL */
+$LDTestRequest='Diagnostic Test Request';
+
+$LDTestType['patho']='Pathology';
+$LDTestType['chemlabor']='Medical Laboratory';
+$LDTestType['baclabor']='Bacteriological Laboratory';
+$LDTestType['blood']='Blood Bank';
+$LDTestType['radio']='Radiology';
+$LDTestType['sono']='Sonography';
+$LDTestType['allamb']='General Ambulatory';
+$LDTestType['unfamb']='Emergency Ambulatory';
+$LDTestType['nuklear']='Nuclear Diagnostics';
+$LDTestType['inmed']='Internal Medicine';
+
+$LDBloodOrder='Request for blood products';
+/* 2002-09-22 EL */
+$LDPendingRequest='Pending request';
+/* 2002-10-06 EL*/
+$LDLoggedOut='Logged out';
+/* 2003-03-18 EL */
+$LDNursesList='Nurses List';
+$LDAppointments='Appointments';
+/* 2003-04-27 */
+$LDInsuranceCoManager='Insurance Company Manager';
+$LDAddressMngr='Address Manager';
+/* 2003-04-30 EL*/
+$LDBilling='eComBill';
+/* 2003-05-18 EL */
+$LDPlsContactEDP='Please contact the EDP department.';
+$LDPlsTryAgain='Please try again';
+/* 2003-07-11 EL */
+$LDAdministration='Administration';
+$LDOutpatientClinic='Outpatient clinic';
+$LDUploadDicom='Upload DICOM images';
+$LDViewDicom='View DICOM images';
+# 2003-08-22 EL
+$LDYellowList='Yellow List';
+$LDRedList='Red List';
+#2003-11-22 EL
+$LDAdvancedSearch='Advanced search';
+$LDNewPerson='Register new person';
+
+$LDClose = 'Close';
+$LDBack = 'Back';
+$LDHelp = 'Help';
