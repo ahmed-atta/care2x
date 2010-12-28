167ae44647d69eb79f5d3209881c2ceeb7e4659e
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2010-12-28 13:14:49 +0000

    moved immunization module to new language system

--- care2x/branches/gettext/modules/immunization/immunization_manage_pass.php
+++ care2x/branches/gettext/modules/immunization/immunization_manage_pass.php
@@ -1,9 +1,10 @@
 <?php
 error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 require('./roots.php');
-require($root_path.'include/helpers/inc_environment_global.php');
+require('../../include/helpers/inc_environment_global.php');
 
-define('LANG_FILE','stdpass.php');
+define('MODULE','immunization');
+define('LANG_FILE_MODULAR','immunization.php');
 define('NO_2LEVEL_CHK',1);
 require_once($root_path.'include/helpers/inc_front_chain_lang.php');
 
