eb7470ef658ab8076292ace50d6cb1723a18ce33
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2011-01-24 20:54:17 +0000

    end of livtime version - functions merged with trunk

--- care2x_tz/branches/wasso/installer/tmp/%%-64/%%-645928537/actions.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2009-05-11 19:00:44
-         compiled from C:%5Cxampplite%5Chtdocs%5Ccare2x%5Cinstaller/templates/actions.tpl */ ?>
-<form action="<?php echo $GLOBALS['HTTP_SERVER_VARS']['PHP_SELF']; ?>
-" method="POST">
-<input type="hidden" name="save_action" value="true">
-<?php echo $this->_tpl_vars['ACTION_HTML']; ?>
-
-</form>
