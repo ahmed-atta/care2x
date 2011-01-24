a08f33aa9be275531d4458448fdf969e26149a7a
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2011-01-24 22:38:41 +0000

    care2x tanzanian basis

---
+++ care2x_tz/trunk/gui/smarty_template/templates_c/default/%%CB^CB6^CB620954%%footer.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.22, created on 2011-01-18 15:04:30
+         compiled from common/footer.tpl */ ?>
+<!--  <?php echo $this->_tpl_vars['SmartyLogo']; ?>
+ -->
+</BODY>
+</HTML>
