fa6daffb8cb36769ea9c7644877a1b9b4b37f699
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2011-02-04 14:52:46 +0000

    CARE2X ELCT Ver.2.05.3.8 seems to be ready (removed all depreciated declared functions of this code - hopefully)

--- care2x_tz/trunk/releaseinfo.php
+++ care2x_tz/trunk/releaseinfo.php
@@ -1,7 +1,9 @@
 <?php
-$version="2.05.3.8";
+$version="2.05.3.9";
 /**
  * History:
+ * Version 2.05.3.9
+ * Compatibility given now to PHP 5.3.5
  * Version 2.05.3.8
  * Improving the lab module, covering to the billing section. Embedded reporting elements on discharge and also patient tracking and presciption tracking
  * Version 2.05.3.5, 22-dec-08
