d0ceaf9c32c7189317452689a8e7dc48043d4e2a
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2011-02-07 10:28:02 +0000

    $_SERVER["HTTPS"] could not be set...and must be checked if this exists

--- care2x_tz/trunk/modules/dental/gui_dental_addnotes.php
+++ care2x_tz/trunk/modules/dental/gui_dental_addnotes.php
@@ -3,13 +3,16 @@
 require('./roots.php');
 
  $pageURL = 'http';
- if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ if (isset($_SERVER["HTTPS"]))
+ 	if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
  $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ if (isset($_SERVER["SERVER_PORT"])) {
+	 if ($_SERVER["SERVER_PORT"] != "80") {
+	  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	 }
  } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
+	 $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+}
 
 $lang_tables[]='search.php';
 define('LANG_FILE','dental.php');
