f5aa16b82911fa7da77848ba8c26b49f54cd1028
nakei <nakei@8154e661-2335-47f1-af25-7e1110f3596a>
2011-08-09 15:45:25 +0000

    elct merged version

---
+++ care2x_tz/branches/care2x_elct/gui/smarty_template/templates_c/default/%%8A^8A9^8A94DE63%%submenu_row_spacer.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.22, created on 2011-02-06 09:25:28
+         compiled from common/submenu_row_spacer.tpl */ ?>
+<TR  height=1>
+	<TD colSpan=3 class="vspace"><IMG height=1 src="../../gui/img/common/default/pixel.gif" width=5></TD>
+</TR>
