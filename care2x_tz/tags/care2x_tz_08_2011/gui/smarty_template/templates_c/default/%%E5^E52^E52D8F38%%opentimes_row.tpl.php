fbbdf62d0e0d69ba466d13bbcd0016b23e09cbaa
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2011-08-10 09:04:51 +0000

    tag'ing the status of tz from Aug.2011

---
+++ care2x_tz/tags/care2x_tz_08_2011/gui/smarty_template/templates_c/default/%%E5^E52^E52D8F38%%opentimes_row.tpl.php
@@ -0,0 +1,11 @@
+<?php /* Smarty version 2.6.22, created on 2010-11-29 07:40:47
+         compiled from common/opentimes_row.tpl */ ?>
+
+<tr bgcolor=#ffffff>
+	<td><?php echo $this->_tpl_vars['sOpenDay']; ?>
+</td>
+	<td><?php echo $this->_tpl_vars['sOpenTimes']; ?>
+</td>
+	<td><?php echo $this->_tpl_vars['sVisitTimes']; ?>
+</td>
+</tr>
