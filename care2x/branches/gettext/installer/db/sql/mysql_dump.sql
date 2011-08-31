af97420dacef531b42c2fec28ab3bbaba9a7b3f7
robertmeggle <robertmeggle@8154e661-2335-47f1-af25-7e1110f3596a>
2011-08-31 10:57:13 +0000

    committing here the main menu changes

--- care2x/branches/gettext/installer/db/sql/mysql_dump.sql
+++ care2x/branches/gettext/installer/db/sql/mysql_dump.sql
@@ -2085,29 +2085,6 @@ CREATE TABLE IF NOT EXISTS `care_med_products_main` (
 --
 
 CREATE TABLE IF NOT EXISTS `care_menu_main` (
-`nr` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-`sort` tinyint(2) NOT NULL DEFAULT '0',
-`parent` int(2) NOT NULL DEFAULT '0',
-`name` varchar(35) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-`permission` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-`image` varchar(100) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-`LD_var` varchar(35) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-`url` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-`is_visible` tinyint(1) unsigned NOT NULL DEFAULT '1',
-`hide_by` text CHARACTER SET latin1 COLLATE latin1_general_ci,
-`status` varchar(25) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-`modify_id` varchar(60) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-`modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (`nr`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `care_menu_main`
---
-
-
-
-CREATE TABLE IF NOT EXISTS `care_menu_main` (
   `nr` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `sort` tinyint(2) NOT NULL DEFAULT '0',
   `parent` int(2) NOT NULL DEFAULT '0',
@@ -2123,7 +2100,6 @@ CREATE TABLE IF NOT EXISTS `care_menu_main` (
   `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`nr`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
 -- --------------------------------------------------------
 
 --
@@ -4851,7 +4827,7 @@ INSERT INTO care_group VALUES (6, 'prescription', 'Prescription', 'LDPrescriptio
 --
 -- Dumping data for table care_menu_main
 --
-INSERT INTO `care_menu_main` (`nr`, `sort`, `parent`, `name`, `permission`, `image`, `LD_var`, `url`, `is_visible`, `hide_by`, `status`, `modify_id`, `modify_time`) VALUES
+INSERT INTO `care_menu_main` VALUES
 (2, 5, 0, 'Patient', '', '', 'LDPatient', 'modules/registration_admission/patient_register_pass.php', 1, '', '', '20030922232015', '0000-00-00 00:00:00'),
 (3, 10, 0, 'Admission', 'admission', '', 'LDAdmission', 'modules/registration_admission/admission_pass.php', 1, '', '', '20030922232015', '0000-00-00 00:00:00'),
 (4, 15, 0, 'Ambulatory', '', '', 'LDAmbulatory', 'modules/ambulatory/ambulatory.php', 1, '', '', '20030922232015', '0000-00-00 00:00:00'),
@@ -4872,16 +4848,15 @@ INSERT INTO `care_menu_main` (`nr`, `sort`, `parent`, `name`, `permission`, `ima
 (20, 7, 2, 'Appointments', '', '', 'LDAppointments', 'modules/appointment_scheduler/appt_main_pass.php', 1, '', '', '20030922232015', '2003-04-05 00:01:45'),
 (21, 0, 2, 'Admission', '', NULL, 'LDAdmission', 'modules/registration_admission/patient_register_pass.php', 1, NULL, '', NULL, '2011-08-24 22:01:58'),
 (22, 0, 2, 'Registration', '', 'gui/img/common/default/post_discussion.gif', '', 'modules/registration_admission/patient_register_pass.php', 1, NULL, '', NULL, '2011-08-24 22:04:17'),
-(23, 0, 2, 'Search', '', 'gui/img/common/default/findnew.gif', 'LDSearch', 'modules/registration_admission/patient_register_pass.php&target=search', 1, NULL, '', NULL, '2011-08-24 22:04:59'),
+(23, 0, 2, 'Search', '', 'gui/img/common/default/findnew.gif', 'LDSearch', 'modules/registration_admission/patient_register_pass.php?target=search', 1, NULL, '', NULL, '2011-08-24 22:04:59'),
 (24, 0, 2, 'Archive', '', 'LDArchive', '', 'modules/registration_admission/patient_register_pass.php&target=archiv', 1, NULL, '', NULL, '2011-08-24 22:05:50'),
 (25, 0, 7, 'Wards', '', 'gui/img/common/default/bul_arrowgrnsm.gif', '', 'modules/nursing/nursing.php', 1, NULL, '', NULL, '2011-08-24 22:07:09'),
-(26, 0, 3, 'Archive', '', NULL, 'LDArchive', 'modules/registration_admission/admission_pass.php&target=archiv', 1, NULL, '', NULL, '2011-08-24 22:07:39'),
+(26, 0, 3, 'Archive', '', NULL, 'LDArchive', 'modules/registration_admission/admission_pass.php?target=archiv', 1, NULL, '', NULL, '2011-08-24 22:07:39'),
 (27, 0, 7, 'Search', '', 'gui/img/common/default/findnew.gif', '', 'modules/nursing/nursing-patient-search-start.php', 1, NULL, '', NULL, '2011-08-24 22:08:06'),
 (28, 0, 7, 'Quick view', '', 'gui/img/common/default/eye_s.gif', '', 'modules/nursing/nursing-quickview.php', 1, NULL, '', NULL, '2011-08-24 22:08:36'),
 (29, 0, 0, 'Special Tools', '', NULL, '', '', 1, NULL, '', NULL, '2011-08-29 19:12:10'),
 (30, 0, 0, 'Medicaments', '', NULL, '', '', 1, NULL, '', NULL, '2011-08-29 19:20:15'),
 (31, 0, 0, 'Analysis', '', NULL, '', '', 1, NULL, '', NULL, '2011-08-29 19:23:08');
-
 
 --
 -- Dumping data for table care_method_induction
@@ -5622,4 +5597,3 @@ ALTER TABLE `care_encounter_prescription_sub`
 
 
 INSERT INTO `care_version` (`name`, `type`, `number`, `build`, `date`, `time`, `releaser`) VALUES ('CARE2X', 'beta', '3.0', '7110', '2011-08-18', '00:00:00', 'Gjergj Sheldija/Robert Meggle');
-
