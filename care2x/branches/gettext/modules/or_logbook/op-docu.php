17296c5cb919b464985458983a19614326fc2c0d
mizuko <mizuko@8154e661-2335-47f1-af25-7e1110f3596a>
2011-10-05 20:18:40 +0000

    missing include

--- care2x/branches/gettext/modules/or_logbook/op-docu.php
+++ care2x/branches/gettext/modules/or_logbook/op-docu.php
@@ -46,7 +46,7 @@ $_SESSION['sess_path_referer']=$top_dir.$thisfile;
 # Note: it is advisable to load this after the inc_front_chain_lang.php so
 # that the smarty script can use the user configured template theme
 
-require_once(CARE_BASE .'gui/smarty_template/smarty_care.class.php');
+require_once(CARE_BASE .'include/helpers/smarty_care.class.php');
 $smarty = new smarty_care('common');
 
 # Module title in the toolbar
