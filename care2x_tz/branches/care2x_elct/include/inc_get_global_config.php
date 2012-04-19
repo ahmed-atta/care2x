469d4805e3c128cb8171ab46781ca8344b5f9058
tim_schofield <tim_schofield@8154e661-2335-47f1-af25-7e1110f3596a>
2012-04-19 10:44:56 +0000

    Properly set the config variables

--- care2x_tz/branches/care2x_elct/include/inc_get_global_config.php
+++ care2x_tz/branches/care2x_elct/include/inc_get_global_config.php
@@ -1,25 +1,24 @@
 <?php
 
-	    $sql="SELECT type,value FROM care_config_global WHERE type LIKE '$config_type'";
-	 //   $sql='SELECT type,value FROM care_config_global';
+		$sql="SELECT type,value FROM care_config_global";
 
-	    $global_result=$db->Execute($sql);
-		
+		$global_result=$db->Execute($sql);
+
 		//echo $sql;
-	
-        if($global_result)
-        {
-  		    while($data_result= $global_result->FetchRow())
-			    {
-                      
-                     $data_result['type']=$data_result['value'];				   
-			     }
-	   		
-			    $global_config_ok=1; 
+
+		if($global_result)
+		{
+			while($data_result= $global_result->FetchRow())
+				{
+
+					 ${$data_result['type']}=$data_result['value'];
+
+				 }
+				$global_config_ok=1;
 		}
 		 else
-		 {
-		 	$global_config_ok=0;
-		}	
+		{
+			$global_config_ok=0;
+		}
 
 ?>
