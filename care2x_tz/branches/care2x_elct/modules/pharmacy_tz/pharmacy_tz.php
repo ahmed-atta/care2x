5a4fae1575381a2549b32ea57f10af8e70c4f03e
blesko <blesko@8154e661-2335-47f1-af25-7e1110f3596a>
2012-07-28 00:30:06 +0000

    Converted to smarty to keep inside the frame

--- care2x_tz/branches/care2x_elct/modules/pharmacy_tz/pharmacy_tz.php
+++ care2x_tz/branches/care2x_elct/modules/pharmacy_tz/pharmacy_tz.php
@@ -11,11 +11,83 @@ require($root_path.'include/inc_environment_global.php');
 *
 * See the file "copy_notice.txt" for the licence notice
 */
-$lang_tables[]='pharmacy.php';
-//define('NO_2LEVEL_CHK',1);
+//$lang_tables[]='pharmacy.php';
+define('LANG_FILE','pharmacy.php');
+define('NO_2LEVEL_CHK',1);
 require($root_path.'include/inc_front_chain_lang.php');
+$breakfile=$root_path.'modules/news/start_page.php'.URL_APPEND;
+// reset all 2nd level lock cookies
+require($root_path.'include/inc_2level_reset.php');
 
+# Start Smarty templating here
+ /**
+ * LOAD Smarty
+ */
 
-require ("gui/gui_pharmacy_tz.php");
+ # Note: it is advisable to load this after the inc_front_chain_lang.php so
+ # that the smarty script can use the user configured template theme
+
+ require_once($root_path.'gui/smarty_template/smarty_care.class.php');
+ $smarty = new smarty_care('common');
+
+ # Module title in the toolbar
+
+ $smarty->assign('sToolbarTitle',$LDPharmacy);
+
+ # Help button href
+ $smarty->assign('pbHelp',"javascript:gethelp('pharmacy_menu.php','Pharmacy :: Main Menu')");
+
+ $smarty->assign('breakfile',$breakfile);
+
+ # Window bar title
+ $smarty->assign('title',$LDPharmacy);
+
+//require ("gui/gui_pharmacy_tz.php");
 
 ?>
+<html>
+<head>
+</head>
+<body>
+<?php
+ # Pharmacy submenu block
+
+ $smarty->assign('LDPrescription',$LDPrescription);
+
+$smarty->assign('LDDrugPrescriptions',"<a href=\"pharmacy_tz_pending_prescriptions.php?sid=$sid&lang=$lang&prescrServ=prescr&comming_from=pharmacy\">$LDDrugPrescription</a>");
+$smarty->assign('LDShowPrescriptionsofPatients',$LDShowPrescriptionsofPatients);
+
+$smarty->assign('LDPendingOutpatientPrescriptions',"<a href=\"pharmacy_tz_pending_prescriptions.php?sid=$sid&lang=$lang&prescrServ=prescr&admission=outpatient&comming_from=pharmacy\">$LDDrugPrescriptionIPD</a>");
+$smarty->assign('LDShowPrescriptionsofPatientsOPD',$LDShowPrescriptionsofPatientsOPD);
+
+$smarty->assign('LDPendingInpatientPrescriptions',"<a href=\"pharmacy_tz_pending_prescriptions.php?sid=$sid&lang=$lang&prescrServ=prescr&admission=inpatient&comming_from=pharmacy\">$LDDrugPrescriptionOPD</a>");
+$smarty->assign('LDShowPrescriptionsofPatientsIPD',$LDShowPrescriptionsofPatientsIPD);
+
+ # Pharmacy submenu block
+
+ $smarty->assign('LDOrderingProducts',$LDOrderingProducts);
+
+$smarty->assign('LDOrderCat',"<a href=\"pharmacy_tz_product_catalog.php\">$LDMyProductCatalog</a>");
+$smarty->assign('LDCreateEditUpdateRemove',$LDCreateEditUpdateRemove);
+
+$smarty->assign('LDManagePrice',"<a href=\"magage_pricelist.php\">$LDManagePriceList</a>");
+$smarty->assign('LDManagePriceList_description',$LDManagePriceList_description);
+
+$smarty->assign('LDWardPrescriptions',"<a href=\"pharmacy_tz_show_prescr.php\">$LDPrescrWard</a>");
+$smarty->assign('LDShowPrescrPerWardAndDay',$LDShowPrescrPerWardAndDay);
+
+
+# Assign the submenu to the mainframe center block
+
+ $smarty->assign('sMainBlockIncludeFile','pharmacy/submenu_pharmacy.tpl');
+
+ /**
+ * show  Mainframe Template
+ */
+
+ $smarty->display('common/mainframe.tpl');
+
+
+?>
+</BODY>
+</HTML>
