af7bf905eca2568c430c4646979699cdcf69dc76
blesko <blesko@8154e661-2335-47f1-af25-7e1110f3596a>
2012-07-31 02:02:24 +0000

    lab table updates

--- care2x_tz/branches/care2x_elct/install/mysql/manual/care_test_findings_chemlabor_sub.sql
+++ care2x_tz/branches/care2x_elct/install/mysql/manual/care_test_findings_chemlabor_sub.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2012 at 12:59 PM
+-- Generation Time: Jul 30, 2012 at 10:18 AM
 -- Server version: 5.1.63
 -- PHP Version: 5.2.17
 
@@ -34,16 +34,8 @@ CREATE TABLE IF NOT EXISTS `care_test_findings_chemlabor_sub` (
   `encounter_nr` int(11) NOT NULL DEFAULT '0',
   `paramater_name` varchar(255) DEFAULT '0',
   `parameter_value` varchar(255) DEFAULT '0',
-  `status` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `history` text CHARACTER SET latin1 COLLATE latin1_general_ci,
-  `test_date` date NOT NULL DEFAULT '0000-00-00',
-  `test_time` time DEFAULT NULL,
-  `create_id` varchar(35) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modify_id` varchar(10) DEFAULT NULL,
-  `modify_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`sub_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=46 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
